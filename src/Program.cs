f73c7479e4c48dff8583ab21397691d5dfd82beb
niksedk <nikse.dk@gmail.com>
2010-10-12 11:20:29 +0000

    Initial version
    git-svn-id: https://subtitleedit.googlecode.com/svn/trunk@12 99eadd0c-20b8-1223-b5c4-2a2b2df33de2

---
+++ src/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+using Nikse.SubtitleEdit.Forms;
+
+namespace Nikse.SubtitleEdit
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Main());
+        }
+    }
+}
