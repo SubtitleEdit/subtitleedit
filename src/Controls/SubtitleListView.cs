10cd8b3e525f652844e590f5741f8461e0d6dcca
niksedk <nikse.dk@gmail.com>
2010-10-16 14:59:28 +0000

    minor updates to controls
    git-svn-id: https://subtitleedit.googlecode.com/svn/trunk@55 99eadd0c-20b8-1223-b5c4-2a2b2df33de2

--- src/Controls/SubtitleListView.cs
+++ src/Controls/SubtitleListView.cs
@@ -45,6 +45,7 @@ namespace Nikse.SubtitleEdit.Controls
 
         public SubtitleListView()
         {
+            this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             Columns.AddRange(new[] 
             {
 			    new ColumnHeader { Text="#", Width=55 },
