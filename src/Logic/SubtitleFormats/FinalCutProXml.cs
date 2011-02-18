bf60d6c877d44c345d6eb3d0c383f71e80a71696
niksedk <nikse.dk@gmail.com>
2011-02-18 14:44:37 +0000

    SE 3.1 RC3 - Updated Spanish language file - thx Iban :)
    git-svn-id: https://subtitleedit.googlecode.com/svn/trunk@340 99eadd0c-20b8-1223-b5c4-2a2b2df33de2

--- src/Logic/SubtitleFormats/FinalCutProXml.cs
+++ src/Logic/SubtitleFormats/FinalCutProXml.cs
@@ -39,15 +39,18 @@ namespace Nikse.SubtitleEdit.Logic.SubtitleFormats
 
         private string IsNtsc()
         {
-            if (Configuration.Settings.General.DefaultFrameRate >= 29.976 &&
-                Configuration.Settings.General.DefaultFrameRate <= 30.0)
+            if (FrameRate >= 29.976 && FrameRate <= 30.0)
                 return "TRUE";
-            return "FALSE";
-            
+            return "FALSE";            
         }
 
         public override string ToText(Subtitle subtitle, string title)
         {
+            if (Configuration.Settings.General.DefaultFrameRate > 26)
+                FrameRate = 30;
+            else
+                FrameRate = 25;
+
             string xmlStructure =
                 "<?xml version=\"1.0\" encoding=\"utf-8\" ?>" + Environment.NewLine +
                 "<!DOCTYPE xmeml>" + Environment.NewLine +
@@ -65,7 +68,7 @@ namespace Nikse.SubtitleEdit.Logic.SubtitleFormats
                 "    <name>Text</name>" + Environment.NewLine +
                 "    <rate>" + Environment.NewLine +
                 "        <ntsc>" + IsNtsc() + "</ntsc>" + Environment.NewLine +
-                "        <timebase>" + Configuration.Settings.General.DefaultFrameRate.ToString() + "</timebase>" + Environment.NewLine +
+                "        <timebase>" + (int)Math.Round(FrameRate) + "</timebase>" + Environment.NewLine +
                 "    </rate>" + Environment.NewLine +
                 "    <start></start>" + Environment.NewLine + // start frame?
                 "    <end></end>" + Environment.NewLine + // end frame?
@@ -74,7 +77,7 @@ namespace Nikse.SubtitleEdit.Logic.SubtitleFormats
                 "    <alphatype>black</alphatype>" + Environment.NewLine +
                 "    <effect id=\"subtitle\">" + Environment.NewLine +
                 "        <name>Text</name>" + Environment.NewLine +
-                "        <effectid>Text</effectid>" + Environment.NewLine +
+//                "        <effectid>Text</effectid>" + Environment.NewLine +
                 "        <effectcategory>Text</effectcategory>" + Environment.NewLine +
                 "        <effecttype>generator</effecttype>" + Environment.NewLine +
                 "        <mediatype>video</mediatype>" + Environment.NewLine +
@@ -86,11 +89,254 @@ namespace Nikse.SubtitleEdit.Logic.SubtitleFormats
                 "    </effect>" + Environment.NewLine +
                 "</generatoritem>";
 
+            string xmlTrackStructure2 =
+                @"<track>
+				<generatoritem id='Subtitle Edit'>
+					<name>edit 'B subtitle</name>
+					<duration>3000</duration>
+					<rate>
+						<ntsc>FALSE</ntsc>
+						<timebase>25</timebase>
+					</rate>
+					<in>1375</in>
+					<out>1383</out>
+					<start>0</start>
+					<end>8</end>
+					<enabled>TRUE</enabled>
+					<anamorphic>TRUE</anamorphic>
+					<alphatype>black</alphatype>
+					<logginginfo>
+						<scene>
+						</scene>
+						<shottake>
+						</shottake>
+						<lognote>
+						</lognote>
+						<good>FALSE</good>
+					</logginginfo>
+					<labels>
+						<label2>
+						</label2>
+					</labels>
+					<comments>
+						<mastercomment1>
+						</mastercomment1>
+						<mastercomment2>
+						</mastercomment2>
+						<mastercomment3>
+						</mastercomment3>
+						<mastercomment4>
+						</mastercomment4>
+					</comments>
+					<effect>
+						<name>edit 'B subtitle</name>
+						<effectid>edit 'B subtitle</effectid>
+						<effectcategory>Text</effectcategory>
+						<effecttype>generator</effecttype>
+						<mediatype>video</mediatype>
+						<parameter>
+							<parameterid>part1</parameterid>
+							<name>Text Settings</name>
+							<value>0</value>
+						</parameter>
+						<parameter>
+							<parameterid>str</parameterid>
+							<name>Text</name>
+							<value>&#13;imported text from: small.srt</value>
+						</parameter>
+						<parameter>
+							<parameterid>fontname</parameterid>
+							<name>Font</name>
+							<value>Arial Narrow</value>
+						</parameter>
+						<parameter>
+							<parameterid>fontstyle</parameterid>
+							<name>Style</name>
+							<valuemin>1</valuemin>
+							<valuemax>4</valuemax>
+							<valuelist>
+								<valueentry>
+									<name>Plain</name>
+									<value>1</value>
+								</valueentry>
+								<valueentry>
+									<name>Bold</name>
+									<value>2</value>
+								</valueentry>
+								<valueentry>
+									<name>Italic</name>
+									<value>3</value>
+								</valueentry>
+								<valueentry>
+									<name>Bold/Italic</name>
+									<value>4</value>
+								</valueentry>
+							</valuelist>
+							<value>1</value>
+						</parameter>
+						<parameter>
+							<parameterid>fontalign</parameterid>
+							<name>Alignment</name>
+							<valuemin>1</valuemin>
+							<valuemax>3</valuemax>
+							<valuelist>
+								<valueentry>
+									<name>Left</name>
+									<value>1</value>
+								</valueentry>
+								<valueentry>
+									<name>Center</name>
+									<value>2</value>
+								</valueentry>
+								<valueentry>
+									<name>Right</name>
+									<value>3</value>
+								</valueentry>
+							</valuelist>
+							<value>2</value>
+						</parameter>
+						<parameter>
+							<parameterid>fontsize</parameterid>
+							<name>Size</name>
+							<valuemin>20</valuemin>
+							<valuemax>80</valuemax>
+							<value>28</value>
+						</parameter>
+						<parameter>
+							<parameterid>origin</parameterid>
+							<name>Origin</name>
+							<value>
+								<horiz>0</horiz>
+								<vert>0.3056</vert>
+							</value>
+						</parameter>
+						<parameter>
+							<parameterid>aspect</parameterid>
+							<name>Aspect</name>
+							<valuemin>0</valuemin>
+							<valuemax>2</valuemax>
+							<value>1</value>
+						</parameter>
+						<parameter>
+							<parameterid>textopacity</parameterid>
+							<name>Text Opacity</name>
+							<valuemin>0</valuemin>
+							<valuemax>100</valuemax>
+							<value>100</value>
+						</parameter>
+						<parameter>
+							<parameterid>textcolor</parameterid>
+							<name>Text Color</name>
+							<value>
+								<alpha>255</alpha>
+								<red>255</red>
+								<green>255</green>
+								<blue>255</blue>
+							</value>
+						</parameter>
+						<parameter>
+							<parameterid>fonttrack</parameterid>
+							<name>Tracking</name>
+							<valuemin>0</valuemin>
+							<valuemax>10</valuemax>
+							<value>1</value>
+						</parameter>
+						<parameter>
+							<parameterid>leading</parameterid>
+							<name>Leading</name>
+							<valuemin>-40</valuemin>
+							<valuemax>40</valuemax>
+							<value>-13</value>
+						</parameter>
+						<parameter>
+							<parameterid>autokern</parameterid>
+							<name>Auto Kerning</name>
+							<value>TRUE</value>
+						</parameter>
+						<parameter>
+							<parameterid>part2</parameterid>
+							<name>Outline Settings</name>
+							<value>0</value>
+						</parameter>
+						<parameter>
+							<parameterid>linewidth</parameterid>
+							<name>Width</name>
+							<valuemin>0</valuemin>
+							<valuemax>50</valuemax>
+							<value>8</value>
+						</parameter>
+						<parameter>
+							<parameterid>linesoft</parameterid>
+							<name>Soft</name>
+							<valuemin>0</valuemin>
+							<valuemax>100</valuemax>
+							<value>38</value>
+						</parameter>
+						<parameter>
+							<parameterid>linecolor</parameterid>
+							<name>Color</name>
+							<value>
+								<alpha>255</alpha>
+								<red>0</red>
+								<green>0</green>
+								<blue>0</blue>
+							</value>
+						</parameter>
+						<parameter>
+							<parameterid>part3</parameterid>
+							<name>Shadow Settings</name>
+							<value>0</value>
+						</parameter>
+						<parameter>
+							<parameterid>shadowoffsetx</parameterid>
+							<name>Offset X</name>
+							<valuemin>-20</valuemin>
+							<valuemax>20</valuemax>
+							<value>3</value>
+						</parameter>
+						<parameter>
+							<parameterid>shadowoffsety</parameterid>
+							<name>Offset y</name>
+							<valuemin>-20</valuemin>
+							<valuemax>20</valuemax>
+							<value>3</value>
+						</parameter>
+						<parameter>
+							<parameterid>shadowopacity</parameterid>
+							<name>Opacity</name>
+							<valuemin>0</valuemin>
+							<valuemax>100</valuemax>
+							<value>75</value>
+						</parameter>
+						<parameter>
+							<parameterid>shadowsoft</parameterid>
+							<name>Softness</name>
+							<valuemin>0</valuemin>
+							<valuemax>100</valuemax>
+							<value>2</value>
+						</parameter>
+						<parameter>
+							<parameterid>shadowcolor</parameterid>
+							<name>Color</name>
+							<value>
+								<alpha>255</alpha>
+								<red>0</red>
+								<green>0</green>
+								<blue>0</blue>
+							</value>
+						</parameter>
+					</effect>
+					<sourcetrack>
+						<mediatype>video</mediatype>
+					</sourcetrack>
+				</generatoritem>";
+
             XmlDocument xml = new XmlDocument();
             xml.LoadXml(xmlStructure);
 
             XmlNode videoNode = xml.DocumentElement.SelectSingleNode("sequence/media/video");
 
+            int number = 1;
             foreach (Paragraph p in subtitle.Paragraphs)
             {
                 XmlNode track = xml.CreateElement("track");
@@ -98,14 +344,19 @@ namespace Nikse.SubtitleEdit.Logic.SubtitleFormats
 
                 double frameRate = Configuration.Settings.General.DefaultFrameRate;
                 XmlNode start = track.SelectSingleNode("generatoritem/start");
-                start.InnerText = (((int)Math.Round(p.StartTime.TotalSeconds*frameRate))).ToString();
+                start.InnerText = ((int)Math.Round(p.StartTime.TotalSeconds*frameRate)).ToString();
 
                 XmlNode end = track.SelectSingleNode("generatoritem/end");
-                end.InnerText = (((int)Math.Round(p.EndTime.TotalSeconds * frameRate))).ToString();
+                end.InnerText = ((int)Math.Round(p.EndTime.TotalSeconds * frameRate)).ToString();
 
-                XmlNode text = track.SelectSingleNode("generatoritem/effect/parameter/value");
+                XmlNode text = track.SelectSingleNode("generatoritem/effect/parameter[parameterid='str']/value");
                 text.InnerText = Utilities.RemoveHtmlTags(p.Text);
+
+                XmlNode effect = track.SelectSingleNode("generatoritem/effect");
+                effect.Attributes["id"].InnerText = "Subtitle" + number.ToString();
+
                 videoNode.AppendChild(track);
+                number++;
             }
 
             MemoryStream ms = new MemoryStream();
