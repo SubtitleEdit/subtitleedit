6d4af448dec00244d789057d5085050c08930c9c
XhmikosR <xhmikosr@users.sourceforge.net>
2011-04-20 10:08:46 +0000

    improve build.bat
    git-svn-id: https://subtitleedit.googlecode.com/svn/trunk@403 99eadd0c-20b8-1223-b5c4-2a2b2df33de2

--- build.bat
+++ build.bat
@@ -1,8 +1,13 @@
 @ECHO OFF
 SETLOCAL
-TITLE Compiling Subtitle Edit...
-
 CD /D %~dp0
+
+rem Check for the help switches
+IF /I "%~1"=="help"   GOTO SHOWHELP
+IF /I "%~1"=="/help"  GOTO SHOWHELP
+IF /I "%~1"=="-help"  GOTO SHOWHELP
+IF /I "%~1"=="--help" GOTO SHOWHELP
+IF /I "%~1"=="/?"     GOTO SHOWHELP
 
 IF NOT DEFINED VS100COMNTOOLS (
   ECHO Visual Studio 2010 wasn't found
@@ -34,8 +39,11 @@ IF "%~1" == "" (
 PUSHD "src"
 
 CALL "%VS100COMNTOOLS%vsvars32.bat"
+TITLE %BUILDTYPE%ing SubtitleEdit with MSVC 2010 - Release^|Any CPU...
 
-devenv SubtitleEdit.sln /%BUILDTYPE% "Release|Any CPU"
+"%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe" SubtitleEdit.sln^
+ /t:%BUILDTYPE% /p:Configuration=Release /p:Platform="Any CPU" /maxcpucount^
+ /consoleloggerparameters:DisableMPLogging;Summary;Verbosity=minimal
 IF %ERRORLEVEL% NEQ 0 GOTO EndWithError
 
 ECHO.
@@ -48,6 +56,7 @@ CALL :SubDetectInnoSetup
 IF DEFINED InnoSetupPath (
   PUSHD "installer"
 
+  TITLE Compiling installer...
   "%InnoSetupPath%\iscc.exe" /Q "Subtitle_Edit_installer.iss"
   IF %ERRORLEVEL% NEQ 0 GOTO EndWithError
 
@@ -73,6 +82,20 @@ ECHO  **ERROR: Build failed and aborted!**
 PAUSE
 ENDLOCAL
 EXIT
+
+
+:SHOWHELP
+TITLE "%~nx0 %1"
+ECHO. & ECHO.
+ECHO Usage:   %~nx0 [Clean^|Build^|Rebuild]
+ECHO.
+ECHO Notes:   You can also prefix the commands with "-", "--" or "/".
+ECHO          The arguments are case insesitive.
+ECHO. & ECHO.
+ECHO Executing "%~nx0" will use the defaults: "%~nx0 build"
+ECHO.
+ENDLOCAL
+EXIT /B
 
 
 :SubDetectInnoSetup
