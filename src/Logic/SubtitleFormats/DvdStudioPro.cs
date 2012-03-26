08108a8904d9c33f0e91b18ab49e23fdc92ba6f0
niksedk <nikse.dk@gmail.com>
2012-03-26 14:04:37 +0000

    Fixed italics/bold in Dvd Studio Pro format - thx Mikko :)
    git-svn-id: https://subtitleedit.googlecode.com/svn/trunk@1073 99eadd0c-20b8-1223-b5c4-2a2b2df33de2

--- src/Logic/SubtitleFormats/DvdStudioPro.cs
+++ src/Logic/SubtitleFormats/DvdStudioPro.cs
@@ -7,7 +7,7 @@ namespace Nikse.SubtitleEdit.Logic.SubtitleFormats
 {
     public class DvdStudioPro : SubtitleFormat
     {
-        static Regex _regexTimeCodes = new Regex(@"^\d+:\d+:\d+:\d+\t,\t\d+:\d+:\d+:\d+\t,\t.*$", RegexOptions.Compiled);
+        static readonly Regex RegexTimeCodes = new Regex(@"^\d+:\d+:\d+:\d+\t,\t\d+:\d+:\d+:\d+\t,\t.*$", RegexOptions.Compiled);
 
         public override string Extension
         {
@@ -31,7 +31,7 @@ namespace Nikse.SubtitleEdit.Logic.SubtitleFormats
 
         public override bool IsMine(List<string> lines, string fileName)
         {
-            Subtitle subtitle = new Subtitle();
+            var subtitle = new Subtitle();
             LoadSubtitle(subtitle, lines, fileName);
             return subtitle.Paragraphs.Count > _errorCount;
         }
@@ -56,22 +56,21 @@ $FadeOut                =   0
 $HorzAlign          =   Center
 ";
 
-            StringBuilder sb = new StringBuilder();
+            var sb = new StringBuilder();
             sb.AppendLine(header);
             foreach (Paragraph p in subtitle.Paragraphs)
             {
                 double factor = (1000.0 / Configuration.Settings.General.CurrentFrameRate);
                 string startTime = string.Format(timeFormat, p.StartTime.Hours, p.StartTime.Minutes, p.StartTime.Seconds, (int)Math.Round(p.StartTime.Milliseconds  / factor));
                 string endTime = string.Format(timeFormat, p.EndTime.Hours, p.EndTime.Minutes, p.EndTime.Seconds, (int)Math.Round(p.EndTime.Milliseconds / factor));
-                sb.Append(string.Format(paragraphWriteFormat, startTime, endTime, p.Text.Replace(Environment.NewLine, " | ")));
+                sb.Append(string.Format(paragraphWriteFormat, startTime, endTime, EncodeStyles(p.Text.Replace(Environment.NewLine, " | "))));
             }
             return sb.ToString().Trim();
         }
 
         public static byte GetFrameFromMilliseconds(int milliseconds, double frameRate)
         {
-            int frame = (int)(milliseconds / (1000.0 / frameRate));
-            return (byte)(frame);
+            return (byte)Math.Round(milliseconds / (1000.0 / frameRate));
         }
 
         public override void LoadSubtitle(Subtitle subtitle, List<string> lines, string fileName)
@@ -82,10 +81,10 @@ $HorzAlign          =   Center
             {
                 if (line.Trim().Length > 0 && line[0] != '$')
                 {
-                    if (_regexTimeCodes.Match(line).Success)
+                    if (RegexTimeCodes.Match(line).Success)
                     {
                         string[] threePart = line.Split(new[] { "\t,\t"}, StringSplitOptions.None);
-                        Paragraph p = new Paragraph();
+                        var p = new Paragraph();
                         if (threePart.Length == 3 &&
                             GetTimeCode(p.StartTime, threePart[0]) &&
                             GetTimeCode(p.EndTime, threePart[1]))
@@ -93,6 +92,7 @@ $HorzAlign          =   Center
                             number++;
                             p.Number = number;
                             p.Text = threePart[2].TrimEnd().Replace(" | ", Environment.NewLine).Replace("|", Environment.NewLine);
+                            p.Text = DecodeStyles(p.Text);
                             subtitle.Paragraphs.Add(p);
                         }
                     }
@@ -102,6 +102,62 @@ $HorzAlign          =   Center
                     }
                 }
             }
+        }
+
+        internal static string DecodeStyles(string text)
+        {
+            var sb = new StringBuilder();
+            bool italicOn = false;
+            bool boldOn = false;
+            bool skipNext = false;
+            for (int i = 0; i<text.Length; i++)
+            {
+                if (skipNext)
+                {
+                    skipNext = false;
+                }
+                else
+                {
+                    if (text.Substring(i).StartsWith("^I"))
+                    {
+                        if (!italicOn)
+                            sb.Append("<i>");
+                        else
+                            sb.Append("</i>");
+                        italicOn = !italicOn;
+                        skipNext = true;
+                    }
+                    else if (text.Substring(i).StartsWith("^B"))
+                    {
+                        if (!boldOn)
+                            sb.Append("<b>");
+                        else
+                            sb.Append("</b>");
+                        boldOn = !boldOn;
+                        skipNext = true;
+                    }
+                    else
+                    {
+                        sb.Append(text.Substring(i, 1));
+                    }                    
+                }
+            }
+            return sb.ToString();
+        }
+
+        internal static string EncodeStyles(string text)
+        {
+            text = text.Replace("<i>", "^I");
+            text = text.Replace("<I>", "^I");
+            text = text.Replace("</i>", "^I");
+            text = text.Replace("</I>", "^I");
+
+            text = text.Replace("<b>", "^B");
+            text = text.Replace("<B>", "^B");
+            text = text.Replace("</b>", "^B");
+            text = text.Replace("</B>", "^B");
+
+            return text;
         }
 
         private static bool GetTimeCode(TimeCode timeCode, string timeString)
@@ -114,7 +170,7 @@ $HorzAlign          =   Center
                 timeCode.Seconds = int.Parse(timeParts[2]);
                 int frames = int.Parse(timeParts[3]);
 
-                int milliseconds = (int)(1000.0 / Configuration.Settings.General.CurrentFrameRate * frames);
+                int milliseconds = (int)Math.Round(1000.0 / Configuration.Settings.General.CurrentFrameRate * frames);
                 if (milliseconds > 999)
                     milliseconds = 999;
 
