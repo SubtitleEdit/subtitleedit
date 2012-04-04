100542e48652cb66807f5188b6b2c7a8c2b70293
niksedk <nikse.dk@gmail.com>
2012-04-04 16:45:49 +0000

    Settings: "Merge lines shorter than" cannot be larger than "Single line max length"
    git-svn-id: https://subtitleedit.googlecode.com/svn/trunk@1090 99eadd0c-20b8-1223-b5c4-2a2b2df33de2

--- src/Forms/Settings.cs
+++ src/Forms/Settings.cs
@@ -843,6 +843,8 @@ namespace Nikse.SubtitleEdit.Forms
             toolsSettings.StartSceneIndex = comboBoxToolsStartSceneIndex.SelectedIndex;
             toolsSettings.EndSceneIndex = comboBoxToolsEndSceneIndex.SelectedIndex;
             toolsSettings.MergeLinesShorterThan = comboBoxMergeShortLineLength.SelectedIndex + 10;
+            if (toolsSettings.MergeLinesShorterThan >= gs.SubtitleLineMaximumLength)
+                toolsSettings.MergeLinesShorterThan = gs.SubtitleLineMaximumLength - 1;
             toolsSettings.MusicSymbol = comboBoxToolsMusicSymbol.SelectedItem.ToString();
             toolsSettings.MusicSymbolToReplace = textBoxMusicSymbolsToReplace.Text;
             toolsSettings.SpellCheckAutoChangeNames = checkBoxSpellCheckAutoChangeNames.Checked;
