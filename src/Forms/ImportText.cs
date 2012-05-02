05413fa61bc2976f8dcee04369ee3d103bad1a39
niksedk <nikse.dk@gmail.com>
2012-05-02 13:42:00 +0000

    Minor fix for import text - "Remove lines without letters" now works better
    git-svn-id: https://subtitleedit.googlecode.com/svn/trunk@1157 99eadd0c-20b8-1223-b5c4-2a2b2df33de2

--- src/Forms/ImportText.cs
+++ src/Forms/ImportText.cs
@@ -276,6 +276,11 @@ namespace Nikse.SubtitleEdit.Forms
 
         private bool ContainsLetters(string line)
         {
+            if (line.Replace("0", string.Empty).Replace("1", string.Empty).Replace("2", string.Empty).Replace("3", string.Empty).Replace("4", string.Empty).Replace("5", string.Empty).Replace("6", string.Empty)
+                .Replace("7", string.Empty).Replace("8", string.Empty).Replace("9", string.Empty).Replace(":", string.Empty).Replace(".", string.Empty).Replace(",", string.Empty).
+                Replace("-", string.Empty).Replace(">", string.Empty).Trim().Length == 0)
+                return false;
+
             foreach (char ch in line)
             {
                 if (!("\r\n\t .?" + Convert.ToChar(0)).Contains(ch.ToString(CultureInfo.InvariantCulture)))
