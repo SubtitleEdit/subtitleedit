828f49664f4c5aeec202f9e61f1f12ad293842a3
niksedk <nikse.dk@gmail.com>
2012-05-14 17:02:45 +0000

    Added shortcut for new time-line selection "Add text there"
    git-svn-id: https://subtitleedit.googlecode.com/svn/trunk@1182 99eadd0c-20b8-1223-b5c4-2a2b2df33de2

--- src/Forms/Settings.cs
+++ src/Forms/Settings.cs
@@ -564,6 +564,8 @@ namespace Nikse.SubtitleEdit.Forms
                 audioVisualizerNode.Nodes.Add(Configuration.Settings.Language.Settings.WaveformSeekSilenceForward + GetShortcutText(Configuration.Settings.Shortcuts.WaveformSearchSilenceForward));
             if (!string.IsNullOrEmpty(Configuration.Settings.Language.Settings.WaveformSeekSilenceBack)) // TODO: Remove in SE 3.3
                 audioVisualizerNode.Nodes.Add(Configuration.Settings.Language.Settings.WaveformSeekSilenceBack + GetShortcutText(Configuration.Settings.Shortcuts.WaveformSearchSilenceBack));
+            if (!string.IsNullOrEmpty(Configuration.Settings.Language.Settings.WaveformAddTextHere)) // TODO: Remove in SE 3.3
+                audioVisualizerNode.Nodes.Add(Configuration.Settings.Language.Settings.WaveformAddTextHere + GetShortcutText(Configuration.Settings.Shortcuts.WaveformAddTextHere));
             audioVisualizerNode.Nodes.Add(Configuration.Settings.Language.WaveForm.PlaySelection + GetShortcutText(Configuration.Settings.Shortcuts.WaveformPlaySelection));
             treeViewShortcuts.Nodes.Add(audioVisualizerNode);
 
@@ -1116,6 +1118,8 @@ namespace Nikse.SubtitleEdit.Forms
                         Configuration.Settings.Shortcuts.WaveformSearchSilenceForward = GetShortcut(node.Text);
                     else if (Configuration.Settings.Language.Settings.WaveformSeekSilenceBack != null && text == (Configuration.Settings.Language.Settings.WaveformSeekSilenceBack).Replace("&", string.Empty)) // SE 3.3
                         Configuration.Settings.Shortcuts.WaveformSearchSilenceBack = GetShortcut(node.Text);
+                    else if (Configuration.Settings.Language.Settings.WaveformAddTextHere != null && text == (Configuration.Settings.Language.Settings.WaveformAddTextHere).Replace("&", string.Empty)) // SE 3.3
+                        Configuration.Settings.Shortcuts.WaveformAddTextHere = GetShortcut(node.Text);
                 }
             }
 
