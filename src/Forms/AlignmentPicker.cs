48ec96fbc083f048e11751e53427aec80d5271db
niksedk <nikse.dk@gmail.com>
2012-06-04 16:30:09 +0000

    New "alignment picker"
    git-svn-id: https://subtitleedit.googlecode.com/svn/trunk@1224 99eadd0c-20b8-1223-b5c4-2a2b2df33de2

---
+++ src/Forms/AlignmentPicker.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Nikse.SubtitleEdit.Forms
+{
+   
+
+    public partial class AlignmentPicker : Form
+    {
+        Timer t1 = new Timer();
+        public ContentAlignment Alignment { get; set; }
+
+        public AlignmentPicker()
+        {
+            InitializeComponent();
+            t1.Interval = 100;
+            t1.Tick += new EventHandler(t1_Tick);
+            t1.Start();
+        }
+
+        public void Done()
+        {
+            DialogResult = DialogResult.OK;
+        }
+
+        void t1_Tick(object sender, EventArgs e)
+        {
+            var currentPos = System.Windows.Forms.Cursor.Position;
+            if (currentPos.X < this.Left || currentPos.X > this.Left + this.Width)
+                Close();
+            if (currentPos.Y < this.Top || currentPos.Y > this.Top + this.Height)
+                Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Alignment = ContentAlignment.TopLeft;
+            Done();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Alignment = ContentAlignment.TopCenter;
+            Done();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Alignment = ContentAlignment.TopRight;
+            Done();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Alignment = ContentAlignment.MiddleLeft;
+            Done();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Alignment = ContentAlignment.MiddleCenter;
+            Done();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Alignment = ContentAlignment.MiddleRight;
+            Done();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            Alignment = ContentAlignment.BottomLeft;
+            Done();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            Alignment = ContentAlignment.BottomCenter;
+            Done();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            Alignment = ContentAlignment.BottomRight;
+            Done();
+        }
+
+        private void AlignmentPicker_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+                DialogResult = DialogResult.Cancel;
+        }
+
+        private void AlignmentPicker_Shown(object sender, EventArgs e)
+        {
+            button8.Focus();
+        }
+
+    }
+}
