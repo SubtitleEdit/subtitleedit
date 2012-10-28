6cd10797a4bbbc86e6672e7f9f51308712d81bc3
niksedk <nikse.dk@gmail.com>
2012-10-28 17:01:44 +0000

    Added some missing language tags - thx Martin and Quetsbeek :)
    git-svn-id: https://subtitleedit.googlecode.com/svn/trunk@1447 99eadd0c-20b8-1223-b5c4-2a2b2df33de2

--- src/Forms/WaveFormGenerateTimeCodes.cs
+++ src/Forms/WaveFormGenerateTimeCodes.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System; 
 using System.Windows.Forms;
 using Nikse.SubtitleEdit.Logic;
 
@@ -24,9 +24,26 @@ namespace Nikse.SubtitleEdit.Forms
             radioButtonStartFromPos.Text = l.CurrentVideoPosition;
             radioButtonStartFromStart.Text = l.Beginning;
             groupBoxDeleteLines.Text = l.DeleteLines;
+            groupBoxDetectOptions.Text = l.DetectOptions;
+            labelScanBlocksMs.Text = l.ScanBlocksOfMs;
+            labelAbove1.Text = l.BlockAverageVolMin1;
+            labelAbove2.Text = l.BlockAverageVolMin2;
+            labelBelow1.Text = l.BlockAverageVolMax1;
+            labelBelow2.Text = l.BlockAverageVolMax2;
+            groupBoxOther.Text = l.Other;
+            labelSplit1.Text = l.SplitLongLinesAt1;
+            labelSplit2.Text = l.SplitLongLinesAt2;
             radioButtonDeleteAll.Text = Configuration.Settings.Language.General.All;
             radioButtonDeleteNone.Text = Configuration.Settings.Language.General.None;
             radioButtonForward.Text = l.FromCurrentVideoPosition;
+
+            numericUpDownBlockSize.Left = labelScanBlocksMs.Left + labelScanBlocksMs.Width + 3;
+            numericUpDownMinVol.Left = labelAbove1.Left + labelAbove1.Width + 3;
+            labelAbove2.Left = numericUpDownMinVol.Left + numericUpDownMinVol.Width + 3;
+            numericUpDownMaxVol.Left = labelBelow1.Left + labelBelow1.Width + 3;
+            labelBelow2.Left = numericUpDownMaxVol.Left + numericUpDownMaxVol.Width + 3;
+            numericUpDownDefaultMilliseconds.Left = labelSplit1.Left + labelSplit1.Width + 3;
+            labelSplit2.Left = numericUpDownDefaultMilliseconds.Left + numericUpDownDefaultMilliseconds.Width + 3;
         }
 
         private void buttonOK_Click(object sender, EventArgs e)
