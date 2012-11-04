182607cef8276f6d52657872566bdb182655a713
niksedk <nikse.dk@gmail.com>
2012-11-04 14:33:55 +0000

    Added new subtitle format
    git-svn-id: https://subtitleedit.googlecode.com/svn/trunk@1462 99eadd0c-20b8-1223-b5c4-2a2b2df33de2

---
+++ src/Logic/SubtitleFormats/UnknownSubtitle40.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Nikse.SubtitleEdit.Logic.SubtitleFormats
+{
+    class UnknownSubtitle40 : SubtitleFormat
+    {
+        // 0:01 – 0:03
+        static Regex regexTimeCodes = new Regex(@"^\d+:\d\d – \d+:\d\d$", RegexOptions.Compiled);
+
+        public override string Extension
+        {
+            get { return ".txt"; }
+        }
+
+        public override string Name
+        {
+            get { return "Uknown 40"; }
+        }
+
+        public override bool IsTimeBased
+        {
+            get { return true; }
+        }
+
+        public override bool IsMine(List<string> lines, string fileName)
+        {
+            var subtitle = new Subtitle();
+            LoadSubtitle(subtitle, lines, fileName);
+            return subtitle.Paragraphs.Count > _errorCount;
+        }
+
+        public override string ToText(Subtitle subtitle, string title)
+        {
+            var sb = new StringBuilder();
+            int index = 0;
+            foreach (Paragraph p in subtitle.Paragraphs)
+            {
+                sb.AppendLine(string.Format("{0} – {1}{2}{3}", EncodeTimeCode(p.StartTime), EncodeTimeCode(p.EndTime), Environment.NewLine, Utilities.RemoveHtmlTags(p.Text)));
+                index++;
+            }
+            return sb.ToString();
+        }
+
+        private string EncodeTimeCode(TimeCode time)
+        {
+            //00:50
+            int seconds = time.Seconds;
+            if (time.Milliseconds >= 500)
+                seconds++;
+            return string.Format("{0}:{1:00}", time.Hours * 60 + time.Minutes, time.Seconds);
+        }
+
+        public override void LoadSubtitle(Subtitle subtitle, List<string> lines, string fileName)
+        {
+            Paragraph p = null;
+            subtitle.Paragraphs.Clear();
+            foreach (string line in lines)
+            {
+                if (regexTimeCodes.IsMatch(line))
+                {
+                    string[] temp = line.Split('–');
+                    string start = temp[0].Trim();
+                    string end = temp[1].Trim();
+
+                    string[] startParts = start.Split(":".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                    string[] endParts = end.Split(":".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                    if (startParts.Length == 2 && endParts.Length == 2)
+                    {
+                        p = new Paragraph(DecodeTimeCode(startParts), DecodeTimeCode(endParts), string.Empty);
+                        subtitle.Paragraphs.Add(p);
+                    }
+                }
+                else if (line.Trim().Length == 0)
+                {
+                    // skip these lines
+                }
+                else if (line.Trim().Length > 0 && p != null)
+                {
+                    if (string.IsNullOrEmpty(p.Text))
+                        p.Text = line;
+                    else
+                        p.Text = p.Text.TrimEnd() + Environment.NewLine + line;
+                    if (p.Text.Length > 500)
+                        return;
+                }
+            }
+            subtitle.Renumber(1);
+        }
+
+        private TimeCode DecodeTimeCode(string[] parts)
+        {
+            //00:00:07:12
+            string minutes = parts[0];
+            string seconds = parts[1];
+
+            TimeCode tc = new TimeCode(0, int.Parse(minutes), int.Parse(seconds), 0);
+            return tc;
+        }
+
+    }
+}
+
