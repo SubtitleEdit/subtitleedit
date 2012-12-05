583afba3186016acd8d4b84946d7272e63668095
niksedk <nikse.dk@gmail.com>
2012-12-05 15:07:50 +0000

    Added Catalan language file for SE 3.3 - thx Juansa :)
    git-svn-id: https://subtitleedit.googlecode.com/svn/trunk@1522 99eadd0c-20b8-1223-b5c4-2a2b2df33de2

--- src/Logic/Utilities.cs
+++ src/Logic/Utilities.cs
@@ -157,7 +157,7 @@ namespace Nikse.SubtitleEdit.Logic
 
         public static IEnumerable<string> GetMovieFileExtensions()
         {
-            return new List<string> { ".avi", ".mkv", ".wmv", ".mpg", ".mpeg", ".divx", ".mp4", ".asf", ".flv", ".mov", ".m4v", ".vob", ".ogv", ".webm", ".ts" };
+            return new List<string> { ".avi", ".mkv", ".wmv", ".mpg", ".mpeg", ".divx", ".mp4", ".asf", ".flv", ".mov", ".m4v", ".vob", ".ogv", ".webm", ".ts", ".m2ts" };
         }
 
         public static string GetVideoFileFilter()
