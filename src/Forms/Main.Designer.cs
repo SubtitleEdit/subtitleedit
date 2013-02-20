1e7085d166ee121fc9d0365ba64d523178027d74
niksedk <nikse.dk@gmail.com>
2013-02-20 11:09:28 +0000

    Remove some right-click menus in Network mode
    git-svn-id: https://subtitleedit.googlecode.com/svn/trunk@1647 99eadd0c-20b8-1223-b5c4-2a2b2df33de2

--- src/Forms/Main.Designer.cs
+++ src/Forms/Main.Designer.cs
@@ -1985,7 +1985,7 @@
             this.changeCasingForSelectedLinesToolStripMenuItem,
             this.toolStripMenuItemSaveSelectedLines});
             this.contextMenuStripListview.Name = "contextMenuStripListview";
-            this.contextMenuStripListview.Size = new System.Drawing.Size(285, 782);
+            this.contextMenuStripListview.Size = new System.Drawing.Size(285, 804);
             this.contextMenuStripListview.Opening += new System.ComponentModel.CancelEventHandler(this.ContextMenuStripListviewOpening);
             // 
             // setStylesForSelectedLinesToolStripMenuItem
