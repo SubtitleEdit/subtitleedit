5f60a68a4fb33cf21437dd4d24d97deb3015ce57
niksedk <nikse.dk@gmail.com>
2013-03-22 05:35:46 +0000

    copy paste bug - thx fox :)
    git-svn-id: https://subtitleedit.googlecode.com/svn/trunk@1756 99eadd0c-20b8-1223-b5c4-2a2b2df33de2

--- src/Forms/Main.Designer.cs
+++ src/Forms/Main.Designer.cs
@@ -2443,7 +2443,7 @@
             this.toolStripButtonWaveFormPause.Name = "toolStripButtonWaveFormPause";
             this.toolStripButtonWaveFormPause.Size = new System.Drawing.Size(23, 22);
             this.toolStripButtonWaveFormPause.Text = "toolStripButton1";
-            this.toolStripButtonWaveFormPlay.Visible = false;
+            this.toolStripButtonWaveFormPause.Visible = false;
             this.toolStripButtonWaveFormPause.Click += new System.EventHandler(this.toolStripButtonWaveFormPause_Click);
             // 
             // toolStripButtonWaveFormPlay
@@ -2455,7 +2455,6 @@
             this.toolStripButtonWaveFormPlay.Name = "toolStripButtonWaveFormPlay";
             this.toolStripButtonWaveFormPlay.Size = new System.Drawing.Size(23, 22);
             this.toolStripButtonWaveFormPlay.Text = "toolStripButton1";
-            this.toolStripButtonWaveFormPlay.Visible = true;
             this.toolStripButtonWaveFormPlay.Click += new System.EventHandler(this.toolStripButtonWaveFormPlay_Click);
             // 
             // toolStripButtonLockCenter
