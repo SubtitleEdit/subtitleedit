103fbf4db4bc527e2cb21129d7c7d320c9cd30da
niksedk <nikse.dk@gmail.com>
2013-11-13 19:52:50 +0000

    Waveform now also has a custom shortcut for 'insert text at video position' (default = Alt+Insert)
    git-svn-id: https://subtitleedit.googlecode.com/svn/trunk@2181 99eadd0c-20b8-1223-b5c4-2a2b2df33de2

--- src/Logic/Settings.cs
+++ src/Logic/Settings.cs
@@ -754,6 +754,7 @@ namespace Nikse.SubtitleEdit.Logic
         public string MainAdjustSelected100MsBack { get; set; }
         public string MainInsertAfter { get; set; }
         public string MainTextBoxInsertAfter { get; set; }
+        public string MainWaveformInsertAfter { get; set; }
         public string MainInsertBefore { get; set; }
         public string MainMergeDialogue { get; set; }
         public string MainToogleFocus { get; set; }
@@ -840,6 +841,7 @@ namespace Nikse.SubtitleEdit.Logic
             MainAdjustSelected100MsBack = string.Empty;
             MainInsertAfter = "Alt+Insert";
             MainTextBoxInsertAfter = "Alt+Insert";
+            MainWaveformInsertAfter = "Alt+Insert";
             MainInsertBefore = "Control+Shift+Insert";
             MainMergeDialogue = string.Empty;
             WaveformVerticalZoom = string.Empty;
@@ -2141,6 +2143,9 @@ namespace Nikse.SubtitleEdit.Logic
                 subNode = node.SelectSingleNode("MainTextBoxInsertAfter");
                 if (subNode != null)
                     settings.Shortcuts.MainTextBoxInsertAfter = subNode.InnerText;
+                subNode = node.SelectSingleNode("MainWaveformInsertAfter");
+                if (subNode != null)
+                    settings.Shortcuts.MainWaveformInsertAfter = subNode.InnerText;               
                 subNode = node.SelectSingleNode("MainInsertBefore");
                 if (subNode != null)
                     settings.Shortcuts.MainInsertBefore = subNode.InnerText;
@@ -2676,6 +2681,7 @@ namespace Nikse.SubtitleEdit.Logic
             textWriter.WriteElementString("MainAdjustSelected100MsBack", settings.Shortcuts.MainAdjustSelected100MsBack);
             textWriter.WriteElementString("MainInsertAfter", settings.Shortcuts.MainInsertAfter);
             textWriter.WriteElementString("MainTextBoxInsertAfter", settings.Shortcuts.MainTextBoxInsertAfter);
+            textWriter.WriteElementString("MainWaveformInsertAfter", settings.Shortcuts.MainWaveformInsertAfter);
             textWriter.WriteElementString("MainInsertBefore", settings.Shortcuts.MainInsertBefore);
             textWriter.WriteElementString("MainMergeDialogue", settings.Shortcuts.MainMergeDialogue);
             textWriter.WriteElementString("MainToogleFocus", settings.Shortcuts.MainToogleFocus);
