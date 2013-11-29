7c57954c2b62576f41fa27cfdc4d7c7820af29d3
niksedk <nikse.dk@gmail.com>
2013-11-29 17:09:45 +0000

    Added Vietnamese language file - thx everytime :)
    git-svn-id: https://subtitleedit.googlecode.com/svn/trunk@2246 99eadd0c-20b8-1223-b5c4-2a2b2df33de2

--- src/Forms/AddWareForm.cs
+++ src/Forms/AddWareForm.cs
@@ -55,10 +55,8 @@ namespace Nikse.SubtitleEdit.Forms
             string targetFile = Path.GetTempFileName() + ".wav";
 //            string parameters = "-I dummy -vvv \"" + SourceVideoFileName + "\" --sout=#transcode{vcodec=none,acodec=s16l}:file{dst=\"" + targetFile + "\"}  vlc://quit";
       //      string parameters = "-I dummy -vvv --no-sout-video --audio-track=" + _audioTrackNumber.ToString() + " --sout #transcode{" + _encodeParamters + "}:std{mux=wav,access=file,dst=\"" + targetFile + "\"} \"" + SourceVideoFileName + "\" vlc://quit";
-            string parameters = "\"" + SourceVideoFileName + "\" -I dummy -vvv --no-sout-video --audio-track=" + _audioTrackNumber.ToString() + " --sout=\"#transcode{acodec=s16l,channels=1,ab=64,samplerate=8000}:std{access=file,mux=wav,dst=" + targetFile + "}\" vlc://quit";
-
-
-
+            string parameters = "\"" + SourceVideoFileName + "\" -I dummy -vvv --no-sout-video --audio-track=" + _audioTrackNumber.ToString() + " --sout=\"#transcode{acodec=s16l,channels=2,ab=128,samplerate=48000}:std{access=file,mux=wav,dst=" + targetFile + "}\" vlc://quit";
+                                                                                                                                                                                                // ,samplerate=48000
             string exeFilePath;
             if (Utilities.IsRunningOnLinux() || Utilities.IsRunningOnMac())
             {
