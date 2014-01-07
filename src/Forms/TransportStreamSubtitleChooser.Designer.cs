0d2da4fd16d8ed63d898468442c0df0a69f90a58
niksedk <nikse.dk@gmail.com>
2014-01-07 20:10:03 +0000

    Working on dvb sub
    git-svn-id: https://subtitleedit.googlecode.com/svn/trunk@2353 99eadd0c-20b8-1223-b5c4-2a2b2df33de2

--- src/Forms/TransportStreamSubtitleChooser.Designer.cs
+++ src/Forms/TransportStreamSubtitleChooser.Designer.cs
@@ -29,37 +29,40 @@
         private void InitializeComponent()
         {
             this.labelChoose = new System.Windows.Forms.Label();
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBoxTracks = new System.Windows.Forms.ListBox();
             this.buttonCancel = new System.Windows.Forms.Button();
             this.buttonOK = new System.Windows.Forms.Button();
+            this.listBoxSubtitles = new System.Windows.Forms.ListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // labelChoose
             // 
             this.labelChoose.AutoSize = true;
-            this.labelChoose.Location = new System.Drawing.Point(9, 21);
+            this.labelChoose.Location = new System.Drawing.Point(9, 15);
             this.labelChoose.Name = "labelChoose";
             this.labelChoose.Size = new System.Drawing.Size(220, 13);
             this.labelChoose.TabIndex = 52;
             this.labelChoose.Text = "More than one subtitle found - please choose";
             // 
-            // listBox1
+            // listBoxTracks
             // 
-            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
+            this.listBoxTracks.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(12, 44);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(548, 264);
-            this.listBox1.TabIndex = 51;
+            this.listBoxTracks.FormattingEnabled = true;
+            this.listBoxTracks.Location = new System.Drawing.Point(13, 31);
+            this.listBoxTracks.Name = "listBoxTracks";
+            this.listBoxTracks.Size = new System.Drawing.Size(589, 82);
+            this.listBoxTracks.TabIndex = 51;
+            this.listBoxTracks.SelectedIndexChanged += new System.EventHandler(this.listBoxTracks_SelectedIndexChanged);
             // 
             // buttonCancel
             // 
             this.buttonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.buttonCancel.BackColor = System.Drawing.SystemColors.Control;
             this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.buttonCancel.Location = new System.Drawing.Point(485, 312);
+            this.buttonCancel.Location = new System.Drawing.Point(526, 450);
             this.buttonCancel.Name = "buttonCancel";
             this.buttonCancel.Size = new System.Drawing.Size(75, 21);
             this.buttonCancel.TabIndex = 50;
@@ -70,7 +73,7 @@
             // buttonOK
             // 
             this.buttonOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.buttonOK.Location = new System.Drawing.Point(404, 312);
+            this.buttonOK.Location = new System.Drawing.Point(445, 450);
             this.buttonOK.Name = "buttonOK";
             this.buttonOK.Size = new System.Drawing.Size(75, 21);
             this.buttonOK.TabIndex = 49;
@@ -78,22 +81,50 @@
             this.buttonOK.UseVisualStyleBackColor = true;
             this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
             // 
+            // listBoxSubtitles
+            // 
+            this.listBoxSubtitles.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBoxSubtitles.FormattingEnabled = true;
+            this.listBoxSubtitles.Location = new System.Drawing.Point(12, 119);
+            this.listBoxSubtitles.Name = "listBoxSubtitles";
+            this.listBoxSubtitles.Size = new System.Drawing.Size(589, 199);
+            this.listBoxSubtitles.TabIndex = 53;
+            this.listBoxSubtitles.SelectedIndexChanged += new System.EventHandler(this.listBoxSubtitles_SelectedIndexChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(13, 324);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(588, 116);
+            this.pictureBox1.TabIndex = 54;
+            this.pictureBox1.TabStop = false;
+            // 
             // TransportStreamSubtitleChooser
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(572, 345);
+            this.ClientSize = new System.Drawing.Size(613, 483);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.listBoxSubtitles);
             this.Controls.Add(this.labelChoose);
-            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.listBoxTracks);
             this.Controls.Add(this.buttonCancel);
             this.Controls.Add(this.buttonOK);
             this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(600, 510);
             this.Name = "TransportStreamSubtitleChooser";
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Transport stream subtitle chooser";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.TransportStreamSubtitleChooser_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -102,8 +133,10 @@
         #endregion
 
         private System.Windows.Forms.Label labelChoose;
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBoxTracks;
         private System.Windows.Forms.Button buttonCancel;
         private System.Windows.Forms.Button buttonOK;
+        private System.Windows.Forms.ListBox listBoxSubtitles;
+        private System.Windows.Forms.PictureBox pictureBox1;
     }
 }
--- src/Forms/TransportStreamSubtitleChooser.cs
+++ src/Forms/TransportStreamSubtitleChooser.cs
@@ -11,6 +11,8 @@ namespace Nikse.SubtitleEdit.Forms
 {
     public partial class TransportStreamSubtitleChooser : Form
     {
+        Logic.TransportStream.TransportStreamParser _tsParser;
+
         public TransportStreamSubtitleChooser()
         {
             InitializeComponent();
@@ -50,20 +52,61 @@ namespace Nikse.SubtitleEdit.Forms
 
         internal void Initialize(Logic.TransportStream.TransportStreamParser tsParser)
         {
+            _tsParser = tsParser;
             foreach (int id in tsParser.SubtitlePacketIds)
             {
-                string s = string.Format(string.Format("Packet id {0}", id));
-                listBox1.Items.Add(s);
+                string s = string.Format(string.Format("Transport Packet Identifier (PID) = {0}, number of subtitles = {1}", id, tsParser.GetDvbSubtitles(id).Count));
+                listBoxTracks.Items.Add(s);
             }
-            listBox1.SelectedIndex = 0;
+            listBoxTracks.SelectedIndex = 0;
         }
 
         public int SelectedIndex
         {
             get
             {
-                return listBox1.SelectedIndex;
+                return listBoxTracks.SelectedIndex;
             }
+        }
+
+        private void listBoxTracks_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int idx = listBoxTracks.SelectedIndex;
+            if (idx < 0)
+                return;
+
+            listBoxSubtitles.Items.Clear();
+            int pid = _tsParser.SubtitlePacketIds[idx];
+            var list = _tsParser.GetDvbSubtitles(pid);
+            int i = 0;
+            foreach (var sub in list)
+            {
+                i++;
+                var tc = new TimeCode(TimeSpan.FromMilliseconds(sub.StartMilliseconds));
+                listBoxSubtitles.Items.Add(string.Format("{0}: {1}, {2} images", i, tc.ToString(), sub.Pes.ObjectDataList.Count));
+            }
+            if (list.Count > 0)
+                listBoxSubtitles.SelectedIndex = 0;
+        }
+
+        private void listBoxSubtitles_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int idx = listBoxSubtitles.SelectedIndex;
+            if (idx < 0)
+                return;
+
+            int pid = _tsParser.SubtitlePacketIds[listBoxTracks.SelectedIndex];
+            var list = _tsParser.GetDvbSubtitles(pid);
+
+            var dvbBmp = list[idx].Pes.GetImageFull();
+            NikseBitmap nDvbBmp = new NikseBitmap(dvbBmp);
+            nDvbBmp.CropTopTransparent(2);
+            nDvbBmp.CropSidesAndBottom(2, Color.Transparent, true);
+            dvbBmp.Dispose();
+            var oldImage = pictureBox1.Image;
+            pictureBox1.Image = nDvbBmp.GetBitmap();
+            if (oldImage != null)
+                oldImage.Dispose();
         }
 
     }
