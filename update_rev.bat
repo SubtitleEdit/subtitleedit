6f4bfec412aa5d8a043b286a36cb5f34cf723718
XhmikosR <xhmikosr@users.sourceforge.net>
2014-02-03 20:44:32 +0200

    Use the built UpdateAssemblyDescription.exe instead of the pre-built one.

--- update_rev.bat
+++ update_rev.bat
@@ -3,11 +3,7 @@ SETLOCAL
 
 PUSHD %~dp0
 
-IF EXIST "SubWCRev.exe" SET "SUBWCREV=SubWCRev.exe"
-FOR %%A IN (SubWCRev.exe) DO (SET SUBWCREV=%%~$PATH:A)
-IF NOT DEFINED SUBWCREV GOTO SubNoSubWCRev
-
-"%SUBWCREV%" . "src\Properties\AssemblyInfo.cs.in" "src\Properties\AssemblyInfo.cs" -f
+"src\UpdateAssemblyDescription\bin\Release\UpdateAssemblyDescription.exe" "src\Properties\AssemblyInfo.cs.template" "src\Properties\AssemblyInfo.cs"
 IF %ERRORLEVEL% NEQ 0 GOTO SubError
 
 
@@ -16,17 +12,6 @@ POPD
 ENDLOCAL
 EXIT /B
 
-
-:SubNoSubWCRev
-ECHO. & ECHO SubWCRev, which is part of TortoiseSVN, wasn't found!
-ECHO You should (re)install TortoiseSVN.
-GOTO SubCommon
-
 :SubError
 ECHO Something went wrong when generating the revision number.
-
-:SubCommon
-ECHO I'll use VERSION_REV=0 for now.
-
-TYPE "src\Properties\AssemblyInfo.cs.template" > "src\Properties\AssemblyInfo.cs"
-GOTO END
+EXIT /B
