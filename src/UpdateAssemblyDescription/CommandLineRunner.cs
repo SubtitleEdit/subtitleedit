803c3ca2d2c3783c003bb099c3f824247299afbb
nikse.dk <nikse.dk@gmail.com>
2014-02-03 20:43:53 +0200

    Add UpdateAssemblyDescription source to the repository.

---
+++ src/UpdateAssemblyDescription/CommandLineRunner.cs
@@ -0,0 +1,43 @@
+﻿using System.Diagnostics;
+
+namespace UpdateAssemblyDescription
+{
+    public class CommandLineRunner
+    {
+        public string Result { get; set; }
+
+        public bool RunCommandAndGetOutput(string command, string arguments, string workingFolder)
+        {
+            var p = new Process();
+            p.StartInfo.FileName = command;
+            p.StartInfo.Arguments = arguments;
+            p.StartInfo.WorkingDirectory = workingFolder;
+            p.StartInfo.UseShellExecute = false;
+            p.StartInfo.RedirectStandardInput = true;
+            p.StartInfo.RedirectStandardOutput = true;
+            p.StartInfo.CreateNoWindow = true;
+            p.OutputDataReceived += _mplayer_OutputDataReceived;
+
+            try
+            {
+                p.Start();
+            }
+            catch
+            {
+                return false;
+            }
+            p.BeginOutputReadLine(); // Async reading of output to prevent deadlock
+            if (p.WaitForExit(9000))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        void _mplayer_OutputDataReceived(object sender, DataReceivedEventArgs e)
+        {
+            if (e != null && e.Data != null)
+                Result = e.Data;
+        }
+    }
+}
