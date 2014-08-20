5a37f0a8350b9e063cca2a2492bdad5fe4b558c4
niksedk <nikse.dk@gmail.com>
2014-08-20 22:04:10 +0200

    Added video file extension mxf

--- src/Logic/Utilities.cs
+++ src/Logic/Utilities.cs
@@ -158,7 +158,7 @@ namespace Nikse.SubtitleEdit.Logic
 
         public static List<string> GetMovieFileExtensions()
         {
-            return new List<string> { ".avi", ".mkv", ".wmv", ".mpg", ".mpeg", ".divx", ".mp4", ".asf", ".flv", ".mov", ".m4v", ".vob", ".ogv", ".webm", ".ts", ".m2ts", ".avs" };
+            return new List<string> { ".avi", ".mkv", ".wmv", ".mpg", ".mpeg", ".divx", ".mp4", ".asf", ".flv", ".mov", ".m4v", ".vob", ".ogv", ".webm", ".ts", ".m2ts", ".avs", ".mxf" };
         }
 
         public static string GetVideoFileFilter(bool includeAudioFiles)
