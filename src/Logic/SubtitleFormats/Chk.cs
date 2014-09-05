930fd2f89efa89d0810a49b3e7d076919f3876e2
niksedk <nikse.dk@gmail.com>
2014-09-05 19:34:47 +0200

    Working on format CHK

--- src/Logic/SubtitleFormats/Chk.cs
+++ src/Logic/SubtitleFormats/Chk.cs
@@ -1,15 +1,17 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 
 namespace Nikse.SubtitleEdit.Logic.SubtitleFormats
 {
+
+    /// <summary>
+    /// .CHK subtitle file format - 128 bytes blocks, first byte in block is id (01==text)
+    /// </summary>
     public class Chk  : SubtitleFormat
     {
         private Encoding _codePage = Encoding.GetEncoding(850);
-        private string _languageId = "EN";
+        private string _languageId = "DEN"; // English
 
         public override string Extension
         {
@@ -30,8 +32,8 @@ namespace Nikse.SubtitleEdit.Logic.SubtitleFormats
         {
             if (fileName.ToLower().EndsWith(".chk"))
             {
-                //TODO: something like checking the first bytes...
-                return true;
+                var buffer = Utilities.ReadAllBytes(fileName);
+                return buffer.Length > 0 && buffer[0] == 0x1d;
             }
             return false;
         }
@@ -56,39 +58,17 @@ namespace Nikse.SubtitleEdit.Logic.SubtitleFormats
             }
         }
 
-        private Queue<Paragraph> _stack = new Queue<Paragraph>();
+        private Queue<Paragraph> _timeCodeQueue = new Queue<Paragraph>();
 
         private Paragraph ReadParagraph(byte[] buffer, int index)
         {
-            if (buffer[index] != 0x0a && buffer[index] != 1)
-            {
-                _stack = new Queue<Paragraph>();
-                for (int i = 0; i < 15; i++)
-                {
-                    int start = index + 2 + (i*8);
-                    int startBig = buffer[start + 3];
-                    int startFrame = buffer[start + 4];
-                    int endBig = buffer[start + 5];
-                    int endFrame = buffer[start + 6];
-                    Paragraph p = new Paragraph();
-                    //p.StartTime.TotalMilliseconds = startFrame * Configuration.Settings.General.CurrentFrameRate + startBig * 256;
-                    //p.EndTime.TotalMilliseconds = endFrame * Configuration.Settings.General.CurrentFrameRate + endBig * 256;
-                    p.StartTime.TotalMilliseconds = startFrame * Configuration.Settings.General.CurrentFrameRate + startBig * new TimeSpan(0, 43, 41, 600).TotalMilliseconds;
-                    p.EndTime.TotalMilliseconds = endFrame * Configuration.Settings.General.CurrentFrameRate + endBig * new TimeSpan(0, 43, 41, 600).TotalMilliseconds;
-                    _stack.Enqueue(p);
-                }
-
-            }
-            else if (buffer[index] != 1)
-            {
-                // time codes or what?
-
-            }
-            else // 01 = text
+            if (buffer[index] == 1) // text
             {
                 var sb = new StringBuilder();
                 int skipCount = 0;
                 int textLength = buffer[index + 2] - 11;
+                int start = index + 13;
+               
                 for (int i = 0; i <= textLength; i++)
                 {
                     if (skipCount > 0)
@@ -97,51 +77,121 @@ namespace Nikse.SubtitleEdit.Logic.SubtitleFormats
                     }
                     else if (buffer[index + 13 + i] == 0xFE)
                     {
+                        sb.Append(GetText(buffer, start, index + i + 13));
+                        start = index + 13 + i + 3;
                         skipCount = 2;
                         sb.AppendLine();
                     }
                     else if (buffer[index + 13 + i] == 0)
                     {
+                        sb.Append(GetText(buffer, start, index + i + 13));
                         break;
                     }
-                    else
+                    if (i == textLength)
                     {
-                        sb.Append(_codePage.GetString(buffer, index + 13 + i, 1));
+                        sb.Append(GetText(buffer, start, index + i + 13 + 1));
                     }
                 }
+                
                 Paragraph p;
-                if (_stack.Count > 0)
-                    p = _stack.Dequeue();
+                if (_timeCodeQueue.Count > 0)
+                    p = _timeCodeQueue.Dequeue();
                 else
                     p = new Paragraph();
                 p.Number = buffer[index + 3] * 256 + buffer[index + 4]; // Subtitle number
                 p.Text = sb.ToString();
-                if (p.Number == 0 && p.Text.StartsWith("LANG:D", StringComparison.Ordinal) && p.Text.Length > 8)
+                if (p.Number == 0 && p.Text.StartsWith("LANG:", StringComparison.Ordinal) && p.Text.Length > 8)
                 {
-                    _languageId = p.Text.Substring(6, 2);
-                }
-                else
-                {
-                    if (_languageId == "SP")
-                    {
-                        p.Text = p.Text.Replace("ÔA", "Á");
-                        p.Text = p.Text.Replace("ÔE", "É");
-                        p.Text = p.Text.Replace("ÔI", "Í");
-                        p.Text = p.Text.Replace("ÓN", "Ñ");
-                        p.Text = p.Text.Replace("ÔO", "Ó");
-                        p.Text = p.Text.Replace("ÔU", "Ú");
-
-                        p.Text = p.Text.Replace("Ôa", "á");
-                        p.Text = p.Text.Replace("Ôe", "é");
-                        p.Text = p.Text.Replace("Ôi", "í");
-                        p.Text = p.Text.Replace("Ón", "ñ");
-                        p.Text = p.Text.Replace("Ôo", "ó");
-                        p.Text = p.Text.Replace("Ôu", "ú");
-                    }
+                    _languageId = p.Text.Substring(5, 3);
                 }
                 return p;
             }
+            else if (buffer[index] == 0x0a)
+            {
+                // ?
+            }
+            else // time codes
+            {
+                _timeCodeQueue = new Queue<Paragraph>();
+                for (int i = 0; i < 15; i++)
+                {
+                    int start = index + 2 + (i * 8);
+
+                    int totalFrameNumber = buffer[start + 4];
+                    totalFrameNumber = buffer[start + 4];
+                    totalFrameNumber += buffer[start + 5] << 8;
+                    totalFrameNumber += buffer[start + 3] << 16; // ??
+                    
+                    int durationInFrames = buffer[start + 6];
+
+                    var p = new Paragraph(string.Empty, FramesToMilliseconds(totalFrameNumber), FramesToMilliseconds(totalFrameNumber + durationInFrames));
+                    _timeCodeQueue.Enqueue(p);
+                }
+            }
             return null;
+        }
+
+        private string GetText(byte[] buffer, int start, int end)
+        {
+            string text = string.Empty;
+            if (buffer[start] == 0x1f && buffer[start + 1] == 0x57 && buffer[start + 2] == 0x31 && buffer[start + 3] == 0x36) // W16
+            {
+                if (end - start > 4)
+                    text = System.Text.Encoding.GetEncoding(950).GetString(buffer, start + 4, end - start - 4);
+            }
+            else
+            {
+                if (end - start > 0)
+                    text = _codePage.GetString(buffer, start, end - start );
+            }
+
+            //if (_languageId == "DSP" || _languageId == "DFR")
+            {
+                text = text.Replace("ÔA", "Á");
+                text = text.Replace("ÔE", "É");
+                text = text.Replace("ÔI", "Í");
+                text = text.Replace("ÓN", "Ñ");
+                text = text.Replace("ÔO", "Ó");
+                text = text.Replace("ÔU", "Ú");
+
+                text = text.Replace("Ôa", "á");
+                text = text.Replace("Ôe", "é");
+                text = text.Replace("Ôi", "í");
+                text = text.Replace("Ón", "ñ");
+                text = text.Replace("Ôo", "ó");
+                text = text.Replace("Ôu", "ú");
+
+
+                text = text.Replace("ÒA", "À");
+                text = text.Replace("ÒE", "È");
+                text = text.Replace("ÒU", "Ù");
+
+                text = text.Replace("Òa", "à");
+                text = text.Replace("Òe", "è");
+                text = text.Replace("Òu", "ù");                
+            }
+            //if (_languageId == "DGE")
+            {
+                text = text.Replace("ÕU", "Ü");
+                text = text.Replace("ÕA", "Ä");
+                text = text.Replace("ÕO", "Ö");
+
+                text = text.Replace("Õu", "ü");
+                text = text.Replace("Õa", "ä");
+                text = text.Replace("Õo", "ö");
+
+
+                text = text.Replace("õa", "â");
+                text = text.Replace("õe", "ê");
+                text = text.Replace("õi", "î");
+                text = text.Replace("õu", "û");
+
+                text = text.Replace("õA", "Â");
+                text = text.Replace("õE", "Ê");
+                text = text.Replace("õI", "Î");
+                text = text.Replace("õU", "Û");
+            }
+            return text;
         }
 
     }
