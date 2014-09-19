f56998dba248dfcc656ffd6e889497ee55b7c0c3
niksedk <nikse.dk@gmail.com>
2014-09-19 16:01:44 +0200

    Added setting for Cheetah caption not to write end time if duration < 5 seconds

--- src/Logic/Settings.cs
+++ src/Logic/Settings.cs
@@ -264,6 +264,8 @@ namespace Nikse.SubtitleEdit.Logic
         public int CurrentCavena890LanguageIdLine1 { get; set; }
         public int CurrentCavena890LanguageIdLine2 { get; set; }
 
+        public bool CheetahCaptionLessThan5SecondsNoEndTime { get; set; }
+
         public bool SamiDisplayTwoClassesAsTwoSubtitles { get; set; }
         public int SamiHtmlEncodeMode { get; set; }
 
@@ -1785,6 +1787,9 @@ namespace Nikse.SubtitleEdit.Logic
                 subNode = node.SelectSingleNode("FcpFontName");
                 if (subNode != null)
                     settings.SubtitleSettings.FcpFontName = subNode.InnerText;
+                subNode = node.SelectSingleNode("CheetahCaptionLessThan5SecondsNoEndTime");
+                if (subNode != null)
+                    settings.SubtitleSettings.CheetahCaptionLessThan5SecondsNoEndTime = Convert.ToBoolean(subNode.InnerText);                
                 subNode = node.SelectSingleNode("NuendoCharacterListFile");
                 if (subNode != null)
                     settings.SubtitleSettings.NuendoCharacterListFile = subNode.InnerText;
@@ -2814,6 +2819,7 @@ namespace Nikse.SubtitleEdit.Logic
                 textWriter.WriteElementString("TimedText10TimeCodeFormat", settings.SubtitleSettings.TimedText10TimeCodeFormat);
                 textWriter.WriteElementString("FcpFontSize", settings.SubtitleSettings.FcpFontSize.ToString(CultureInfo.InvariantCulture));
                 textWriter.WriteElementString("FcpFontName", settings.SubtitleSettings.FcpFontName);
+                textWriter.WriteElementString("CheetahCaptionLessThan5SecondsNoEndTime", settings.SubtitleSettings.CheetahCaptionLessThan5SecondsNoEndTime.ToString(CultureInfo.InvariantCulture));
                 textWriter.WriteElementString("NuendoCharacterListFile", settings.SubtitleSettings.NuendoCharacterListFile);
                 textWriter.WriteEndElement();
 
