270d08b2ee9df0bd40bcc1236e64126aa62ef2a2
XhmikosR <xhmikosr@gmail.com>
2014-09-20 13:05:34 +0300

    Update LanguageDeserializer.cs

--- src/Logic/LanguageDeserializer.cs
+++ src/Logic/LanguageDeserializer.cs
@@ -1024,7 +1024,7 @@ namespace Nikse.SubtitleEdit.Logic
                 case "EffectTypewriter/TotalMilliseconds":
                     language.EffectTypewriter.TotalMilliseconds = reader.Value;
                     break;
-                case "EffectTypewriter/EndDelayInMillisecs":
+                case "EffectTypewriter/EndDelayInMilliseconds":
                     language.EffectTypewriter.EndDelayInMilliseconds = reader.Value;
                     break;
                 case "ExportCustomText/Title":
