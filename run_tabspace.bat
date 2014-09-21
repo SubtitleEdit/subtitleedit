1cdd3bcf3082f7cbab22545b264fa5dfba0e13b9
XhmikosR <xhmikosr@gmail.com>
2014-09-21 11:19:59 +0300

    Update "run_tabspace.bat" after the NuGet packages addition.

--- run_tabspace.bat
+++ run_tabspace.bat
@@ -3,7 +3,7 @@ SETLOCAL
 
 PUSHD %~dp0
 
-tabspace /ext:bat;c;cc;cpp;cs;cxx;h;hpp;hxx;iss;xml /exclude:*\*.designer.cs;*\zlib\*;*Logic\NHunspell\*
+tabspace /ext:bat;c;cc;cpp;cs;cxx;h;hpp;hxx;iss;xml /exclude:*\*.designer.cs
 
 POPD
 ENDLOCAL
