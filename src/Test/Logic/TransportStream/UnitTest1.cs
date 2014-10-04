3d463eecec5912588bd124faafb33328b78c6cd3
niksedk <nikse.dk@gmail.com>
2014-10-04 20:48:15 +0200

    Added first unit test with external file

---
+++ src/Test/Logic/TransportStream/UnitTest1.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+
+namespace Test.Logic.TransportStream
+{
+    [TestClass]
+    [DeploymentItem("Files")]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TransportStreamTest()
+        {
+            string fileName = Path.Combine(Directory.GetCurrentDirectory(), "sample_TS_with_graphics.ts");
+            var parser = new Nikse.SubtitleEdit.Logic.TransportStream.TransportStreamParser();
+            parser.ParseTSFile(fileName);
+            var subtitles = parser.GetDvbSubtitles(41);
+
+            Assert.IsTrue(subtitles.Count == 10);
+            Assert.IsTrue(subtitles[0].Pes.GetImageFull().Width == 719);
+            Assert.IsTrue(subtitles[0].Pes.GetImageFull().Height == 575);
+        }
+    }
+}
