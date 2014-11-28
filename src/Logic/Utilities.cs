6963dae25a9d947347e83ba3ba5ebfcf745ca1b0
Ivandro Ismael <ivandrofly@gmail.com>
2014-11-28 11:11:12 +0000

    Some refact in 'Utilities' & possible '|' bug fixed

--- src/Logic/Utilities.cs
+++ src/Logic/Utilities.cs
@@ -1889,8 +1889,7 @@ namespace Nikse.SubtitleEdit.Logic
         public static void GetLineLengths(Label label, string text)
         {
             label.ForeColor = Color.Black;
-            string cleanText = RemoveHtmlTags(text, true).Replace(Environment.NewLine, "|");
-            string[] lines = cleanText.Split('|');
+            var lines = Utilities.RemoveHtmlTags(text, true).Replace(Environment.NewLine, "\n").Split('\n');
 
             const int max = 3;
 
