bd5a796015ecaa182024d0d154d62bfa14692887
Michał Kępień <github@kempniu.pl>
2014-12-11 10:28:00 +0100

    Trim paragraph end time to split time when splitting subtitles

--- src/Forms/SplitSubtitle.cs
+++ src/Forms/SplitSubtitle.cs
@@ -91,6 +91,8 @@ namespace Nikse.SubtitleEdit.Forms
                     }
                     else if (p.EndTime.TotalMilliseconds > splitTime.TotalMilliseconds)
                     {
+                        Paragraph p1 = part1.Paragraphs[part1.Paragraphs.Count - 1];
+                        p1.EndTime = new TimeCode(splitTime.TotalMilliseconds);
                         Paragraph p2 = new Paragraph(p);
                         p2.StartTime = new TimeCode(splitTime.TotalMilliseconds);
                         part2.Paragraphs.Add(p2);
