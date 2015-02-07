11c457317160ad376a37cf600c6f0347a3f03934
ivandrofly <ivandrofly@gmail.com>
2015-02-07 00:19:07 +0000

    Fixed typos + Refact

--- src/Logic/Forms/FixCommonErrorsHelper.cs
+++ src/Logic/Forms/FixCommonErrorsHelper.cs
@@ -31,10 +31,7 @@ namespace Nikse.SubtitleEdit.Logic.Forms
             // "...foobar" / "... foobar"
             if (text.StartsWith("\"..", StringComparison.Ordinal))
             {
-                while (text.Length > 1 && text[1] == '.')
-                    text = text.Remove(1, 1);
-                if (text.Length > 1 && text[1] == ' ')
-                    text = text.Remove(1, 1);
+                text = "\"" + text.Remove(0, 1).TrimStart('.', ' ');
             }
 
             text = text.Replace("-..", "- ..");
@@ -312,7 +309,7 @@ namespace Nikse.SubtitleEdit.Logic.Forms
             return text;
         }
 
-        public static string RemoveSpacesBeingLineAfterEllipese(string line)
+        public static string RemoveSpacesBeginLineAfterEllipses(string line)
         {
             if (line.StartsWith("... ", StringComparison.Ordinal))
                 line = line.Remove(3, 1);
@@ -321,13 +318,12 @@ namespace Nikse.SubtitleEdit.Logic.Forms
                 var idx = line.IndexOf('>') + 1;
                 var pre = line.Substring(0, idx);
                 line = line.Remove(0, idx).TrimStart();
-                if(line.StartsWith("... "))
+                if (line.StartsWith("... ", StringComparison.Ordinal))
                     line = line.Remove(3, 1);
                 line = pre + line;
             }
             if (line.LineStartsWithHtmlTag(false, true)) //<font color="#000000"> and <font>
             {
-                // Todo: what if line number is > 1
                 var closeIdx = line.IndexOf('>', 5);
                 if (closeIdx >= 5 && line.Length > closeIdx + 5)
                 {
