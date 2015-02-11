d0079754d3f7f3f616e2b7bdcc62766e2b3db987
Waldi Ravens <wr86420@gmail.com>
2015-02-11 22:01:49 +0100

    Formatting

--- src/Core/FileUtil.cs
+++ src/Core/FileUtil.cs
@@ -105,13 +105,13 @@ namespace Nikse.SubtitleEdit.Core
                 if (count != buffer.Length)
                     return false;
                 return buffer[0] == 137
-                       && buffer[1] == 80
-                       && buffer[2] == 78
-                       && buffer[3] == 71
-                       && buffer[4] == 13
-                       && buffer[5] == 10
-                       && buffer[6] == 26
-                       && buffer[7] == 10;
+                    && buffer[1] == 80
+                    && buffer[2] == 78
+                    && buffer[3] == 71
+                    && buffer[4] == 13
+                    && buffer[5] == 10
+                    && buffer[6] == 26
+                    && buffer[7] == 10;
             }
         }
 
@@ -124,8 +124,8 @@ namespace Nikse.SubtitleEdit.Core
                 if (count != buffer.Length)
                     return false;
                 return buffer[0] == 0x69
-                       && buffer[1] == 0x69
-                       && buffer[2] == 0x69;
+                    && buffer[1] == 0x69
+                    && buffer[2] == 0x69;
             }
         }
 
