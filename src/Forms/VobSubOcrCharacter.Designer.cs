4b180b6d6eed9487a11362d50689c5edc92b2469
ivandrofly <ivandrofly@gmail.com>
2015-02-17 14:11:44 +0000

    Fixed: DataView now anchored properly

--- src/Forms/VobSubOcrCharacter.Designer.cs
+++ src/Forms/VobSubOcrCharacter.Designer.cs
@@ -1046,6 +1046,8 @@
             this.dataGridView1.AllowUserToDeleteRows = false;
             this.dataGridView1.AllowUserToResizeColumns = false;
             this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.ColumnHeadersVisible = false;
