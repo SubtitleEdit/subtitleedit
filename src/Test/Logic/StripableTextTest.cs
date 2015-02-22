577585839d3fb918d25567f8e947a687af023061
niksedk <nikse.dk@gmail.com>
2015-02-22 22:16:41 +0100

    Added a few unit tests

---
+++ src/Test/Logic/StripableTextTest.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nikse.SubtitleEdit.Logic;
+
+namespace Test.Logic
+{
+    [TestClass]
+    public class StripableTextTest
+    {
+        [TestMethod]
+        public void StripableTextItalic()
+        {
+            var st = new StripableText("<i>Hi!</i>");
+            Assert.AreEqual(st.Pre, "<i>");
+            Assert.AreEqual(st.Post, "!</i>");
+            Assert.AreEqual(st.StrippedText, "Hi");
+        }
+
+        [TestMethod]
+        public void StripableTextAss()
+        {
+            var st = new StripableText("{\\an9}Hi!");
+            Assert.AreEqual(st.Pre, "{\\an9}");
+            Assert.AreEqual(st.Post, "!");
+            Assert.AreEqual(st.StrippedText, "Hi");
+        }
+
+        [TestMethod]
+        public void StripableTextFont()
+        {
+            var st = new StripableText("<font color=\"red\">Hi!</font>");
+            Assert.AreEqual(st.Pre, "<font color=\"red\">");
+            Assert.AreEqual(st.Post, "!</font>");
+            Assert.AreEqual(st.StrippedText, "Hi");
+        }
+
+    }
+}
