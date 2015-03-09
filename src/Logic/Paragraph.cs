48ee6c27e1b551ff4577ba96d516138251a22921
ivandrofly <ivandrofly@gmail.com>
2015-03-09 19:40:56 +0000

    Refact | Optimization

--- src/Logic/Paragraph.cs
+++ src/Logic/Paragraph.cs
@@ -144,7 +144,7 @@ namespace Nikse.SubtitleEdit.Logic
             {
                 if (string.IsNullOrEmpty(Text))
                     return 0;
-                int wordCount = HtmlUtil.RemoveHtmlTags(Text).Split((" ,.!?;:()[]" + Environment.NewLine).ToCharArray(), StringSplitOptions.RemoveEmptyEntries).Length;
+                int wordCount = HtmlUtil.RemoveHtmlTags(Text, true).Split(new[] { ' ', ',', '.', '!', '?', ';', ':', '(', ')', '[', ']', '\n', '\r' }, StringSplitOptions.RemoveEmptyEntries).Length;
                 return (60.0 / Duration.TotalSeconds) * wordCount;
             }
         }
