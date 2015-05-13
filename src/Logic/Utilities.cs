f7dfae86d514869d1bf8d71ab71241153cc9efeb
ivandrofly <ivandrofly@gmail.com>
2015-05-13 16:12:21 +0100

    Minor refact in index checking

--- src/Logic/Utilities.cs
+++ src/Logic/Utilities.cs
@@ -2061,12 +2061,12 @@ namespace Nikse.SubtitleEdit.Logic
         public static string GetRegExGroup(string regEx)
         {
             int start = regEx.IndexOf("(?<", StringComparison.Ordinal);
-            if (start >= 0 && regEx.IndexOf(')') > start)
+            if (start >= 0 && regEx.IndexOf(')', start + 3) > start)
             {
+                start += 3;
                 int end = regEx.IndexOf('>', start);
                 if (end > start)
                 {
-                    start += 3;
                     return regEx.Substring(start, end - start);
                 }
             }
@@ -2122,9 +2122,10 @@ namespace Nikse.SubtitleEdit.Logic
             while (index >= 0)
             {
                 count++;
-                if ((index + 1) == text.Length)
+                index++;
+                if (index == text.Length)
                     return count;
-                index = text.IndexOf(tag, index + 1);
+                index = text.IndexOf(tag, index);
             }
             return count;
         }
