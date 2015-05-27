da07b9b50c63f6ee8c5186b407d45d106388c97d
Waldi Ravens <wr86420@gmail.com>
2015-05-27 08:20:06 +0200

    Use Utilities.FixLargeFonts (Forms/ChooseStyle)

--- src/Forms/ChooseStyle.cs
+++ src/Forms/ChooseStyle.cs
@@ -33,18 +33,7 @@ namespace Nikse.SubtitleEdit.Forms
             buttonCancel.Text = Configuration.Settings.Language.General.Cancel;
 
             InitializeListView();
-            FixLargeFonts();
-        }
-
-        private void FixLargeFonts()
-        {
-            Graphics graphics = this.CreateGraphics();
-            SizeF textSize = graphics.MeasureString(buttonOK.Text, this.Font);
-            if (textSize.Height > buttonOK.Height - 4)
-            {
-                int newButtonHeight = (int)(textSize.Height + 7 + 0.5);
-                Utilities.SetButtonHeight(this, newButtonHeight, 1);
-            }
+            Utilities.FixLargeFonts(this, buttonOK);
         }
 
         private void ChooseStyle_KeyDown(object sender, KeyEventArgs e)
