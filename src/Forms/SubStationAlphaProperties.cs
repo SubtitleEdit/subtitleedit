1019a521992dd1e5a205304f96b30dbb1180288c
Waldi Ravens <wr86420@gmail.com>
2015-05-27 08:41:27 +0200

    Use Utilities.FixLargeFonts (Forms/SubStationAlphaProperties)

--- src/Forms/SubStationAlphaProperties.cs
+++ src/Forms/SubStationAlphaProperties.cs
@@ -137,18 +137,7 @@ namespace Nikse.SubtitleEdit.Forms
             buttonOK.Text = Configuration.Settings.Language.General.Ok;
             buttonCancel.Text = Configuration.Settings.Language.General.Cancel;
 
-            FixLargeFonts();
-        }
-
-        private void FixLargeFonts()
-        {
-            Graphics graphics = CreateGraphics();
-            SizeF textSize = graphics.MeasureString(buttonCancel.Text, Font);
-            if (textSize.Height > buttonCancel.Height - 4)
-            {
-                int newButtonHeight = (int)(textSize.Height + 7 + 0.5);
-                Utilities.SetButtonHeight(this, newButtonHeight, 1);
-            }
+            Utilities.FixLargeFonts(this, buttonCancel);
         }
 
         private void buttonCancel_Click(object sender, EventArgs e)
