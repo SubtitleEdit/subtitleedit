427a5db9d948a66a873d5de37c2ff5dd7b2de7a9
Waldi Ravens <wr86420@gmail.com>
2015-05-28 13:51:30 +0200

    Added Nikse.SubtitleEdit.Properties.Resources.SubtitleEditFormIcon <== Icons/SE.ico

--- src/Properties/Resources.Designer.cs
+++ src/Properties/Resources.Designer.cs
@@ -81,6 +81,16 @@ namespace Nikse.SubtitleEdit.Properties {
         }
         
         /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Icon similar to (Icon).
+        /// </summary>
+        public static System.Drawing.Icon SubtitleEditFormIcon {
+            get {
+                object obj = ResourceManager.GetObject("SubtitleEditFormIcon", resourceCulture);
+                return ((System.Drawing.Icon)(obj));
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
         public static System.Drawing.Bitmap TransparentBackground {
