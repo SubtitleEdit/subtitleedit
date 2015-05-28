05ebc2f6527119073a5b75dd1006efa87a52eddf
Waldi Ravens <wr86420@gmail.com>
2015-05-28 13:43:36 +0200

    Reset Main.Icon

--- src/Forms/Main.Designer.cs
+++ src/Forms/Main.Designer.cs
@@ -4471,7 +4471,6 @@
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.menuStrip1);
             this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.KeyPreview = true;
             this.MainMenuStrip = this.menuStrip1;
             this.MinimumSize = new System.Drawing.Size(800, 554);
