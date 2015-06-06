1b0ae146b18bc7c74f2d16527f6b235234413d56
ivandrofly <ivandrofly@gmail.com>
2015-06-06 21:18:25 +0100

    Show friendly msg when user try to drop JPG file

--- src/Logic/LanguageStructure.cs
+++ src/Logic/LanguageStructure.cs
@@ -1055,6 +1055,7 @@
             public string ErrorLoadRar { get; set; }
             public string ErrorLoadZip { get; set; }
             public string ErrorLoadPng { get; set; }
+            public string ErrorLoadJpg { get; set; }
             public string ErrorLoadSrr { get; set; }
             public string ErrorLoadTorrent { get; set; }
             public string ErrorLoadBinaryZeroes { get; set; }
