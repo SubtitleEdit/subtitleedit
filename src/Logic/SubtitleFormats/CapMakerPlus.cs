77c5a5018cc67aa3d4bcafd2c96d17f7a5283690
ivandrofly <ivandrofly@gmail.com>
2015-06-23 20:49:23 +0000

    Add readonly modifier for regex

--- src/Logic/SubtitleFormats/CapMakerPlus.cs
+++ src/Logic/SubtitleFormats/CapMakerPlus.cs
@@ -10,7 +10,7 @@ namespace Nikse.SubtitleEdit.Logic.SubtitleFormats
     public class CapMakerPlus : SubtitleFormat
     {
 
-        private static Regex regexTimeCodes = new Regex(@"^\d\d:\d\d:\d\d:\d\d$", RegexOptions.Compiled);
+        private static readonly Regex regexTimeCodes = new Regex(@"^\d\d:\d\d:\d\d:\d\d$", RegexOptions.Compiled);
 
         public override string Extension
         {
