909246334952a95466f7f1900b41a26535d73ea9
Nikolaj Olsson <nikse.dk@gmail.com>
2015-08-27 08:42:29 +0200

    Make Xif public

--- libse/SubtitleFormats/Xif.cs
+++ libse/SubtitleFormats/Xif.cs
@@ -6,7 +6,7 @@ using System.Xml;
 
 namespace Nikse.SubtitleEdit.Core.SubtitleFormats
 {
-    class Xif : SubtitleFormat
+    public class Xif : SubtitleFormat
     {
         public override string Extension
         {
