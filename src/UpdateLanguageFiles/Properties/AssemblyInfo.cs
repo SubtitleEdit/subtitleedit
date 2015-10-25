66fead5e78764e69def09364a9d0e7b95f051667
Waldi Ravens <wr86420@gmail.com>
2015-10-25 16:56:02 +0200

    Update AssemblyInfo.cs (UpdateLanguageFiles)

--- src/UpdateLanguageFiles/Properties/AssemblyInfo.cs
+++ src/UpdateLanguageFiles/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("UpdateAssemblyDescription")]
+[assembly: AssemblyTitle("UpdateLanguageFiles")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("UpdateAssemblyDescription")]
+[assembly: AssemblyProduct("UpdateLanguageFiles")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
