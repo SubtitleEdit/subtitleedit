d12fba21f1717e3274e5ef6a729a8247727570d5
Waldi Ravens <wr86420@gmail.com>
2015-10-27 13:27:32 +0200

    Moved SubtitleEdit.srt.ico to Win32Resources and adjusted SE-installer accordingly

---
+++ src/Win32Resources/Resources.h
@@ -0,0 +1,12 @@
+﻿//
+//  Resource identifiers for Resources.rc and Subtitle_Edit_installer.iss
+//
+// All macros must be suitable for both Resource Compiler and Inno Preprocessor!
+//
+
+#define RC_APPNAME          10000
+#define RC_APPDESC          10001
+
+#define RCICON_SRT          101
+#define RCDESC_SRT          10101
+#define RCDESC_SRT_DEFAULT  "SubRip Text subtitles"
