51490e0e442e22ddaacd0e5b60b21f56c9d637ae
Waldi Ravens <wr86420@gmail.com>
2015-11-02 12:40:31 +0100

    SE-installer: Added .sup to the file types supported by SE

--- src/Win32Resources/Resources.h
+++ src/Win32Resources/Resources.h
@@ -10,3 +10,7 @@
 #define RCICON_SRT          101
 #define RCDESC_SRT          10101
 #define RCDESC_SRT_DEFAULT  "SubRip Text subtitles"
+
+#define RCICON_SUP          102
+#define RCDESC_SUP          10102
+#define RCDESC_SUP_DEFAULT  "Blu-ray PGS subtitles"
