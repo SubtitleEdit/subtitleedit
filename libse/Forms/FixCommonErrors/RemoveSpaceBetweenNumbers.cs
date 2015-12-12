5fca2f8ca82fb4838f15e396bf558c27e8660855
Waldi Ravens <wr86420@gmail.com>
2015-12-12 12:03:14 +0100

    Define one single RemoveSpaceBetweenNumbers method i/o two

--- libse/Forms/FixCommonErrors/RemoveSpaceBetweenNumbers.cs
+++ libse/Forms/FixCommonErrors/RemoveSpaceBetweenNumbers.cs
@@ -5,42 +5,24 @@ namespace Nikse.SubtitleEdit.Core.Forms.FixCommonErrors
 {
     public class RemoveSpaceBetweenNumbers : IFixCommonError
     {
-
-        private static readonly Regex RemoveSpaceBetweenNumbersRegEx = new Regex(@"\d \d", RegexOptions.Compiled);
-
         public void Fix(Subtitle subtitle, IFixCallbacks callbacks)
         {
             var language = Configuration.Settings.Language.FixCommonErrors;
-            string fixAction = language.RemoveSpaceBetweenNumber;
+            var fixAction = language.RemoveSpaceBetweenNumber;
             int noOfFixes = 0;
             for (int i = 0; i < subtitle.Paragraphs.Count; i++)
             {
-                Paragraph p = subtitle.Paragraphs[i];
-                string text = p.Text;
-                Match match = RemoveSpaceBetweenNumbersRegEx.Match(text);
-                int counter = 0;
-                while (match.Success && counter < 100 && text.Length > match.Index + 1)
+                var p = subtitle.Paragraphs[i];
+                if (callbacks.AllowFix(p, fixAction))
                 {
-                    string temp = text.Substring(match.Index + 2);
-                    if (temp != "1/2" &&
-                        !temp.StartsWith("1/2 ", StringComparison.Ordinal) &&
-                        !temp.StartsWith("1/2.", StringComparison.Ordinal) &&
-                        !temp.StartsWith("1/2!", StringComparison.Ordinal) &&
-                        !temp.StartsWith("1/2?", StringComparison.Ordinal) &&
-                        !temp.StartsWith("1/2<", StringComparison.Ordinal))
+                    var text = Utilities.RemoveSpaceBetweenNumbers(p.Text);
+                    if (text != p.Text)
                     {
-                        text = text.Remove(match.Index + 1, 1);
+                        var oldText = p.Text;
+                        p.Text = text;
+                        noOfFixes++;
+                        callbacks.AddFixToListView(p, fixAction, oldText, p.Text);
                     }
-                    if (text.Length > match.Index + 1)
-                        match = RemoveSpaceBetweenNumbersRegEx.Match(text, match.Index + 2);
-                    counter++;
-                }
-                if (callbacks.AllowFix(p, fixAction) && p.Text != text)
-                {
-                    string oldText = p.Text;
-                    p.Text = text;
-                    noOfFixes++;
-                    callbacks.AddFixToListView(p, fixAction, oldText, p.Text);
                 }
             }
             callbacks.UpdateFixStatus(noOfFixes, language.FixCommonOcrErrors, string.Format(language.RemoveSpaceBetweenNumbersFixed, noOfFixes));
