5ea0bb5601af5f3ab699b6e247b6cf0cfa179007
niksedk <nikse.dk@gmail.com>
2016-01-13 21:20:22 +0100

    Export TextST can now "Duplicate" a "Region style"

--- libse/SubtitleFormats/TextST.cs
+++ libse/SubtitleFormats/TextST.cs
@@ -85,6 +85,34 @@ namespace Nikse.SubtitleEdit.Core.SubtitleFormats
 
         public class RegionStyle
         {
+            public RegionStyle()
+            {
+            }
+
+            public RegionStyle(RegionStyle regionStyle)
+            {
+                RegionStyleId = regionStyle.RegionStyleId;
+                RegionHorizontalPosition = regionStyle.RegionHorizontalPosition;
+                RegionVerticalPosition = regionStyle.RegionVerticalPosition;
+                RegionWidth = regionStyle.RegionWidth;
+                RegionHeight = regionStyle.RegionHeight;
+                RegionBgPaletteEntryIdRef = regionStyle.RegionBgPaletteEntryIdRef;
+                TextBoxHorizontalPosition = regionStyle.TextBoxHorizontalPosition;
+                TextBoxVerticalPosition = regionStyle.TextBoxVerticalPosition;
+                TextBoxWidth = regionStyle.TextBoxWidth;
+                TextBoxHeight = regionStyle.TextBoxHeight;
+                TextFlow = regionStyle.TextFlow;
+                TextHorizontalAlignment = regionStyle.TextHorizontalAlignment;
+                TextVerticalAlignment = regionStyle.TextVerticalAlignment;
+                LineSpace = regionStyle.LineSpace;
+                FontIdRef = regionStyle.FontIdRef;
+                FontStyle = regionStyle.FontStyle;
+                FontSize = regionStyle.FontSize;
+                FontPaletteEntryIdRef = regionStyle.FontPaletteEntryIdRef;
+                FontOutlinePaletteEntryIdRef = regionStyle.FontOutlinePaletteEntryIdRef;
+                FontOutlineThickness = regionStyle.FontOutlineThickness;
+            }
+
             public int RegionStyleId { get; set; }
             public int RegionHorizontalPosition { get; set; }
             public int RegionVerticalPosition { get; set; }
@@ -231,6 +259,9 @@ namespace Nikse.SubtitleEdit.Core.SubtitleFormats
 
             public void WriteToStream(Stream stream, int numberOfSubtitles)
             {
+                NumberOfRegionStyles = RegionStyles.Count;
+                NumberOfUserStyles = UserStyles.Count;
+
                 byte[] regionStyle = MakeRegionStyle();
                 stream.Write(new byte[] { 0, 0, 1, 0xbf }, 0, 4); // MPEG-2 Private stream 2
                 var size = regionStyle.Length + 5;
--- src/Forms/ExportTextST.cs
+++ src/Forms/ExportTextST.cs
@@ -618,6 +618,10 @@ namespace Nikse.SubtitleEdit.Forms
                     {
                         contextMenuStripAddSubtitleContent.Show(treeView1, p);
                     }
+                    else if (node.Tag is TextST.RegionStyle)
+                    {
+                        contextMenuStripRegionStyle.Show(treeView1, p);
+                    }
                 }
             }
         }
@@ -789,6 +793,27 @@ namespace Nikse.SubtitleEdit.Forms
         private void numericUpDownFontSetFontId_ValueChanged(object sender, EventArgs e)
         {
             _currentSubtitleFontSet.FontId = GetIntFromNumericUpDown(sender);
+        }
+
+        private void toolStripMenuItemDuplicateRegionStyle_Click(object sender, EventArgs e)
+        {
+            var regionStyle = _currentNode.Tag as TextST.RegionStyle;
+            if (regionStyle != null)
+            {
+                int regionStyleId = 0;
+                foreach (var rs in _textST.StyleSegment.RegionStyles)
+                {
+                    regionStyleId = Math.Max(regionStyleId, rs.RegionStyleId);
+                }
+                regionStyleId++;
+
+                var newRegionStyle = new TextST.RegionStyle(regionStyle) { RegionStyleId = regionStyleId };
+                _textST.StyleSegment.RegionStyles.Add(newRegionStyle);
+                var newNode = new TreeNode("Region style") { Tag = newRegionStyle };
+                _currentNode.Parent.Nodes.Add(newNode);
+                _currentNode.Parent.Text = "Region styles (" + _textST.StyleSegment.RegionStyles.Count + ")";
+                treeView1.SelectedNode = newNode;
+            }
         }
 
     }
