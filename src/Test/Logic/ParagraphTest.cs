8e7a395a00674bf4ae19b9d9a7c518f6bd341f9d
niksedk <nikse.dk@gmail.com>
2016-01-21 20:04:43 +0100

    Minor refact + unit tests

--- src/Test/Logic/ParagraphTest.cs
+++ src/Test/Logic/ParagraphTest.cs
@@ -35,6 +35,29 @@ namespace Test.Logic
             Assert.AreEqual(0, p.NumberOfLines);
         }
 
+        [TestMethod]
+        public void TestAdjust1()
+        {
+            var p = new Paragraph { Text = string.Empty, StartTime = new TimeCode(0,1,1,1) };
+            p.Adjust(1, 10);
+            Assert.AreEqual(new TimeCode(0,1,11,1).TotalMilliseconds, p.StartTime.TotalMilliseconds);
+        }
+
+        [TestMethod]
+        public void TestAdjust2()
+        {
+            var p = new Paragraph { Text = string.Empty, StartTime = new TimeCode(0, 1, 1, 1) };
+            p.Adjust(2, 10);
+            Assert.AreEqual(new TimeCode(0, 2, 12, 2).TotalMilliseconds, p.StartTime.TotalMilliseconds);
+        }
+
+        [TestMethod]
+        public void TestAdjust3()
+        {
+            var p = new Paragraph { Text = string.Empty, StartTime = new TimeCode(0, 0, 0, 0) };
+            p.Adjust(1, 1);
+            Assert.AreEqual(1, p.StartTime.TotalSeconds);
+        }
 
     }
 }
