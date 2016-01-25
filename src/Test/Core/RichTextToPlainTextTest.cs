11723921d11921ad9fb13e37ea3a722f566cd45d
niksedk <nikse.dk@gmail.com>
2016-01-25 16:25:22 +0100

    Added a few unit tests

---
+++ src/Test/Core/RichTextToPlainTextTest.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nikse.SubtitleEdit.Core;
+
+namespace Test.Core
+{
+    [TestClass]
+    public class RichTextToPlainTextTest
+    {
+        [TestMethod]
+        public void TestConvertToRtfSlash()
+        {
+            var result = RichTextToPlainText.ConvertToRtf(@"Brian\Benny!");
+            Assert.IsTrue(result.Contains(@"Brian\\Benny!"));
+            result = RichTextToPlainText.ConvertToText(result);
+            Assert.IsTrue(result.Trim() == @"Brian\Benny!");
+        }
+
+        [TestMethod]
+        public void TestConvertToRtfCurlyBracketStart()
+        {
+            var result = RichTextToPlainText.ConvertToRtf(@"Brian{Benny!");
+            Assert.IsTrue(result.Contains(@"Brian\{Benny!"));
+            result = RichTextToPlainText.ConvertToText(result);
+            Assert.IsTrue(result.Trim() == @"Brian{Benny!");
+        }
+
+        [TestMethod]
+        public void TestConvertToRtfCurlyBracketEnd()
+        {
+            var result = RichTextToPlainText.ConvertToRtf(@"Brian}Benny!");
+            Assert.IsTrue(result.Contains(@"Brian\}Benny!"));
+            result = RichTextToPlainText.ConvertToText(result);
+            Assert.IsTrue(result.Trim() == @"Brian}Benny!");
+        }
+
+    }
+}
