cb9a68718975246f4fff87459700694eb56c3d2a
niksedk <nikse.dk@gmail.com>
2016-01-29 18:25:40 +0100

    Added unit test

--- src/Test/Core/SubtitleTest.cs
+++ src/Test/Core/SubtitleTest.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Nikse.SubtitleEdit.Core;
+using System;
 using System.Collections.Generic;
 
 namespace Test.Core
@@ -94,6 +95,40 @@ namespace Test.Core
             Assert.AreEqual(sub.Paragraphs.Count, 2);
             Assert.AreEqual(sub.Paragraphs[0], p1);
             Assert.AreEqual(sub.Paragraphs[1], p3);
+        }
+
+        [TestMethod]
+        public void TestChangeFrameRate1()
+        {
+            var sub = new Subtitle();
+            var p1 = new Paragraph("1", 0, 1000);
+            var p2 = new Paragraph("2", 2000, 3000);
+            sub.Paragraphs.Add(p1);
+            sub.Paragraphs.Add(p2);
+
+            sub.ChangeFrameRate(25.0, 25.0);
+            Assert.AreEqual(sub.Paragraphs.Count, 2);
+            Assert.AreEqual(sub.Paragraphs[0].StartTime.TotalMilliseconds, 0);
+            Assert.AreEqual(sub.Paragraphs[0].EndTime.TotalMilliseconds, 1000);
+            Assert.AreEqual(sub.Paragraphs[1].StartTime.TotalMilliseconds, 2000);
+            Assert.AreEqual(sub.Paragraphs[1].EndTime.TotalMilliseconds, 3000);
+        }
+
+        [TestMethod]
+        public void TestChangeFrameRate2()
+        {
+            var sub = new Subtitle();
+            var p1 = new Paragraph("1", 0, 1000);
+            var p2 = new Paragraph("2", 2000, 3000);
+            sub.Paragraphs.Add(p1);
+            sub.Paragraphs.Add(p2);
+
+            sub.ChangeFrameRate(25.0, 30.0);
+            Assert.AreEqual(sub.Paragraphs.Count, 2);
+            Assert.AreEqual(sub.Paragraphs[0].StartTime.TotalMilliseconds, 0);
+            Assert.IsTrue(Math.Abs(sub.Paragraphs[0].EndTime.TotalMilliseconds - 833.33333333333) < 0.01);
+            Assert.IsTrue(Math.Abs(sub.Paragraphs[1].StartTime.TotalMilliseconds - 1666.6666666666667) < 0.01);
+            Assert.IsTrue(Math.Abs(sub.Paragraphs[1].EndTime.TotalMilliseconds - 2500) < 0.01);
         }
 
     }
