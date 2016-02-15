7337fdd5e751cb91225d06422d536031b7517748
Ivandro Ismael <ivandrofly@hotmail.com>
2016-02-15 07:01:32 +0000

    [feat] - add export feature to Status Log

--- src/Forms/StatusLog.cs
+++ src/Forms/StatusLog.cs
@@ -10,6 +10,7 @@ namespace Nikse.SubtitleEdit.Forms
             InitializeComponent();
             Text = Configuration.Settings.Language.Main.StatusLog;
             buttonOK.Text = Configuration.Settings.Language.General.Ok;
+            buttonExportLog.Text = Configuration.Settings.Language.Statistics.Export;
             textBoxStatusLog.Text = logText;
         }
 
@@ -21,6 +22,27 @@ namespace Nikse.SubtitleEdit.Forms
             if (e.KeyData == (Keys.Control | Keys.A) && textBoxStatusLog.Text.Length > 0)
             {
                 textBoxStatusLog.SelectAll();
+            }
+        }
+
+        private void buttonExportLog_Click(object sender, System.EventArgs e)
+        {
+            var logMessages = textBoxStatusLog.Text;
+            if (logMessages.Length == 0)
+                return;
+            using (var sfd = new SaveFileDialog() { FileName = "Subtitle-Log", DefaultExt = "txt", Filter = "Text files (*.txt)|*.txt|All files (*.*)|*.*" })
+            {
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    try
+                    {
+                        System.IO.File.WriteAllText(sfd.FileName, logMessages, System.Text.Encoding.UTF8);
+                    }
+                    catch (System.Exception ex)
+                    {
+                        MessageBox.Show(ex.Message);
+                    }
+                }
             }
         }
     }
