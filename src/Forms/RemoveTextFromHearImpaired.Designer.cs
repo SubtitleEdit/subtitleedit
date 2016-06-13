7a9b5e8893e4593b64e8c36ed7bec6b4b720656a
Nikolaj Olsson <nikse.dk@gmail.com>
2016-06-13 21:41:35 +0200

    Yet another fix for "Apply" / "OK" in "Remove text for HI" - thx Sami :) (this apparently needs good testing)

--- src/Forms/RemoveTextFromHearImpaired.Designer.cs
+++ src/Forms/RemoveTextFromHearImpaired.Designer.cs
@@ -119,6 +119,7 @@
             this.listViewFixes.TabIndex = 0;
             this.listViewFixes.UseCompatibleStateImageBehavior = false;
             this.listViewFixes.View = System.Windows.Forms.View.Details;
+            this.listViewFixes.ItemChecked += new System.Windows.Forms.ItemCheckedEventHandler(this.listViewFixes_ItemChecked);
             // 
             // columnHeaderApply
             // 
