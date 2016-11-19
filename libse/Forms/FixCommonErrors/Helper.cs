54b811cfc3ffdb8e7970ffc7a7250bdbe97997c5
Ivandro Ismael <ivandrofly@gmail.com>
2016-11-19 22:26:46 +0000

    [FixDialogsOnOneLine] - Minor fixes.

--- libse/Forms/FixCommonErrors/Helper.cs
+++ libse/Forms/FixCommonErrors/Helper.cs
@@ -158,10 +158,10 @@ namespace Nikse.SubtitleEdit.Core.Forms.FixCommonErrors
                     string part0 = noTagLines[0];
                     string part1 = noTagLines[1];
                     if (part0.Length > 1 && "-—!?.\")]".Contains(part0[part0.Length - 1]) &&
-                        part1.Length > 1 && ("'" + Utilities.UppercaseLetters).Contains(part1[0]))
+                        part1.Length > 1 && (char.IsUpper(part1[0]) || part1[0] == '\''))
                     {
                         text = text.Replace(" - ", Environment.NewLine + "- ");
-                        if (Utilities.AllLettersAndNumbers.Contains(part0[0]))
+                        if (char.IsLetter((part0[0])) || CharUtils.IsDigit(part0[0]))
                         {
                             if (text.Length > 3 && text[0] == '<' && text[2] == '>')
                                 text = "<" + text[1] + ">" + "- " + text.Substring(3).TrimStart();
