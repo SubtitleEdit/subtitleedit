77a95e1b8bc42a48cc08452b079fa961ff4a276c
Ivandro Ismael <ivandrofly@gmail.com>
2016-12-25 01:53:25 +0000

    [OcrFixEngine] - Call optimized methods to fix white-spaces.

--- src/Logic/OCR/OcrFixEngine.cs
+++ src/Logic/OCR/OcrFixEngine.cs
@@ -347,12 +347,8 @@ namespace Nikse.SubtitleEdit.Logic.Ocr
 
         public string FixOcrErrors(string text, int index, string lastLine, bool logSuggestions, AutoGuessLevel autoGuess)
         {
-            while (text.Contains(Environment.NewLine + " "))
-                text = text.Replace(Environment.NewLine + " ", Environment.NewLine);
-            while (text.Contains(" " + Environment.NewLine))
-                text = text.Replace(" " + Environment.NewLine, Environment.NewLine);
-            while (text.Contains(Environment.NewLine + Environment.NewLine))
-                text = text.Replace(Environment.NewLine + Environment.NewLine, Environment.NewLine);
+            text = text.FixResursiveLineBreaks();
+            text = text.FixExtraSpaces();
             text = text.Trim();
 
             // Try to prevent resizing when fixing Ocr-hardcoded.
