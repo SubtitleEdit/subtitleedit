770945ea77b15233d8eaaa71b28a4e1eb5adbd72
Ivandro Ismael <ivandrofly@gmail.com>
2017-02-19 03:44:17 +0000

    [ApplyDurationLimits] - Control.Right equivalent to Left+Width.

--- src/Forms/ApplyDurationLimits.cs
+++ src/Forms/ApplyDurationLimits.cs
@@ -45,8 +45,8 @@ namespace Nikse.SubtitleEdit.Forms
 
         private void FixLargeFonts()
         {
-            if (labelNote.Left + labelNote.Width + 5 > Width)
-                Width = labelNote.Left + labelNote.Width + 5;
+            if (labelNote.Right + 5 > Width)
+                Width = labelNote.Right + 5;
             UiUtil.FixLargeFonts(this, buttonOK);
         }
 
