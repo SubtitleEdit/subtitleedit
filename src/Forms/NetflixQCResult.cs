859b39730d1fd8ae51bb3baff268f09aad409032
askolesov <kolesov.alex.s@gmail.com>
2017-03-03 11:53:48 +0300

    Implemented clickable links in the QC report message.

--- src/Forms/NetflixQCResult.cs
+++ src/Forms/NetflixQCResult.cs
@@ -13,19 +13,30 @@ namespace Nikse.SubtitleEdit.Forms
     public partial class NetflixQCResult : Form
     {
         private string Message { get; set; }
-        private string FileToLocate { get; set; }
+        private List<string> FilesToLocate { get; set; }
 
-        public NetflixQCResult(string message, string fileToLocate)
+        public NetflixQCResult(string message, List<string> filesToLocate)
         {
             InitializeComponent();
 
             Message = message;
-            FileToLocate = fileToLocate;
+            FilesToLocate = filesToLocate;
 
+            InitUi();
+        }
+
+        private void InitUi()
+        {
             lblText.Text = Message;
+
+            FilesToLocate
+                .Where(Message.Contains)
+                .ToList()
+                .ForEach(s => lblText.Links.Add(Message.IndexOf(s), s.Length, s));
+
             Width = Math.Max(lblText.Width + lblText.Left * 3, 300);
 
-            btnOpen.Visible = !string.IsNullOrEmpty(fileToLocate);
+            btnOpen.Visible = FilesToLocate.Count != 0;
         }
 
         protected override bool ProcessDialogKey(Keys keyData)
@@ -45,8 +56,13 @@ namespace Nikse.SubtitleEdit.Forms
 
         private void btnOpen_Click(object sender, EventArgs e)
         {
-            Process.Start("explorer.exe", string.Format(@"/select,""{0}"" ", FileToLocate));
+            Process.Start("explorer.exe", string.Format(@"/select,""{0}"" ", FilesToLocate[0]));
             Close();
+        }
+
+        private void lblText_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("explorer.exe", string.Format(@"/select,""{0}"" ", e.Link.LinkData));
         }
     }
 }
