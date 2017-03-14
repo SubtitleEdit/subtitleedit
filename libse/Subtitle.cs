2825d5be6ebaa8fbdc9ee5437f039d6e4a86acfc
askolesov <kolesov.alex.s@gmail.com>
2017-03-14 14:08:54 +0300

    Minor reverts.

--- libse/Subtitle.cs
+++ libse/Subtitle.cs
@@ -14,8 +14,8 @@ namespace Nikse.SubtitleEdit.Core
         private readonly List<HistoryItem> _history;
         private SubtitleFormat _format;
         private bool _wasLoadedWithFrameNumbers;
-        public string Header { get; set; } = "";
-        public string Footer { get; set; } = "";
+        public string Header { get; set; } = string.Empty;
+        public string Footer { get; set; } = string.Empty;
 
         public string FileName { get; set; }
 
