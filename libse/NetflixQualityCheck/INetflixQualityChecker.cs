67dce44e53b9ef4b09bcbb8131e4bc70a103d34d
Nikolaj Olsson <nikse.dk@gmail.com>
2017-03-23 15:34:49 +0100

    Fix for re-open Netflix format (was opened as iTunes TT) + minor cleanup

--- libse/NetflixQualityCheck/INetflixQualityChecker.cs
+++ libse/NetflixQualityCheck/INetflixQualityChecker.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-
-namespace Nikse.SubtitleEdit.Core.NetflixQualityCheck
+﻿namespace Nikse.SubtitleEdit.Core.NetflixQualityCheck
 {
     public interface INetflixQualityChecker
     {
--- libse/NetflixQualityCheck/NetflixQualityReportBuilder.cs
+++ libse/NetflixQualityCheck/NetflixQualityReportBuilder.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using System.Text;
 
 namespace Nikse.SubtitleEdit.Core.NetflixQualityCheck
