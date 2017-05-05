0d9add74377281c262f4ccd6c7d5292f87fd321e
Ivandro Ismael <ivandrofly@gmail.com>
2017-05-05 23:55:41 +0100

    [utilities] - call optimized method to reverse parenthesis.

--- libse/Utilities.cs
+++ libse/Utilities.cs
@@ -1261,15 +1261,7 @@ namespace Nikse.SubtitleEdit.Core
             var lines = text.SplitToLines();
             foreach (string line in lines)
             {
-                string s = line.Trim();
-                for (int i = 0; i < s.Length; i++)
-                {
-                    if (s[i] == ')')
-                        s = s.Remove(i, 1).Insert(i, "(");
-                    else if (s[i] == '(')
-                        s = s.Remove(i, 1).Insert(i, ")");
-                }
-
+                string s = ReverseParenthesis(line.Trim());
                 bool numbersOn = false;
                 string numbers = string.Empty;
                 for (int i = 0; i < s.Length; i++)
