7da5b5185cf2bcc659325230597d9ea7d0c70200
Nikolaj Olsson <nikse.dk@gmail.com>
2018-02-09 16:42:07 +0100

    Fix alignment tags in DVD Studio Pro formats - thx Shas2509 :) Work on #2755

--- libse/SubtitleFormats/DvdStudioProSpaceOne.cs
+++ libse/SubtitleFormats/DvdStudioProSpaceOne.cs
@@ -33,13 +33,26 @@ $FadeOut                =   0
 $HorzAlign          =   Center
 ";
 
+            var verticalAlign = "$VertAlign=Bottom";
+            var lastVerticalAlign = verticalAlign;
             var sb = new StringBuilder();
             sb.AppendLine(header);
             foreach (Paragraph p in subtitle.Paragraphs)
             {
                 string startTime = string.Format(timeFormat, p.StartTime.Hours, p.StartTime.Minutes, p.StartTime.Seconds, MillisecondsToFramesMaxFrameRate(p.StartTime.Milliseconds));
                 string endTime = string.Format(timeFormat, p.EndTime.Hours, p.EndTime.Minutes, p.EndTime.Seconds, MillisecondsToFramesMaxFrameRate(p.EndTime.Milliseconds));
+
+                bool topAlign = p.Text.StartsWith("{\\an7}", StringComparison.Ordinal) ||
+                                p.Text.StartsWith("{\\an8}", StringComparison.Ordinal) ||
+                                p.Text.StartsWith("{\\an9}", StringComparison.Ordinal);
+                verticalAlign = topAlign ? "$VertAlign=Top" : "$VertAlign=Bottom";
+                if (lastVerticalAlign != verticalAlign)
+                {
+                    sb.AppendLine(verticalAlign);
+                }
+
                 sb.AppendFormat(paragraphWriteFormat, startTime, endTime, DvdStudioPro.EncodeStyles(p.Text));
+                lastVerticalAlign = verticalAlign;
             }
             return sb.ToString().Trim();
         }
@@ -53,6 +66,7 @@ $HorzAlign          =   Center
         {
             _errorCount = 0;
             int number = 0;
+            bool alignTop = false;
             foreach (string line in lines)
             {
                 if (!string.IsNullOrWhiteSpace(line) && line[0] != '$' && !line.StartsWith("//", StringComparison.Ordinal))
@@ -70,12 +84,26 @@ $HorzAlign          =   Center
                             string text = line.Substring(25).Trim();
                             p.Text = text.Replace(" | ", Environment.NewLine).Replace("|", Environment.NewLine);
                             p.Text = DvdStudioPro.DecodeStyles(p.Text);
+                            if (alignTop)
+                                p.Text = "{\\an8}" + p.Text;
                             subtitle.Paragraphs.Add(p);
                         }
                     }
                     else
                     {
                         _errorCount++;
+                    }
+                }
+                else if (line != null && line.TrimStart().StartsWith("$VertAlign", StringComparison.OrdinalIgnoreCase))
+                {
+                    var s = line.RemoveChar(' ').RemoveChar('\t');
+                    if (s.Equals("$VertAlign=Bottom", StringComparison.OrdinalIgnoreCase))
+                    {
+                        alignTop = false;
+                    }
+                    else if (s.Equals("$VertAlign=Top", StringComparison.OrdinalIgnoreCase))
+                    {
+                        alignTop = true;
                     }
                 }
             }
