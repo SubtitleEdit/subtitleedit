5486fd5bf05241ae64dc47f4328a573eab7a5c23
Nikolaj Olsson <nikse.dk@gmail.com>
2018-02-15 03:59:36 +0100

    Add read of format fcpxml 1.7 - thx lesmovie :)

---
+++ libse/SubtitleFormats/FinalCutProXml17.cs
@@ -0,0 +1,11 @@
+﻿namespace Nikse.SubtitleEdit.Core.SubtitleFormats
+{
+   
+    public class FinalCutProXml17 : FinalCutProXml15
+    {
+        public FinalCutProXml17()
+        {
+            FcpXmlVersion = "1.7";
+        }
+    }
+}
