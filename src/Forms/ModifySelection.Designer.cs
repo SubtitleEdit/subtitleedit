b30ab32ba826d924dcb2067d20bb4bb7d36db5a9
Nikolaj Olsson <nikse.dk@gmail.com>
2018-03-29 13:13:35 +0200

    Add some style support to "Modify selection"

--- src/Forms/ModifySelection.Designer.cs
+++ src/Forms/ModifySelection.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Nikse.SubtitleEdit.Forms
 {
-    partial class ModifySelection
+    sealed partial class ModifySelection
     {
         /// <summary>
         /// Required designer variable.
@@ -37,6 +37,8 @@
             this.checkBoxCaseSensitive = new System.Windows.Forms.CheckBox();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.comboBoxRule = new System.Windows.Forms.ComboBox();
+            this.listViewStyles = new System.Windows.Forms.ListView();
+            this.columnHeaderStyleName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.buttonCancel = new System.Windows.Forms.Button();
             this.buttonOK = new System.Windows.Forms.Button();
             this.buttonApply = new System.Windows.Forms.Button();
@@ -45,6 +47,7 @@
             this.columnHeaderApply = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeaderLine = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.columnHeaderText = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeaderStyle = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.labelInfo = new System.Windows.Forms.Label();
             this.groupBoxWhatToDo.SuspendLayout();
             this.groupBoxRule.SuspendLayout();
@@ -59,9 +62,9 @@
             this.groupBoxWhatToDo.Controls.Add(this.radioButtonSubtractFromSelection);
             this.groupBoxWhatToDo.Controls.Add(this.radioButtonAddToSelection);
             this.groupBoxWhatToDo.Controls.Add(this.radioButtonNewSelection);
-            this.groupBoxWhatToDo.Location = new System.Drawing.Point(343, 12);
+            this.groupBoxWhatToDo.Location = new System.Drawing.Point(380, 12);
             this.groupBoxWhatToDo.Name = "groupBoxWhatToDo";
-            this.groupBoxWhatToDo.Size = new System.Drawing.Size(381, 120);
+            this.groupBoxWhatToDo.Size = new System.Drawing.Size(411, 135);
             this.groupBoxWhatToDo.TabIndex = 1;
             this.groupBoxWhatToDo.TabStop = false;
             this.groupBoxWhatToDo.Text = "What to do with matches";
@@ -117,9 +120,10 @@
             this.groupBoxRule.Controls.Add(this.checkBoxCaseSensitive);
             this.groupBoxRule.Controls.Add(this.textBox1);
             this.groupBoxRule.Controls.Add(this.comboBoxRule);
+            this.groupBoxRule.Controls.Add(this.listViewStyles);
             this.groupBoxRule.Location = new System.Drawing.Point(12, 12);
             this.groupBoxRule.Name = "groupBoxRule";
-            this.groupBoxRule.Size = new System.Drawing.Size(324, 120);
+            this.groupBoxRule.Size = new System.Drawing.Size(362, 135);
             this.groupBoxRule.TabIndex = 0;
             this.groupBoxRule.TabStop = false;
             this.groupBoxRule.Text = "Rule";
@@ -127,7 +131,7 @@
             // checkBoxCaseSensitive
             // 
             this.checkBoxCaseSensitive.AutoSize = true;
-            this.checkBoxCaseSensitive.Location = new System.Drawing.Point(7, 54);
+            this.checkBoxCaseSensitive.Location = new System.Drawing.Point(6, 47);
             this.checkBoxCaseSensitive.Name = "checkBoxCaseSensitive";
             this.checkBoxCaseSensitive.Size = new System.Drawing.Size(94, 17);
             this.checkBoxCaseSensitive.TabIndex = 2;
@@ -137,9 +141,9 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(198, 27);
+            this.textBox1.Location = new System.Drawing.Point(198, 21);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(120, 20);
+            this.textBox1.Size = new System.Drawing.Size(158, 20);
             this.textBox1.TabIndex = 1;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
@@ -153,17 +157,32 @@
             "Ends with",
             "Not contains",
             "RegEx"});
-            this.comboBoxRule.Location = new System.Drawing.Point(7, 26);
+            this.comboBoxRule.Location = new System.Drawing.Point(6, 20);
             this.comboBoxRule.Name = "comboBoxRule";
             this.comboBoxRule.Size = new System.Drawing.Size(185, 21);
             this.comboBoxRule.TabIndex = 0;
             this.comboBoxRule.SelectedIndexChanged += new System.EventHandler(this.comboBoxRule_SelectedIndexChanged);
             // 
+            // listViewStyles
+            // 
+            this.listViewStyles.CheckBoxes = true;
+            this.listViewStyles.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeaderStyleName});
+            this.listViewStyles.FullRowSelect = true;
+            this.listViewStyles.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None;
+            this.listViewStyles.Location = new System.Drawing.Point(198, 21);
+            this.listViewStyles.Name = "listViewStyles";
+            this.listViewStyles.Size = new System.Drawing.Size(158, 108);
+            this.listViewStyles.TabIndex = 3;
+            this.listViewStyles.UseCompatibleStateImageBehavior = false;
+            this.listViewStyles.View = System.Windows.Forms.View.Details;
+            this.listViewStyles.ItemChecked += new System.Windows.Forms.ItemCheckedEventHandler(this.listViewStyles_ItemChecked);
+            // 
             // buttonCancel
             // 
             this.buttonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.buttonCancel.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.buttonCancel.Location = new System.Drawing.Point(554, 400);
+            this.buttonCancel.Location = new System.Drawing.Point(621, 446);
             this.buttonCancel.Name = "buttonCancel";
             this.buttonCancel.Size = new System.Drawing.Size(75, 21);
             this.buttonCancel.TabIndex = 6;
@@ -175,7 +194,7 @@
             // 
             this.buttonOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.buttonOK.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.buttonOK.Location = new System.Drawing.Point(473, 400);
+            this.buttonOK.Location = new System.Drawing.Point(540, 446);
             this.buttonOK.Name = "buttonOK";
             this.buttonOK.Size = new System.Drawing.Size(75, 21);
             this.buttonOK.TabIndex = 5;
@@ -187,7 +206,7 @@
             // 
             this.buttonApply.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.buttonApply.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.buttonApply.Location = new System.Drawing.Point(635, 400);
+            this.buttonApply.Location = new System.Drawing.Point(702, 446);
             this.buttonApply.Name = "buttonApply";
             this.buttonApply.Size = new System.Drawing.Size(89, 21);
             this.buttonApply.TabIndex = 7;
@@ -201,9 +220,9 @@
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.groupBoxPreview.Controls.Add(this.listViewFixes);
-            this.groupBoxPreview.Location = new System.Drawing.Point(12, 138);
+            this.groupBoxPreview.Location = new System.Drawing.Point(12, 153);
             this.groupBoxPreview.Name = "groupBoxPreview";
-            this.groupBoxPreview.Size = new System.Drawing.Size(712, 255);
+            this.groupBoxPreview.Size = new System.Drawing.Size(779, 286);
             this.groupBoxPreview.TabIndex = 2;
             this.groupBoxPreview.TabStop = false;
             this.groupBoxPreview.Text = "Matching lines";
@@ -214,12 +233,13 @@
             this.listViewFixes.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
             this.columnHeaderApply,
             this.columnHeaderLine,
-            this.columnHeaderText});
+            this.columnHeaderText,
+            this.columnHeaderStyle});
             this.listViewFixes.Dock = System.Windows.Forms.DockStyle.Fill;
             this.listViewFixes.FullRowSelect = true;
             this.listViewFixes.Location = new System.Drawing.Point(3, 16);
             this.listViewFixes.Name = "listViewFixes";
-            this.listViewFixes.Size = new System.Drawing.Size(706, 236);
+            this.listViewFixes.Size = new System.Drawing.Size(773, 267);
             this.listViewFixes.TabIndex = 1;
             this.listViewFixes.UseCompatibleStateImageBehavior = false;
             this.listViewFixes.View = System.Windows.Forms.View.Details;
@@ -237,13 +257,17 @@
             // columnHeaderText
             // 
             this.columnHeaderText.Text = "Text";
-            this.columnHeaderText.Width = 500;
+            this.columnHeaderText.Width = 450;
+            // 
+            // columnHeaderStyle
+            // 
+            this.columnHeaderStyle.Text = "Style";
             // 
             // labelInfo
             // 
             this.labelInfo.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
             this.labelInfo.AutoSize = true;
-            this.labelInfo.Location = new System.Drawing.Point(12, 400);
+            this.labelInfo.Location = new System.Drawing.Point(12, 446);
             this.labelInfo.Name = "labelInfo";
             this.labelInfo.Size = new System.Drawing.Size(47, 13);
             this.labelInfo.TabIndex = 3;
@@ -253,7 +277,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(736, 432);
+            this.ClientSize = new System.Drawing.Size(803, 478);
             this.Controls.Add(this.labelInfo);
             this.Controls.Add(this.groupBoxWhatToDo);
             this.Controls.Add(this.groupBoxPreview);
@@ -271,6 +295,7 @@
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Create/modify selection";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.ModifySelection_KeyDown);
+            this.Resize += new System.EventHandler(this.ModifySelection_Resize);
             this.groupBoxWhatToDo.ResumeLayout(false);
             this.groupBoxWhatToDo.PerformLayout();
             this.groupBoxRule.ResumeLayout(false);
@@ -301,5 +326,8 @@
         private System.Windows.Forms.Label labelInfo;
         private System.Windows.Forms.CheckBox checkBoxCaseSensitive;
         private System.Windows.Forms.RadioButton radioButtonIntersect;
+        private System.Windows.Forms.ColumnHeader columnHeaderStyle;
+        private System.Windows.Forms.ListView listViewStyles;
+        private System.Windows.Forms.ColumnHeader columnHeaderStyleName;
     }
 }
