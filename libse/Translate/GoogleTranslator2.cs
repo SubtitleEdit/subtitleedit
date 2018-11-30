9adac1fdc276132241741d46ae5b22dc68495e55
Nikolaj Olsson <nikse.dk@gmail.com>
2018-11-30 06:38:49 +0100

    Work on gt

---
+++ libse/Translate/GoogleTranslator2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Nikse.SubtitleEdit.Core.Translate
+{
+    /// <summary>
+    /// Google translate via Google Cloud API - see https://cloud.google.com/translate/
+    /// </summary>
+    public class GoogleTranslator2 : ITranslator
+    {
+        public List<TranslationPair> GetTranslationPairs()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetName()
+        {
+            return "Google translate";
+        }
+
+        public string GetUrl()
+        {
+            return "https://translate.google.com/";
+        }
+
+        public List<string> Translate(string sourceLanguage, string targetLanguage, List<string> texts, StringBuilder log)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
