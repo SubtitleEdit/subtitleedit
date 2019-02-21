4748b62ac451eed96b9662790d3fcd3926c3689a
Ivandro Ismael <ivandrofly@hotmail.com>
2019-02-21 16:45:01 +0000

    resolve #3387

--- src/Forms/Main.cs
+++ src/Forms/Main.cs
@@ -14209,7 +14209,7 @@ namespace Nikse.SubtitleEdit.Forms
                             var original = Utilities.GetOriginalParagraph(_subtitle.GetIndex(p), p, _subtitleAlternate.Paragraphs);
                             if (original != null)
                             {
-                                newP.Text += Environment.NewLine + Environment.NewLine + original.Text;
+                                newP.Text = newP.Text.TrimEnd() + Environment.NewLine + original.Text.TrimStart();
                             }
 
                             subtitle.Paragraphs.Add(newP);
