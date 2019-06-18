62071fc1e1733f835b6acce4f1517afd12b83f17
Nikolaj Olsson <nio@bluegarden.com>
2019-06-18 14:42:45 +0200

    Fix for crash in "Batch convert" Fixes PR ae32039822b8a3c38b864aeeda7b5eaeb1abda44

--- libse/Forms/DurationsBridgeGaps.cs
+++ libse/Forms/DurationsBridgeGaps.cs
@@ -7,6 +7,7 @@ namespace Nikse.SubtitleEdit.Core.Forms
     {
         public static int BridgeGaps(Subtitle subtitle, int minMsBetweenLines, bool divideEven, double maxMs, List<int> fixedIndexes, Dictionary<string, string> dic)
         {
+            int fixedCount = 0;
             if (minMsBetweenLines > maxMs)
             {
                 string message = $"{nameof(DurationsBridgeGaps)}: {nameof(minMsBetweenLines)} cannot be smaller than {nameof(maxMs)}!";
@@ -17,8 +18,8 @@ namespace Nikse.SubtitleEdit.Core.Forms
             int count = subtitle.Paragraphs.Count - 1;
             for (int i = 0; i < count; i++)
             {
-                Paragraph cur = subtitle.Paragraphs[i];
-                Paragraph next = subtitle.Paragraphs[i + 1];
+                var cur = subtitle.Paragraphs[i];
+                var next = subtitle.Paragraphs[i + 1];
 
                 double currentGaps = next.StartTime.TotalMilliseconds - cur.EndTime.TotalMilliseconds;
 
@@ -28,7 +29,7 @@ namespace Nikse.SubtitleEdit.Core.Forms
                     continue;
                 }
 
-                // next paragraph start-time will be pull to try to meet the current parragraph
+                // next paragraph start-time will be pull to try to meet the current paragraph
                 if (divideEven)
                 {
                     next.StartTime.TotalMilliseconds -= currentGaps / 2.0;
@@ -40,12 +41,13 @@ namespace Nikse.SubtitleEdit.Core.Forms
                     fixedIndexes.Add(i);
                     fixedIndexes.Add(i + 1);
                 }
+                fixedCount++;
 
                 double newGaps = next.StartTime.TotalMilliseconds - cur.EndTime.TotalMilliseconds;
                 dic?.Add(cur.ID, $"{currentGaps / TimeCode.BaseUnit:0.000} => {newGaps / TimeCode.BaseUnit:0.000}");
             }
 
-            return fixedIndexes.Count / 2;
+            return fixedCount;
         }
     }
 }
