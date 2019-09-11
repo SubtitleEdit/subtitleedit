2afe10563701d868bb36bdafa872d2c18d063abe
Nikolaj Olsson <nikse.dk@gmail.com>
2019-09-11 21:32:03 +0200

    Fix mp4 box "mvhd"

--- libse/ContainerFormats/Mp4/Boxes/Mvhd.cs
+++ libse/ContainerFormats/Mp4/Boxes/Mvhd.cs
@@ -7,10 +7,10 @@ namespace Nikse.SubtitleEdit.Core.ContainerFormats.Mp4.Boxes
     /// </summary>
     public class Mvhd : Box
     {
-        public readonly uint CreationTime;
-        public readonly uint ModificationTime;
-        public readonly uint Duration;
-        public readonly uint TimeScale;
+        public readonly ulong CreationTime;
+        public readonly ulong ModificationTime;
+        public readonly ulong Duration;
+        public readonly ulong TimeScale;
 
         public Mvhd(Stream fs)
         {
@@ -20,11 +20,21 @@ namespace Nikse.SubtitleEdit.Core.ContainerFormats.Mp4.Boxes
             {
                 return;
             }
-
-            CreationTime = GetUInt(4);
-            ModificationTime = GetUInt(8);
-            TimeScale = GetUInt(12);
-            Duration = GetUInt(16);
+            int version = Buffer[0];
+            if (version == 0)
+            {
+                CreationTime = GetUInt(4);
+                ModificationTime = GetUInt(8);
+                TimeScale = GetUInt(12);
+                Duration = GetUInt(16);
+            }
+            else
+            {
+                CreationTime = GetUInt64(4); // 64-bit 
+                ModificationTime = GetUInt64(12); // 64-bit 
+                TimeScale = GetUInt(20); // 32-bit
+                Duration = GetUInt64(24); // 64-bit 
+            }
         }
     }
 }
