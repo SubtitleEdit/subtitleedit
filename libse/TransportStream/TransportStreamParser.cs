7f24682c6b71cb242f600124cfd3e61392c5d8c7
Nikolaj Olsson <nikse.dk@gmail.com>
2019-10-25 18:27:24 +0200

    work

--- libse/TransportStream/TransportStreamParser.cs
+++ libse/TransportStream/TransportStreamParser.cs
@@ -4,7 +4,6 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices;
 using System.Text;
 
 namespace Nikse.SubtitleEdit.Core.TransportStream
