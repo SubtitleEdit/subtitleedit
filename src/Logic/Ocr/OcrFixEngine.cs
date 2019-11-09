4142af624dcd3bb3d0d272b6fecb05270ec754ab
Nikolaj Olsson <nikse.dk@gmail.com>
2019-11-09 16:05:49 +0100

    Work on #3824

--- src/Logic/Ocr/OcrFixEngine.cs
+++ src/Logic/Ocr/OcrFixEngine.cs
@@ -1295,7 +1295,7 @@ namespace Nikse.SubtitleEdit.Logic.Ocr
             }
 
             var words = new List<string>();
-            foreach (var w in tempLine.Replace("<i>", string.Empty).Replace("</i>", string.Empty).Split(' ', '\r', '\n', '-'))
+            foreach (var w in tempLine.Replace("<i>", string.Empty).Replace("</i>", string.Empty).Split(SpellCheckWordLists.SplitChars.ToArray()))
             {
                 words.Add(w.Trim(trimChars));
             }
