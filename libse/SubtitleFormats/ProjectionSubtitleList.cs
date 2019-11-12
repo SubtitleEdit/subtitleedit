c005877e71f4ce0c1fd37bec2d4aaec94f5b4554
Nikolaj Olsson <nikse.dk@gmail.com>
2019-11-12 08:20:17 +0100

    More work on format psl

--- libse/SubtitleFormats/ProjectionSubtitleList.cs
+++ libse/SubtitleFormats/ProjectionSubtitleList.cs
@@ -15,15 +15,40 @@ namespace Nikse.SubtitleEdit.Core.SubtitleFormats
             foreach (var p in subtitle.Paragraphs)
             {
                 sb.Append($"{EncodeTime(p.StartTime)} {EncodeTime(p.EndTime)} {{\\uc0\\pard\\qc");
-                foreach (var line in p.Text.SplitToLines())
+                var text = Utilities.RemoveSsaTags(p.Text);
+                text = HtmlUtil.RemoveOpenCloseTags(text, HtmlUtil.TagBold, HtmlUtil.TagUnderline, HtmlUtil.TagFont);
+                foreach (var line in text.SplitToLines())
                 {
                     sb.Append("{");
-                    sb.Append(line); //TODO: add RTF tags
+                    sb.Append(RftEncode(line)); 
                     sb.Append("}\\par");
                 }
                 sb.AppendLine("}");
             }
             sb.AppendLine();
+            return sb.ToString();
+        }
+
+        private string RftEncode(string line)
+        {
+            var sb = new StringBuilder();
+            for (var index = 0; index < line.Length; index++)
+            {
+                char ch = line[index];
+                var number = (int)ch;
+                if (ch == '\\' || ch == '{' || ch == '}')
+                {
+                    sb.Append($"\\{ch}");
+                }
+                else if (number <= byte.MaxValue)
+                {
+                    sb.Append(ch);
+                }
+                else
+                {
+                    sb.Append($"\\u{number.ToString(CultureInfo.InvariantCulture).PadLeft(4, '0')}");
+                }
+            }
             return sb.ToString();
         }
 
@@ -63,12 +88,76 @@ namespace Nikse.SubtitleEdit.Core.SubtitleFormats
                     long.TryParse(frameArray[1], NumberStyles.None, CultureInfo.InvariantCulture, out var endFrame))
 
                 {
-                    var text = line.Remove(0, startText);
-                    //TODO: remove RTF tags
+                    var rtf = line.Remove(0, startText);
+                    var text = RtfDecode(rtf, "en");
                     subtitle.Paragraphs.Add(new Paragraph(text, FramesToMilliseconds(startFrame), FramesToMilliseconds(endFrame)));
                 }
             }
             subtitle.Renumber();
+        }
+
+        private string RtfDecode(string text, string language)
+        {
+            var codeOn = false;
+            var sb = new StringBuilder();
+            char last = ' ';
+            var i = 0;
+            while (i < text.Length)
+            {
+                var ch = text[i];
+                if (codeOn && ch == '{')
+                {
+                    codeOn = false;
+                }
+                else if (codeOn && ch != ' ')
+                {
+
+                }
+                else if (codeOn && ch == ' ')
+                {
+                    codeOn = false;
+                    sb.Append(ch);
+                }
+                else if (last == '\\')
+                {
+                    if (ch == 'u' && i + 5 < text.Length && char.IsNumber(text[i + 1]) && char.IsNumber(text[i + 2]) && char.IsNumber(text[i + 3]) && char.IsNumber(text[i + 4]))
+                    {
+                        var unicodeNumber = text.Substring(i + 1, 4);
+                        var unescaped = char.ConvertFromUtf32(int.Parse(unicodeNumber));
+                        sb.Append(unescaped);
+                        i += 4;
+                    }
+                    if (ch == 'p' && i + 5 < text.Length && text[i + 1] == 'a' && text[i + 2] == 'r' && " \r\n\\{}".Contains(text[i + 3]))
+                    {
+                        sb.AppendLine();
+                        i += 2;
+                    }
+                    else if (ch >= 'a' && ch <= 'z' || ch == '*')
+                    {
+                        codeOn = true;
+                    }
+                    else
+                    {
+                        sb.Append(ch);
+                    }
+                }
+                else if (ch == '\\')
+                {
+                    // start of code
+                }
+                else if (ch == '{' || ch == '}')
+                {
+                    // start/end of block
+                    codeOn = false;
+                }
+                else
+                {
+                    sb.Append(ch);
+                }
+                i++;
+                last = ch;
+            }
+            return Utilities.RemoveUnneededSpaces(sb.ToString(), language);
         }
     }
 }
