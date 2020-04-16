ae177e0a1d7c122147d30f6fdb45e2ec2fc597d3
Nikolaj Olsson <nikse.dk@gmail.com>
2020-04-16 14:12:31 +0200

    Move "audio file extensions" to list (minor refact)

--- libse/Utilities.cs
+++ libse/Utilities.cs
@@ -29,32 +29,21 @@ namespace Nikse.SubtitleEdit.Core
         private static readonly Regex NumberSeparatorNumberRegEx = new Regex(@"\b\d+[\.:;] \d+\b", RegexOptions.Compiled);
 
         public static string[] VideoFileExtensions { get; } = { ".avi", ".mkv", ".wmv", ".mpg", ".mpeg", ".divx", ".mp4", ".asf", ".flv", ".mov", ".m4v", ".vob", ".ogv", ".webm", ".ts", ".m2ts", ".mts", ".avs", ".mxf" };
+        public static string[] AudioFileExtensions { get; } = { ".mp3", ".wav", ".wma", ".ogg", ".mpa", ".m4a", ".ape", ".aiff", ".flac", ".aac", ".ac3", ".mka" };
 
         public static string GetVideoFileFilter(bool includeAudioFiles)
         {
             var sb = new StringBuilder();
-            sb.Append(Configuration.Settings.Language.General.VideoFiles + "|");
-            int i = 0;
-            foreach (string extension in VideoFileExtensions)
-            {
-                if (i > 0)
-                {
-                    sb.Append(';');
-                }
+            sb.Append(Configuration.Settings.Language.General.VideoFiles + "|*");
+            sb.Append(string.Join(";*", VideoFileExtensions));
 
-                sb.Append('*');
-                sb.Append(extension);
-                i++;
-            }
             if (includeAudioFiles)
             {
-                sb.Append('|');
-                sb.Append(Configuration.Settings.Language.General.AudioFiles);
-                sb.Append("|*.mp3;*.wav;*.wma;*.ogg;*.mpa;*.m4a;*.ape;*.aiff;*.flac;*.aac;*.ac3;*.mka");
+                sb.Append("|" + Configuration.Settings.Language.General.AudioFiles + "|*");
+                sb.Append(string.Join(";*", AudioFileExtensions));
             }
-            sb.Append('|');
-            sb.Append(Configuration.Settings.Language.General.AllFiles);
-            sb.Append("|*.*");
+
+            sb.Append("|" + Configuration.Settings.Language.General.AllFiles + "|*.*");
             return sb.ToString();
         }
 
