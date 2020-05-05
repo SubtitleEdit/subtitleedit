SubtitleEdit/subtitleedit

commit f77c53d651cee5d72b3e1dde5a2091c492ef55d8

Fix changed-status when opening a file from cmd line - thx GCRaistlin :) See http://forum.doom9.net/showthread.php?p=1910300#post1910300

src/Forms/Main.cs
@@ -19733,7 +19733,6 @@ namespace Nikse.SubtitleEdit.Forms
                 labelDuration.Left = numericUpDownDuration.Left;
             }
 
-            _changeSubtitleHash = _subtitle.GetFastHashCode(GetCurrentEncoding().BodyName);
             comboBoxSubtitleFormats.AutoCompleteSource = AutoCompleteSource.ListItems;
             comboBoxSubtitleFormats.AutoCompleteMode = AutoCompleteMode.Append;
             InitializePlayRateDropDown();

commit 01f65e13e4924041596be859c453dcf5ccadf938

Fix title change * for empty file - thx OmrSi :) Regression from commit f77c53d651cee5d72b3e1dde5a2091c492ef55d8 Related to comment https://github.com/SubtitleEdit/subtitleedit/issues/4131#issuecomment-623760280 in #4131

src/Forms/Main.cs
@@ -19783,6 +19783,11 @@ namespace Nikse.SubtitleEdit.Forms
                 labelDuration.Left = numericUpDownDuration.Left;
             }
 
+            if (string.IsNullOrEmpty(_fileName) && (_subtitle == null || _subtitle.Paragraphs.Count == 0))
+            {
+                _changeSubtitleHash = _subtitle.GetFastHashCode(GetCurrentEncoding().BodyName);
+            }
+
             comboBoxSubtitleFormats.AutoCompleteSource = AutoCompleteSource.ListItems;
             comboBoxSubtitleFormats.AutoCompleteMode = AutoCompleteMode.Append;
             InitializePlayRateDropDown();