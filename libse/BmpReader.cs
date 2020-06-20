08a5b1d4c11830d636b2947855f991863d669782
Nikolaj Olsson <nikse.dk@gmail.com>
2020-06-20 08:21:31 +0200

    test appveyor

--- libse/BmpReader.cs
+++ libse/BmpReader.cs
@@ -4,17 +4,16 @@ namespace Nikse.SubtitleEdit.Core
 {
     public class BmpReader
     {
-        public string HeaderId { get; private set; }
-        public UInt32 HeaderFileSize { get; private set; }
-        public UInt32 OffsetToPixelArray { get; private set; }
+        public string HeaderId { get; }
+        public uint HeaderFileSize { get; }
+        public uint OffsetToPixelArray { get; }
 
         public BmpReader(string fileName)
         {
-            byte[] buffer = System.IO.File.ReadAllBytes(fileName);
+            var buffer = System.IO.File.ReadAllBytes(fileName);
             HeaderId = System.Text.Encoding.UTF8.GetString(buffer, 0, 2);
             HeaderFileSize = BitConverter.ToUInt32(buffer, 2);
             OffsetToPixelArray = BitConverter.ToUInt32(buffer, 0xa);
         }
-
     }
 }
