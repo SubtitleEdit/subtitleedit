be06dd9dcf9802c421779e9e6b359060da656cf2
Nikolaj Olsson <nikse.dk@gmail.com>
2020-08-05 08:27:37 +0200

    Allow using libvoikko on Linux

--- src/Logic/SpellCheck/Hunspell.cs
+++ src/Logic/SpellCheck/Hunspell.cs
@@ -8,6 +8,12 @@ namespace Nikse.SubtitleEdit.Logic.SpellCheck
     {
         public static Hunspell GetHunspell(string dictionary)
         {
+            // Finnish uses Voikko (not available via hunspell)
+            if (dictionary.EndsWith("fi_fi", StringComparison.OrdinalIgnoreCase))
+            {
+                return new VoikkoSpellCheck(Configuration.BaseDirectory, Configuration.DictionariesDirectory);
+            }
+
             if (Configuration.IsRunningOnLinux)
             {
                 return new LinuxHunspell(dictionary + ".aff", dictionary + ".dic");
@@ -16,12 +22,6 @@ namespace Nikse.SubtitleEdit.Logic.SpellCheck
             if (Configuration.IsRunningOnMac)
             {
                 return new MacHunspell(dictionary + ".aff", dictionary + ".dic");
-            }
-
-            // Finnish uses Voikko (not available via hunspell)
-            if (dictionary.EndsWith("fi_fi", StringComparison.OrdinalIgnoreCase))
-            {
-                return new VoikkoSpellCheck(Configuration.BaseDirectory, Configuration.DictionariesDirectory);
             }
 
             return new WindowsHunspell(dictionary + ".aff", dictionary + ".dic");
