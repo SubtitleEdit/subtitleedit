8d80d49954b2faad5a9b7f31214fcd69805b197a
Nikolaj Olsson <nikse.dk@gmail.com>
2020-08-14 10:15:06 +0200

    Move scc position/style info table to separate class Somewhat related to #4298

---
+++ libse/SccPositionAndStyle.cs
@@ -0,0 +1,22 @@
+﻿using System.Drawing;
+
+namespace Nikse.SubtitleEdit.Core
+{
+    public class SccPositionAndStyle
+    {
+        public Color ForeColor { get; set; }
+        public FontStyle Style { get; set; }
+        public int X { get; set; }
+        public int Y { get; set; }
+        public string Code { get; set; }
+
+        public SccPositionAndStyle(Color color, FontStyle style, int y, int x, string code)
+        {
+            ForeColor = color;
+            Style = style;
+            X = x;
+            Y = y;
+            Code = code;
+        }
+    }
+}
