2d9108d0a9d2091f931ce9e91429cd5ac06d5bce
Nikolaj Olsson <nikse.dk@gmail.com>
2020-08-16 09:21:57 +0200

    Add new sub format - thx Maria :)

---
+++ libse/SubtitleFormats/FinalCutProXml18.cs
@@ -0,0 +1,11 @@
+﻿namespace Nikse.SubtitleEdit.Core.SubtitleFormats
+{
+
+    public class FinalCutProXml18 : FinalCutProXml15
+    {
+        public FinalCutProXml18()
+        {
+            FcpXmlVersion = "1.8";
+        }
+    }
+}
