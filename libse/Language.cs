23f4d33a8d0adfb7d11d12020d8d74affcfeb760
OmrSi <OmrSi97@gmail.com>
2020-10-25 18:26:16 +0300

    Make getting chapters async

--- libse/Language.cs
+++ libse/Language.cs
@@ -1330,6 +1330,7 @@ namespace Nikse.SubtitleEdit.Core
                 NoSubtitlesFound = "No subtitles found",
                 NotAValidMatroskaFileX = "This is not a valid Matroska file: {0}",
                 BlurayNotSubtitlesFound = "Blu-ray sup file does not contain any subtitles or contains errors - try demuxing again.",
+                GettingChapters = "Getting chapters...",
                 ParsingMatroskaFile = "Parsing Matroska file. Please wait...",
                 ParsingTransportStreamFile = "Parsing Transport Stream file. Please wait...",
                 BeforeImportFromMatroskaFile = "Before import subtitle from Matroska file",
