26f3aad037a6aaa1ac657e29f711704f113704c9
Nikolaj Olsson <nikse.dk@gmail.com>
2020-12-06 13:44:28 +0100

    Trying to fix thread issue

--- src/ui/Logic/SyncContextExtensions.cs
+++ src/ui/Logic/SyncContextExtensions.cs
@@ -7,12 +7,14 @@ namespace Nikse.SubtitleEdit.Logic
     {
         public static void Post(this SynchronizationContext context, TimeSpan delay, Action action)
         {
-            Timer timer = null;
-            timer = new Timer(ignore =>
+            var timer = new System.Windows.Forms.Timer { Interval = delay.Milliseconds };
+            timer.Tick += delegate
             {
-                timer?.Dispose();
-                context.Post(ignore2 => action(), null);
-            }, null, delay, TimeSpan.FromMilliseconds(-1));
+                action.Invoke();
+                timer.Stop();
+                timer.Dispose();
+            };
+            timer.Start();
         }
     }
 }
