a2129c1d0f24357132d2d03c3a72f177e60a3125
OmrSi <OmrSi97@gmail.com>
2020-12-19 01:49:44 +0200

    Add hidden option for hiding list view's gridlines in dark theme

--- src/ui/Logic/DarkTheme.cs
+++ src/ui/Logic/DarkTheme.cs
@@ -276,7 +276,7 @@ namespace Nikse.SubtitleEdit.Logic
             {
                 lv.OwnerDraw = true;
                 lv.DrawColumnHeader += lv_DrawColumnHeader;
-                lv.GridLines = false;
+                lv.GridLines = Configuration.Settings.General.DarkThemeShowListViewGridLines;
                 lv.BackColor = Configuration.Settings.General.DarkThemeBackColor;
                 lv.ForeColor = Configuration.Settings.General.DarkThemeForeColor;
             }
