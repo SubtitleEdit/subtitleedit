228fca68815b64cfc22ccb737ec36ff2b0a8eb40
mikl <mikl@fh-zwickau,de>
2020-12-23 00:30:06 +0100

    refactor change paragraph selection strategy in GenericTranslate.cs (now: only translate selected paragraphs) introduce StringSplitChunkAssigner concept to assign string splits to chunks

--- src/libse/Translate/Processor/StringSplitEngine.cs
+++ src/libse/Translate/Processor/StringSplitEngine.cs
@@ -1,98 +1,43 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
-using System.Xml;
-using Nikse.SubtitleEdit.Core.NetflixQualityCheck;
 
 namespace Nikse.SubtitleEdit.Core.Translate.Processor
 {
-    public interface IInseparableMarker
-    {
-        HashSet<int> FindInseparablePositions(string text);
-    }
-
-    /**
-     * at the moments no nested xml tags are supported.
-     */
-    public class XmlTaggedInseparableMarker : IInseparableMarker
-    {
-        private bool IsOpenTag(Match xmlTag)
-        {
-            return !xmlTag.Value.StartsWith("</");
-        }
-        private bool IsCloseTag(Match xmlTag)
-        {
-            return xmlTag.Value.StartsWith("</");
-        }
-
-        public HashSet<int> FindInseparablePositions(string text)
-        {
-            HashSet<int> inseparablePositions = new HashSet<int>();
-            MatchCollection matchesXmlTags = Regex.Matches(text, @"<[^>]+>");
-            Match openingTag = null;
-            foreach (Match xmlTag in matchesXmlTags)
-            {
-                if (openingTag == null && IsOpenTag(xmlTag))
-                {
-                    openingTag = xmlTag;
-                }
-
-                if (openingTag != null && IsCloseTag(xmlTag))
-                {
-                    var closeTag = xmlTag;
-                    for (int i = openingTag.Index; i < closeTag.Index + closeTag.Length; i++)
-                    {
-                        inseparablePositions.Add(i);
-                    }
-
-                    openingTag = null;
-                }
-            }
-            return inseparablePositions;
-
-        }
-    }
-
-    public class UrlInseparableMarker : IInseparableMarker
-    {
-        public HashSet<int> FindInseparablePositions(string text)
-        {
-            HashSet<int> inseparablePositions = new HashSet<int>();
-            MatchCollection matches = Regex.Matches(text, @"\b(?:https?://|www\.)\S+\b");
-            foreach (Match match in matches)
-            {
-                for (int i = match.Index; i < match.Index + match.Length; i++)
-                {
-                    inseparablePositions.Add(i);
-                }
-            }
-            return inseparablePositions;
-        }
-    }
-
-
     public class StringSplitEngine
     {
-        private readonly char[] _delimiters;
-        private List<IInseparableMarker> _inseparableMarkers = new List<IInseparableMarker>();
+        private char[] DelimitersToSplitAfter { get; }
+        private char[] DelimitersToSplitBefore { get; }
+        private readonly List<IUnsplittableMarker> _unsplittableMarkers = new List<IUnsplittableMarker>();
 
-        public StringSplitEngine(char[] delimiters)
+        public StringSplitEngine(char[] delimitersToSplitAfter, char[] delimitersToSplitBefore)
         {
-            this._delimiters = delimiters;
-            AddInseparableMarker(new UrlInseparableMarker());
-            AddInseparableMarker(new XmlTaggedInseparableMarker());
+            DelimitersToSplitAfter = delimitersToSplitAfter;
+            DelimitersToSplitBefore = delimitersToSplitBefore;
+            InitUnsplittableMarkers();
         }
 
-        public void AddInseparableMarker(IInseparableMarker inseparableMarker)
+        public StringSplitEngine(char[] delimitersToSplitAfter) : this(delimitersToSplitAfter, new char[0])
         {
-            _inseparableMarkers.Add(inseparableMarker);
         }
 
-        public IEnumerable<int> FindAllPositionsOf(string text)
+        private void InitUnsplittableMarkers()
         {
+            AddUnsplittableMarker(new UrlUnsplittableMarker());
+            AddUnsplittableMarker(new XmlTaggedUnsplittableMarker());
+        }
+
+        public void AddUnsplittableMarker(IUnsplittableMarker unsplittableMarker)
+        {
+            _unsplittableMarkers.Add(unsplittableMarker);
+        }
+
+        public IEnumerable<int> FindAllSplitPositions(string text)
+        {
+            var unsplittablePositions = EvaluateUnsplittablePositions(text);
             for (int i = 0; i < text.Length; i++)
             {
-                if (_delimiters.Contains(text[i]))
+                if (IsSplittable(text, i, unsplittablePositions))
                 {
                     yield return i;
                 }
@@ -111,26 +56,113 @@ namespace Nikse.SubtitleEdit.Core.Translate.Processor
             return output;
         }
 
-        public List<string> split(string text)
+        public List<string> Split(string text)
         {
-            var splitPositions = FindAllPositionsOf(text);
-            var inseparablePositions = EvaluateInseparablePositions(text);
-            splitPositions =splitPositions.Except(inseparablePositions);
-
-            List<string> sentenceChunks = SplitAt(text, splitPositions.ToList()).ToList();
-            sentenceChunks.RemoveAll(x => x.Trim().Length == 0); //remove empty chunks
-
-            return sentenceChunks;
+            var splitPositions = FindAllSplitPositions(text);
+            var splits = SplitAt(text, splitPositions.ToList()).ToList();
+            splits.RemoveAll(x => x.Trim().Length == 0); //remove potential empty splits
+            return splits;
         }
 
-        public HashSet<int> EvaluateInseparablePositions(string text)
+        public HashSet<int> EvaluateUnsplittablePositions(string text)
         {
-            HashSet<int> inseparablePositions = new HashSet<int>();
-            foreach (var inseparableMarker in _inseparableMarkers)
+            HashSet<int> unsplittablePositions = new HashSet<int>();
+            foreach (var unsplittableMarker in _unsplittableMarkers)
             {
-                inseparablePositions.UnionWith(inseparableMarker.FindInseparablePositions(text));
+                unsplittablePositions.UnionWith(unsplittableMarker.FindUnsplittablePositions(text));
             }
-            return inseparablePositions;
+            return unsplittablePositions;
+        }
+
+        private bool IsSplittable(string text, int potentialSplitPosition, HashSet<int> unsplittablePositions)
+        {
+            if (unsplittablePositions.Contains(potentialSplitPosition))
+            {
+                return false;
+            }
+            if (potentialSplitPosition>0 && potentialSplitPosition < text.Length  && DelimitersToSplitAfter.Contains(text[potentialSplitPosition-1]))
+            {
+                return true;
+            }
+            if (potentialSplitPosition < text.Length && DelimitersToSplitBefore.Contains(text[potentialSplitPosition ]))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool IsSplittable(string text, int potentialSplitPosition)
+        {
+            var unsplittablePositions = EvaluateUnsplittablePositions(text);
+            return IsSplittable(text, potentialSplitPosition, unsplittablePositions);
+        }
+    }
+
+    /// <summary>
+    /// concept to define rules for StringSplitEngine to exclude specific string patterns
+    /// from splitting (despite the presence of potentiality included split delimiters)
+    /// </summary>
+    public interface IUnsplittableMarker
+    {
+        HashSet<int> FindUnsplittablePositions(string text);
+    }
+
+    /**
+     * marks opening & following closing xml tags together with encapsulated text as unsplittable 
+     * (at the moment nested xml tags are NOT supported.)
+     */
+    public class XmlTaggedUnsplittableMarker : IUnsplittableMarker
+    {
+        private bool IsOpenTag(Match xmlTag)
+        {
+            return !xmlTag.Value.StartsWith("</");
+        }
+        private bool IsCloseTag(Match xmlTag)
+        {
+            return xmlTag.Value.StartsWith("</");
+        }
+
+        public HashSet<int> FindUnsplittablePositions(string text)
+        {
+            HashSet<int> unsplittablePositions = new HashSet<int>();
+            MatchCollection matchesXmlTags = Regex.Matches(text, @"<[^>]+>"); //regex to find tags encapsulated by angle brackets
+            Match openingTag = null;
+            foreach (Match xmlTag in matchesXmlTags)
+            {
+                if (openingTag == null && IsOpenTag(xmlTag))
+                {
+                    openingTag = xmlTag;
+                }
+
+                if (openingTag != null && IsCloseTag(xmlTag))
+                {
+                    var closeTag = xmlTag;
+                    for (int i = openingTag.Index; i < closeTag.Index + closeTag.Length; i++)
+                    {
+                        unsplittablePositions.Add(i);
+                    }
+
+                    openingTag = null;
+                }
+            }
+            return unsplittablePositions;
+        }
+    }
+
+    public class UrlUnsplittableMarker : IUnsplittableMarker
+    {
+        public HashSet<int> FindUnsplittablePositions(string text)
+        {
+            HashSet<int> unsplittablePositions = new HashSet<int>();
+            MatchCollection matches = Regex.Matches(text, @"\b(?:https?://|www\.)\S+\b"); //regex to find URLs
+            foreach (Match match in matches)
+            {
+                for (int i = match.Index; i < match.Index + match.Length; i++)
+                {
+                    unsplittablePositions.Add(i);
+                }
+            }
+            return unsplittablePositions;
         }
     }
 }
