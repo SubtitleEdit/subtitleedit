7f1e1ad50a10192f78d63d90e4b1385678f2d165
Nikolaj Olsson <nikse.dk@gmail.com>
2021-01-23 17:37:01 +0100

    A bit more unit testing

--- src/Test/SETextBoxControl/SeTextBoxTests.cs
+++ src/Test/SETextBoxControl/SeTextBoxTests.cs
@@ -24,6 +24,7 @@ namespace Test.Core
             tbRichText.SelectionStart = 0;
             tbRichText.SelectionLength = 3;
 
+            Assert.AreEqual("How", tbRichText.SelectedText);
             Assert.AreEqual(tbNormal.Text, tbRichText.Text);
             Assert.AreEqual(tbNormal.SelectedText, tbRichText.SelectedText);
             Assert.AreEqual(tbNormal.SelectionStart, tbRichText.SelectionStart);
@@ -46,10 +47,61 @@ namespace Test.Core
             tbRichText.SelectionStart = 5;
             tbRichText.SelectionLength = 3;
 
+            Assert.AreEqual("are", tbRichText.SelectedText);
             Assert.AreEqual(tbNormal.Text, tbRichText.Text);
             Assert.AreEqual(tbNormal.SelectedText, tbRichText.SelectedText);
             Assert.AreEqual(tbNormal.SelectionStart, tbRichText.SelectionStart);
             Assert.AreEqual(tbNormal.SelectionLength, tbRichText.SelectionLength);
         }
+
+        [TestMethod]
+        public void SelectionStartThirdLine()
+        {
+            Configuration.Settings.General.SubtitleTextBoxSyntaxColor = false;
+            var tbNormal = new SETextBox();
+            Configuration.Settings.General.SubtitleTextBoxSyntaxColor = true;
+            var tbRichText = new SETextBox();
+
+            tbNormal.Text = "How" + Environment.NewLine + "are you" + Environment.NewLine + "today?";
+            tbRichText.Text = "How" + Environment.NewLine + "are you" + Environment.NewLine + "today?";
+
+            tbNormal.SelectionStart = 14;
+            tbNormal.SelectionLength = 5;
+            tbRichText.SelectionStart = 14;
+            tbRichText.SelectionLength = 5;
+
+            Assert.AreEqual("today", tbRichText.SelectedText);
+            Assert.AreEqual(tbNormal.Text, tbRichText.Text);
+            Assert.AreEqual(tbNormal.SelectedText, tbRichText.SelectedText);
+            Assert.AreEqual(tbNormal.SelectionStart, tbRichText.SelectionStart);
+            Assert.AreEqual(tbNormal.SelectionLength, tbRichText.SelectionLength);
+        }
+
+        [TestMethod]
+        public void SelectedTextThirdLine()
+        {
+            Configuration.Settings.General.SubtitleTextBoxSyntaxColor = false;
+            var tbNormal = new SETextBox();
+            Configuration.Settings.General.SubtitleTextBoxSyntaxColor = true;
+            var tbRichText = new SETextBox();
+
+            tbNormal.Text = "How" + Environment.NewLine + "are you" + Environment.NewLine + "today?";
+            tbRichText.Text = "How" + Environment.NewLine + "are you" + Environment.NewLine + "today?";
+
+            tbNormal.SelectionStart = 14;
+            tbNormal.SelectionLength = 5;
+            tbRichText.SelectionStart = 14;
+            tbRichText.SelectionLength = 5;
+
+            tbNormal.SelectedText = "tomorrow";
+            tbRichText.SelectedText = "tomorrow";
+
+            Assert.AreEqual("How" + Environment.NewLine + "are you" + Environment.NewLine + "tomorrow?", tbRichText.Text);
+            Assert.AreEqual(tbNormal.Text, tbRichText.Text);
+            Assert.AreEqual(tbNormal.SelectedText, tbRichText.SelectedText);
+            Assert.AreEqual(tbNormal.SelectionStart, tbRichText.SelectionStart);
+            Assert.AreEqual(tbNormal.SelectionLength, tbRichText.SelectionLength);
+        }
+
     }
 }
