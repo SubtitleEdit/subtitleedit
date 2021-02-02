82175c8e7299157847a64e81e1266df83340ac1a
Nikolaj Olsson <nikse.dk@gmail.com>
2021-02-02 11:18:11 +0100

    Work on unit tests

---
+++ src/Test/Core/UUEncodingTest.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nikse.SubtitleEdit.Core.Common;
+
+namespace Test.Core
+{
+    [TestClass]
+    public class UUEncodingTest
+    {
+        [TestMethod]
+        public void ForwardAndBackAgain()
+        {
+            var byteArray = new byte[byte.MaxValue];
+            for (int i = byte.MinValue; i < byte.MaxValue; i++)
+            {
+                byteArray[i] = (byte)i;
+            }
+
+            var text = UUEncoding.UUEncode(byteArray);
+            var newBytes = UUEncoding.UUDecode(text);
+
+            Assert.AreEqual(byteArray.Length, newBytes.Length);
+            for (int i = byte.MinValue; i < byte.MaxValue; i++)
+            {
+                Assert.AreEqual(byteArray[i], newBytes[i]);
+            }
+        }
+    }
+}
