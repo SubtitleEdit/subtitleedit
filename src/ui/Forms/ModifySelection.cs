e6da6a1420b3ab62506d6ea6aebbd7c71fb85ac0
OmrSi <omrsi97@gmail.com>
2021-03-09 14:26:31 +0200

    Fix for modify selection last column size
    It used to leave a white part.

--- src/ui/Forms/ModifySelection.cs
+++ src/ui/Forms/ModifySelection.cs
@@ -615,15 +615,18 @@ namespace Nikse.SubtitleEdit.Forms
                                                  - listViewFixes.Columns[1].Width
                                                  - listViewFixes.Columns[3].Width
                                                  - 30;
-                return;
+            }
+            else
+            {
+                listViewFixes.Columns[0].Width = 50;
+                listViewFixes.Columns[1].Width = 80;
+                listViewFixes.Columns[2].Width = listViewFixes.Width
+                                                 - listViewFixes.Columns[0].Width
+                                                 - listViewFixes.Columns[1].Width
+                                                 - 30;
             }
 
-            listViewFixes.Columns[0].Width = 50;
-            listViewFixes.Columns[1].Width = 80;
-            listViewFixes.Columns[2].Width = listViewFixes.Width
-                                             - listViewFixes.Columns[0].Width
-                                             - listViewFixes.Columns[1].Width
-                                             - 30;
+            listViewFixes.Columns[listViewFixes.Columns.Count - 1].Width = -2;
         }
     }
 }
