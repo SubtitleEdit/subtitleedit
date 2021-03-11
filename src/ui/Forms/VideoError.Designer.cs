c0259de74d7182269c3b6e5125dfa70874d89032
Nikolaj Olsson <nikse.dk@gmail.com>
2021-03-11 07:47:46 +0100

    simplify video error dialog

--- src/ui/Forms/VideoError.Designer.cs
+++ src/ui/Forms/VideoError.Designer.cs
@@ -28,62 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.textBoxError = new System.Windows.Forms.TextBox();
-            this.richTextBoxMessage = new System.Windows.Forms.RichTextBox();
             this.buttonCancel = new System.Windows.Forms.Button();
             this.buttonMpvSettings = new System.Windows.Forms.Button();
             this.labelMpvInfo = new System.Windows.Forms.Label();
-            this.groupBox1.SuspendLayout();
+            this.labelError = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // groupBox1
-            // 
-            this.groupBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.groupBox1.Controls.Add(this.textBoxError);
-            this.groupBox1.Location = new System.Drawing.Point(12, 180);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(674, 142);
-            this.groupBox1.TabIndex = 0;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Error message";
-            // 
-            // textBoxError
-            // 
-            this.textBoxError.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.textBoxError.Location = new System.Drawing.Point(7, 20);
-            this.textBoxError.Multiline = true;
-            this.textBoxError.Name = "textBoxError";
-            this.textBoxError.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBoxError.Size = new System.Drawing.Size(661, 116);
-            this.textBoxError.TabIndex = 10;
-            // 
-            // richTextBoxMessage
-            // 
-            this.richTextBoxMessage.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.richTextBoxMessage.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.richTextBoxMessage.Cursor = System.Windows.Forms.Cursors.Arrow;
-            this.richTextBoxMessage.Location = new System.Drawing.Point(13, 23);
-            this.richTextBoxMessage.Name = "richTextBoxMessage";
-            this.richTextBoxMessage.ReadOnly = true;
-            this.richTextBoxMessage.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
-            this.richTextBoxMessage.Size = new System.Drawing.Size(674, 87);
-            this.richTextBoxMessage.TabIndex = 1;
-            this.richTextBoxMessage.TabStop = false;
-            this.richTextBoxMessage.Text = "";
-            this.richTextBoxMessage.LinkClicked += new System.Windows.Forms.LinkClickedEventHandler(this.richTextBoxMessage_LinkClicked);
             // 
             // buttonCancel
             // 
-            this.buttonCancel.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.buttonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
             this.buttonCancel.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.buttonCancel.Location = new System.Drawing.Point(612, 328);
+            this.buttonCancel.Location = new System.Drawing.Point(493, 144);
             this.buttonCancel.Name = "buttonCancel";
             this.buttonCancel.Size = new System.Drawing.Size(75, 23);
             this.buttonCancel.TabIndex = 0;
@@ -93,7 +49,7 @@
             // buttonMpvSettings
             // 
             this.buttonMpvSettings.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonMpvSettings.Location = new System.Drawing.Point(20, 134);
+            this.buttonMpvSettings.Location = new System.Drawing.Point(20, 95);
             this.buttonMpvSettings.Name = "buttonMpvSettings";
             this.buttonMpvSettings.Size = new System.Drawing.Size(272, 23);
             this.buttonMpvSettings.TabIndex = 30;
@@ -104,47 +60,50 @@
             // labelMpvInfo
             // 
             this.labelMpvInfo.AutoSize = true;
-            this.labelMpvInfo.Location = new System.Drawing.Point(17, 118);
+            this.labelMpvInfo.Location = new System.Drawing.Point(17, 79);
             this.labelMpvInfo.Name = "labelMpvInfo";
             this.labelMpvInfo.Size = new System.Drawing.Size(69, 13);
             this.labelMpvInfo.TabIndex = 31;
             this.labelMpvInfo.Text = "labelMpvInfo";
             // 
+            // labelError
+            // 
+            this.labelError.AutoSize = true;
+            this.labelError.Location = new System.Drawing.Point(17, 24);
+            this.labelError.Name = "labelError";
+            this.labelError.Size = new System.Drawing.Size(53, 13);
+            this.labelError.TabIndex = 32;
+            this.labelError.Text = "labelError";
+            // 
             // VideoError
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(699, 361);
+            this.ClientSize = new System.Drawing.Size(580, 179);
+            this.Controls.Add(this.labelError);
             this.Controls.Add(this.labelMpvInfo);
             this.Controls.Add(this.buttonMpvSettings);
             this.Controls.Add(this.buttonCancel);
-            this.Controls.Add(this.richTextBoxMessage);
-            this.Controls.Add(this.groupBox1);
             this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.KeyPreview = true;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
-            this.MinimumSize = new System.Drawing.Size(700, 350);
             this.Name = "VideoError";
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
             this.Text = "Error playing video file - ";
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.VideoError_KeyDown);
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.TextBox textBoxError;
-        private System.Windows.Forms.RichTextBox richTextBoxMessage;
         private System.Windows.Forms.Button buttonCancel;
         private System.Windows.Forms.Button buttonMpvSettings;
         private System.Windows.Forms.Label labelMpvInfo;
+        private System.Windows.Forms.Label labelError;
     }
 }
