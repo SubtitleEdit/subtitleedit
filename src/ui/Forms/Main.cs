e351da4e05b56d8e4df2fe155f49a334d2cacd24
Nikolaj Olsson <nikse.dk@gmail.com>
2021-05-04 07:23:03 +0200

    Allow Unicode hidden space chars when using spell check replace - thx zildan :) Fix #5020

--- src/ui/Forms/Main.cs
+++ src/ui/Forms/Main.cs
@@ -7723,7 +7723,7 @@ namespace Nikse.SubtitleEdit.Forms
                 if (firstChange)
                 {
                     MakeHistoryForUndo(_language.BeforeSpellCheck);
-                    firstChange = false;
+                    firstChange = false; 
                 }
 
                 int startIndex = p.Text.IndexOf(oldWord, StringComparison.Ordinal);
@@ -7735,14 +7735,14 @@ namespace Nikse.SubtitleEdit.Forms
                 while (startIndex >= 0 && startIndex < p.Text.Length && p.Text.Substring(startIndex).Contains(oldWord))
                 {
                     bool startOk = startIndex == 0 ||
-                                   "«»“” <>-—+/'\"[](){}¿¡….,;:!?%&$£\r\n؛،؟\u200E\u200F\u202A\u202B\u202C\u202D\u202E\u00A0".Contains(p.Text[startIndex - 1]) ||
+                                   "«»“” <>-—+/'\"[](){}¿¡….,;:!?%&$£\r\n؛،؟\u200E\u200F\u202A\u202B\u202C\u202D\u202E\u00A0\u200B\uFEFF".Contains(p.Text[startIndex - 1]) ||
                                    char.IsPunctuation(p.Text[startIndex - 1]) ||
                                    startIndex == p.Text.Length - oldWord.Length;
                     if (startOk)
                     {
                         int end = startIndex + oldWord.Length;
                         if (end <= p.Text.Length && end == p.Text.Length ||
-                            "«»“” ,.!?:;'()<>\"-—+/[]{}%&$£…\r\n؛،؟\u200E\u200F\u202A\u202B\u202C\u202D\u202E\u00A0".Contains(p.Text[end]) ||
+                            "«»“” ,.!?:;'()<>\"-—+/[]{}%&$£…\r\n؛،؟\u200E\u200F\u202A\u202B\u202C\u202D\u202E\u00A0\u200B\uFEFF".Contains(p.Text[end]) ||
                             char.IsPunctuation(p.Text[end]))
                         {
                             var lengthBefore = p.Text.Length;
