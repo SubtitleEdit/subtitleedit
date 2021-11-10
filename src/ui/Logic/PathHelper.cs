f081b700f6a5ce0e0c2d46c9410a972d454b0d52
niksedk <nikse.dk@gmail.com>
2021-11-10 23:29:38 +0100

    Don't let file name overflow in Gen waveform - thx Jamakmake :)

---
+++ src/ui/Logic/PathHelper.cs
@@ -0,0 +1,44 @@
+﻿using System.Drawing;
+using System.IO;
+using System.Linq;
+
+namespace Nikse.SubtitleEdit.Logic
+{
+    public static class PathHelper
+    {
+        public static string ShortenPath(Graphics g, Font font, string path, int pixelMaxLength)
+        {
+            var s = path;
+            while (g.MeasureString(s, font).Width > pixelMaxLength)
+            {
+                var arr = s.Split(Path.DirectorySeparatorChar).ToList();
+                if (arr.Count < 3)
+                {
+                    return Path.GetFileName(path);
+                }
+
+                var middle = arr.Count / 2 - 1;
+                if (middle == 0)
+                {
+                    middle++;
+                }
+
+                while (arr[middle] == "..." && middle < arr.Count - 1)
+                {
+                    middle++;
+                }
+
+                if (arr[middle] == "..." || middle >= arr.Count - 1)
+                {
+                    return Path.GetFileName(path);
+                }
+
+                arr[middle] = "...";
+                s = string.Join(Path.DirectorySeparatorChar.ToString(), arr);
+                s = s.Replace("\\...\\...", "\\...");
+            }
+
+            return s;
+        }
+    }
+}
