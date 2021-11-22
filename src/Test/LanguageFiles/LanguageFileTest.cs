1b97f126509834a853b833bce1cca6a3c078f97b
niksedk <nikse.dk@gmail.com>
2021-11-22 16:42:33 +0100

    Fix language file + add unit test - thx Leon :) Fix  #5510

---
+++ src/Test/LanguageFiles/LanguageFileTest.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using System.Xml;
+
+namespace Test.Logic.Dictionaries
+{
+    [TestClass]
+    public class LanguageFileTest
+    {
+        [TestMethod]
+        public void TestLanguageFiles()
+        {
+            var languageDir = Path.Combine(Environment.CurrentDirectory, "Languages");
+            var files = Directory.GetFiles(languageDir, "*.xml");
+
+            foreach (var fileName in files)
+            {
+                var xml = new XmlDocument();
+                try
+                {
+                    xml.Load(fileName);
+
+                    var cultureName = xml.DocumentElement.SelectSingleNode("General/CultureName").InnerText;
+                    var fileNameStart = Path.GetFileName(fileName).Substring(0, cultureName.Length);
+                    Assert.AreEqual(fileNameStart, cultureName);
+                }
+                catch (Exception exception)
+                {
+                    throw new Exception($"Unable to load language file {fileName} : {exception.Message}");
+                }
+            }
+        }
+    }
+}
