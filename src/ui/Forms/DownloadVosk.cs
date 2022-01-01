413ebc346c2c58c1035ae03e3f0b720b35a17f2f
niksedk <nikse.dk@gmail.com>
2022-01-01 16:46:50 +0100

    work on vosk

---
+++ src/ui/Forms/DownloadVosk.cs
@@ -0,0 +1,125 @@
+﻿using Nikse.SubtitleEdit.Core.Common;
+using Nikse.SubtitleEdit.Logic;
+using System;
+using System.IO;
+using System.IO.Compression;
+using System.Net;
+using System.Windows.Forms;
+
+namespace Nikse.SubtitleEdit.Forms
+{
+    public sealed partial class DownloadVosk : Form
+    {
+        public bool AutoClose { get; internal set; }
+
+        public DownloadVosk()
+        {
+            UiUtil.PreInitialize(this);
+            InitializeComponent();
+            UiUtil.FixFonts(this);
+            Text = string.Format(LanguageSettings.Current.SettingsFfmpeg.XDownload, "FFmpeg");
+            buttonOK.Text = LanguageSettings.Current.General.Ok;
+            buttonCancel.Text = LanguageSettings.Current.General.Cancel;
+            UiUtil.FixLargeFonts(this, buttonOK);
+        }
+
+        private void DownloadFfmpeg_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Escape)
+            {
+                DialogResult = DialogResult.Cancel;
+            }
+        }
+
+        private void buttonOK_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.OK;
+        }
+
+        private void buttonCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+        }
+
+        private void DownloadFfmpeg_Shown(object sender, EventArgs e)
+        {
+            try
+            {
+                labelPleaseWait.Text = LanguageSettings.Current.General.PleaseWait;
+                buttonOK.Enabled = false;
+                Refresh();
+                Cursor = Cursors.WaitCursor;
+                var url = "https://github.com/SubtitleEdit/support-files/raw/master/vosk/vosk" + IntPtr.Size * 8 + ".tar.gzip";
+                var wc = new WebClient { Proxy = Utilities.GetProxy() };
+
+                wc.DownloadDataCompleted += wc_DownloadDataCompleted;
+                wc.DownloadProgressChanged += (o, args) =>
+                {
+                    labelPleaseWait.Text = LanguageSettings.Current.General.PleaseWait + "  " + args.ProgressPercentage + "%";
+                };
+                wc.DownloadDataAsync(new Uri(url));
+            }
+            catch (Exception exception)
+            {
+                labelPleaseWait.Text = string.Empty;
+                buttonOK.Enabled = true;
+                Cursor = Cursors.Default;
+                MessageBox.Show(exception.Message + Environment.NewLine + Environment.NewLine + exception.StackTrace);
+            }
+        }
+
+        private void wc_DownloadDataCompleted(object sender, DownloadDataCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                labelPleaseWait.Text = string.Format(LanguageSettings.Current.SettingsFfmpeg.XDownloadFailed, "ffmpeg");
+                buttonOK.Enabled = true;
+                Cursor = Cursors.Default;
+                return;
+            }
+
+            var folder = Configuration.DataDirectory;
+            var tempFileName = FileUtil.GetTempFileName(".tar");
+            using (var ms = new MemoryStream(e.Result))
+            using (var fs = new FileStream(tempFileName, FileMode.Create))
+            using (var zip = new GZipStream(ms, CompressionMode.Decompress))
+            {
+                byte[] buffer = new byte[1024];
+                int nRead;
+                while ((nRead = zip.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    fs.Write(buffer, 0, nRead);
+                }
+            }
+
+            using (var tr = new TarReader(tempFileName))
+            {
+                foreach (var th in tr.Files)
+                {
+                    string fn = Path.Combine(folder, th.FileName.Replace('/', Path.DirectorySeparatorChar));
+                    if (th.IsFolder)
+                    {
+                        Directory.CreateDirectory(Path.Combine(folder, th.FileName.Replace('/', Path.DirectorySeparatorChar)));
+                    }
+                    else if (th.FileSizeInBytes > 0)
+                    {
+                        th.WriteData(fn);
+                    }
+                }
+            }
+            File.Delete(tempFileName);
+
+            Cursor = Cursors.Default;
+            labelPleaseWait.Text = string.Empty;
+
+            if (AutoClose)
+            {
+                DialogResult = DialogResult.OK;
+                return;
+            }
+
+            buttonOK.Enabled = true;
+            labelPleaseWait.Text = string.Format(LanguageSettings.Current.SettingsFfmpeg.XDownloadOk, "Vosk");
+        }
+    }
+}
