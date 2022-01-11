7f411089a5e3bfc8aef72dd70b9dfc23228d8011
niksedk <nikse.dk@gmail.com>
2022-01-11 16:54:42 +0100

    Make buttons in alignment windows a little wider - thx hugok79 :) Fix #5694

--- src/ui/Forms/AlignmentPicker.Designer.cs
+++ src/ui/Forms/AlignmentPicker.Designer.cs
@@ -56,23 +56,23 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(309, 106);
+            this.panel1.Size = new System.Drawing.Size(380, 106);
             this.panel1.TabIndex = 0;
             // 
             // button9
             // 
-            this.button9.Location = new System.Drawing.Point(202, 68);
+            this.button9.Location = new System.Drawing.Point(252, 68);
             this.button9.Name = "button9";
-            this.button9.Size = new System.Drawing.Size(90, 23);
+            this.button9.Size = new System.Drawing.Size(115, 23);
             this.button9.TabIndex = 9;
             this.button9.UseVisualStyleBackColor = true;
             this.button9.Click += new System.EventHandler(this.button9_Click);
             // 
             // button8
             // 
-            this.button8.Location = new System.Drawing.Point(106, 68);
+            this.button8.Location = new System.Drawing.Point(131, 68);
             this.button8.Name = "button8";
-            this.button8.Size = new System.Drawing.Size(90, 23);
+            this.button8.Size = new System.Drawing.Size(115, 23);
             this.button8.TabIndex = 8;
             this.button8.UseVisualStyleBackColor = true;
             this.button8.Click += new System.EventHandler(this.button8_Click);
@@ -81,25 +81,25 @@
             // 
             this.button7.Location = new System.Drawing.Point(10, 68);
             this.button7.Name = "button7";
-            this.button7.Size = new System.Drawing.Size(90, 23);
+            this.button7.Size = new System.Drawing.Size(115, 23);
             this.button7.TabIndex = 7;
             this.button7.UseVisualStyleBackColor = true;
             this.button7.Click += new System.EventHandler(this.button7_Click);
             // 
             // button6
             // 
-            this.button6.Location = new System.Drawing.Point(202, 39);
+            this.button6.Location = new System.Drawing.Point(252, 39);
             this.button6.Name = "button6";
-            this.button6.Size = new System.Drawing.Size(90, 23);
+            this.button6.Size = new System.Drawing.Size(115, 23);
             this.button6.TabIndex = 6;
             this.button6.UseVisualStyleBackColor = true;
             this.button6.Click += new System.EventHandler(this.button6_Click);
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(106, 39);
+            this.button5.Location = new System.Drawing.Point(131, 39);
             this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(90, 23);
+            this.button5.Size = new System.Drawing.Size(115, 23);
             this.button5.TabIndex = 5;
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.button5_Click);
@@ -108,25 +108,25 @@
             // 
             this.button4.Location = new System.Drawing.Point(10, 39);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(90, 23);
+            this.button4.Size = new System.Drawing.Size(115, 23);
             this.button4.TabIndex = 4;
             this.button4.UseVisualStyleBackColor = true;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(202, 10);
+            this.button3.Location = new System.Drawing.Point(252, 10);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(90, 23);
+            this.button3.Size = new System.Drawing.Size(115, 23);
             this.button3.TabIndex = 3;
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(106, 10);
+            this.button2.Location = new System.Drawing.Point(131, 10);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(90, 23);
+            this.button2.Size = new System.Drawing.Size(115, 23);
             this.button2.TabIndex = 2;
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
@@ -135,7 +135,7 @@
             // 
             this.button1.Location = new System.Drawing.Point(10, 10);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(90, 23);
+            this.button1.Size = new System.Drawing.Size(115, 23);
             this.button1.TabIndex = 1;
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
@@ -144,7 +144,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(309, 106);
+            this.ClientSize = new System.Drawing.Size(380, 106);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
             this.KeyPreview = true;
