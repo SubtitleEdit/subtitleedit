e0615bdef3c71c2fb9ac3fe78b050c7a0a575df0
niksedk <nikse.dk@gmail.com>
2022-01-20 07:04:51 +0100

    Minor refact

---
+++ src/libse/AudioToText/DownloadModel.cs
@@ -0,0 +1,132 @@
+﻿namespace Nikse.SubtitleEdit.Core.AudioToText
+{
+    public class DownloadModel
+    {
+        public string Url { get; set; }
+        public string TwoLetterLanguageCode { get; set; }
+        public string LanguageName { get; set; }
+
+        public override string ToString()
+        {
+            return LanguageName;
+        }
+
+        public static readonly DownloadModel[] VoskModels = new[]
+        {
+            new DownloadModel
+            {
+                TwoLetterLanguageCode = "en",
+                LanguageName = "English",
+                Url = "https://alphacephei.com/vosk/models/vosk-model-en-us-0.22-lgraph.zip",
+            },
+            new DownloadModel
+            {
+                TwoLetterLanguageCode = "cn",
+                LanguageName = "Chinese",
+                Url = "https://alphacephei.com/vosk/models/vosk-model-cn-kaldi-multicn-2-lgraph.zip",
+            },
+            new DownloadModel
+            {
+                TwoLetterLanguageCode = "fr",
+                LanguageName = "French",
+                Url = "https://alphacephei.com/vosk/models/vosk-model-small-fr-pguyot-0.3.zip",
+            },
+            new DownloadModel
+            {
+                TwoLetterLanguageCode = "es",
+                LanguageName = "Spanish",
+                Url = "https://alphacephei.com/vosk/models/vosk-model-small-es-0.3.zip",
+            },
+            new DownloadModel
+            {
+                TwoLetterLanguageCode = "de",
+                LanguageName = "German",
+                Url = "https://alphacephei.com/vosk/models/vosk-model-small-de-0.15.zip",
+            },
+            new DownloadModel
+            {
+                TwoLetterLanguageCode = "pt",
+                LanguageName = "Portuguese",
+                Url = "https://alphacephei.com/vosk/models/vosk-model-small-pt-0.3.zip",
+            },
+            new DownloadModel
+            {
+                TwoLetterLanguageCode = "it",
+                LanguageName = "Italian",
+                Url = "https://alphacephei.com/vosk/models/vosk-model-small-it-0.4.zip",
+            },
+            new DownloadModel
+            {
+                TwoLetterLanguageCode = "nl",
+                LanguageName = "Dutch",
+                Url = "https://alphacephei.com/vosk/models/vosk-model-nl-spraakherkenning-0.6-lgraph.zip",
+            },
+            new DownloadModel
+            {
+                TwoLetterLanguageCode = "sv",
+                LanguageName = "Swedish",
+                Url = "https://alphacephei.com/vosk/models/vosk-model-small-sv-rhasspy-0.15.zip",
+            },
+            new DownloadModel
+            {
+                TwoLetterLanguageCode = "ru",
+                LanguageName = "Russian",
+                Url = "https://alphacephei.com/vosk/models/vosk-model-small-ru-0.22.zip",
+            },
+            new DownloadModel
+            {
+                TwoLetterLanguageCode = "fa",
+                LanguageName = "Farsi",
+                Url = "https://alphacephei.com/vosk/models/vosk-model-small-fa-0.5.zip",
+            },
+            new DownloadModel
+            {
+                TwoLetterLanguageCode = "tr",
+                LanguageName = "Turkish",
+                Url = "https://alphacephei.com/vosk/models/vosk-model-small-tr-0.3.zip",
+            },
+            new DownloadModel
+            {
+                TwoLetterLanguageCode = "el",
+                LanguageName = "Greek",
+                Url = "https://alphacephei.com/vosk/models/vosk-model-el-gr-0.7.zip",
+            },
+            new DownloadModel
+            {
+                TwoLetterLanguageCode = "ar",
+                LanguageName = "Arabic",
+                Url = "https://alphacephei.com/vosk/models/vosk-model-ar-mgb2-0.4.zip",
+            },
+            new DownloadModel
+            {
+                TwoLetterLanguageCode = "uk",
+                LanguageName = "Ukrainian",
+                Url = "https://alphacephei.com/vosk/models/vosk-model-small-uk-v3-small.zip",
+            },
+            new DownloadModel
+            {
+                TwoLetterLanguageCode = "ph",
+                LanguageName = "Filipino",
+                Url = "https://alphacephei.com/vosk/models/vosk-model-tl-ph-generic-0.6.zip",
+            },
+            new DownloadModel
+            {
+                TwoLetterLanguageCode = "kz",
+                LanguageName = "Kazakh",
+                Url = "https://alphacephei.com/vosk/models/vosk-model-kz-0.15.zip",
+            },
+            new DownloadModel
+            {
+                TwoLetterLanguageCode = "jp",
+                LanguageName = "Japanese",
+                Url = "https://alphacephei.com/vosk/models/vosk-model-small-ja-0.22.zip",
+            },
+            new DownloadModel
+            {
+                TwoLetterLanguageCode = "ca",
+                LanguageName = "Catalan",
+                Url = "https://alphacephei.com/vosk/models/vosk-model-small-ca-0.4.zip",
+            },
+        };
+    }
+}
--- src/ui/Forms/AudioToTextModelDownload.cs
+++ src/ui/Forms/AudioToTextModelDownload.cs
@@ -1,4 +1,5 @@
-﻿using Nikse.SubtitleEdit.Core.Common;
+﻿using Nikse.SubtitleEdit.Core.AudioToText;
+using Nikse.SubtitleEdit.Core.Common;
 using Nikse.SubtitleEdit.Logic;
 using System;
 using System.IO;
@@ -11,135 +12,6 @@ namespace Nikse.SubtitleEdit.Forms
     {
         public bool AutoClose { get; internal set; }
 
-        public class DownloadModel
-        {
-            public string Url { get; set; }
-            public string TwoLetterLanguageCode { get; set; }
-            public string LanguageName { get; set; }
-            public override string ToString()
-            {
-                return LanguageName;
-            }
-        }
-
-        private readonly DownloadModel[] _voskModels = new[]
-        {
-            new DownloadModel
-            {
-                TwoLetterLanguageCode = "en",
-                LanguageName = "English",
-                Url = "https://alphacephei.com/vosk/models/vosk-model-en-us-0.22-lgraph.zip",
-            },
-            new DownloadModel
-            {
-                TwoLetterLanguageCode = "cn",
-                LanguageName = "Chinese",
-                Url = "https://alphacephei.com/vosk/models/vosk-model-cn-kaldi-multicn-2-lgraph.zip",
-            },
-            new DownloadModel
-            {
-                TwoLetterLanguageCode = "fr",
-                LanguageName = "French",
-                Url = "https://alphacephei.com/vosk/models/vosk-model-small-fr-pguyot-0.3.zip",
-            },
-            new DownloadModel
-            {
-                TwoLetterLanguageCode = "es",
-                LanguageName = "Spanish",
-                Url = "https://alphacephei.com/vosk/models/vosk-model-small-es-0.3.zip",
-            },
-            new DownloadModel
-            {
-                TwoLetterLanguageCode = "de",
-                LanguageName = "German",
-                Url = "https://alphacephei.com/vosk/models/vosk-model-small-de-0.15.zip",
-            },
-            new DownloadModel
-            {
-                TwoLetterLanguageCode = "pt",
-                LanguageName = "Portuguese",
-                Url = "https://alphacephei.com/vosk/models/vosk-model-small-pt-0.3.zip",
-            },
-            new DownloadModel
-            {
-                TwoLetterLanguageCode = "it",
-                LanguageName = "Italian",
-                Url = "https://alphacephei.com/vosk/models/vosk-model-small-it-0.4.zip",
-            },
-            new DownloadModel
-            {
-                TwoLetterLanguageCode = "nl",
-                LanguageName = "Dutch",
-                Url = "https://alphacephei.com/vosk/models/vosk-model-nl-spraakherkenning-0.6-lgraph.zip",
-            },
-            new DownloadModel
-            {
-                TwoLetterLanguageCode = "sv",
-                LanguageName = "Swedish",
-                Url = "https://alphacephei.com/vosk/models/vosk-model-small-sv-rhasspy-0.15.zip",
-            },
-            new DownloadModel
-            {
-                TwoLetterLanguageCode = "ru",
-                LanguageName = "Russian",
-                Url = "https://alphacephei.com/vosk/models/vosk-model-small-ru-0.22.zip",
-            },
-            new DownloadModel
-            {
-                TwoLetterLanguageCode = "fa",
-                LanguageName = "Farsi",
-                Url = "https://alphacephei.com/vosk/models/vosk-model-small-fa-0.5.zip",
-            },
-            new DownloadModel
-            {
-                TwoLetterLanguageCode = "tr",
-                LanguageName = "Turkish",
-                Url = "https://alphacephei.com/vosk/models/vosk-model-small-tr-0.3.zip",
-            },
-            new DownloadModel
-            {
-                TwoLetterLanguageCode = "el",
-                LanguageName = "Greek",
-                Url = "https://alphacephei.com/vosk/models/vosk-model-el-gr-0.7.zip",
-            },
-            new DownloadModel
-            {
-                TwoLetterLanguageCode = "ar",
-                LanguageName = "Arabic",
-                Url = "https://alphacephei.com/vosk/models/vosk-model-ar-mgb2-0.4.zip",
-            },
-            new DownloadModel
-            {
-                TwoLetterLanguageCode = "uk",
-                LanguageName = "Ukrainian",
-                Url = "https://alphacephei.com/vosk/models/vosk-model-small-uk-v3-small.zip",
-            },
-            new DownloadModel
-            {
-                TwoLetterLanguageCode = "ph",
-                LanguageName = "Filipino",
-                Url = "https://alphacephei.com/vosk/models/vosk-model-tl-ph-generic-0.6.zip",
-            },
-            new DownloadModel
-            {
-                TwoLetterLanguageCode = "kz",
-                LanguageName = "Kazakh",
-                Url = "https://alphacephei.com/vosk/models/vosk-model-kz-0.15.zip",
-            },
-            new DownloadModel
-            {
-                TwoLetterLanguageCode = "jp",
-                LanguageName = "Japanese",
-                Url = "https://alphacephei.com/vosk/models/vosk-model-small-ja-0.22.zip",
-            },
-            new DownloadModel
-            {
-                TwoLetterLanguageCode = "ca",
-                LanguageName = "Catalan",
-                Url = "https://alphacephei.com/vosk/models/vosk-model-small-ca-0.4.zip",
-            },
-        };
-
         public AudioToTextModelDownload()
         {
             UiUtil.PreInitialize(this);
@@ -150,7 +22,7 @@ namespace Nikse.SubtitleEdit.Forms
             buttonCancel.Text = LanguageSettings.Current.General.Cancel;
             UiUtil.FixLargeFonts(this, buttonDownload);
 
-            foreach (var downloadModel in _voskModels)
+            foreach (var downloadModel in DownloadModel.VoskModels)
             {
                 comboBoxModels.Items.Add(downloadModel);
             }
@@ -181,7 +53,7 @@ namespace Nikse.SubtitleEdit.Forms
                 buttonDownload.Enabled = false;
                 Refresh();
                 Cursor = Cursors.WaitCursor;
-                string url = downloadModel.Url;
+                var url = downloadModel.Url;
                 var wc = new WebClient { Proxy = Utilities.GetProxy() };
 
                 wc.DownloadDataCompleted += wc_DownloadDataCompleted;
