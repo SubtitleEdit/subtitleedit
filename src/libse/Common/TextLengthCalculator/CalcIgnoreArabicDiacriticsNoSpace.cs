f412900ee802193e14518f995022c351298cddce
OmrSi <omrsi97@gmail.com>
2022-03-02 02:26:11 +0200

    Don't count Arabic Diacritics in Line length

--- src/libse/Common/TextLengthCalculator/CalcIgnoreArabicDiacriticsNoSpace.cs
+++ src/libse/Common/TextLengthCalculator/CalcIgnoreArabicDiacriticsNoSpace.cs
@@ -1,17 +1,12 @@
 ﻿namespace Nikse.SubtitleEdit.Core.Common.TextLengthCalculator
 {
-    public class CalcIgnoreArabicDiacriticsNoSpaceCpsOnly : ICalcLength
+    public class CalcIgnoreArabicDiacriticsNoSpace : ICalcLength
     {
         /// <summary>
-        /// Calculate all text including space (tags are not counted).
+        /// Calculate all text excluding Arabic Diacritics and space (tags are not counted).
         /// </summary>
         public decimal CountCharacters(string text, bool forCps)
         {
-            if (!forCps)
-            {
-                return new CalcAll().CountCharacters(text, false);
-            }
-
             const char zeroWidthSpace = '\u200B';
             const char zeroWidthNoBreakSpace = '\uFEFF';
             var length = 0;
