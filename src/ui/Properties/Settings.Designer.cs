4fc8190a18eb98783b4e862f720545ac2b0759a3
niksedk <nikse.dk@gmail.com>
2022-03-20 17:09:11 +0100

    Update dotnet framework to 4.8

--- src/ui/Properties/Settings.Designer.cs
+++ src/ui/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Nikse.SubtitleEdit.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.8.1.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.1.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
