c987520433c0523ef270e3cca4eb5e8f379b1c02
Martijn van Berkel (Flitskikker) <martijneinstein@hotmail.com>
2022-06-22 21:10:01 +0200

    Add continuation pause and shortcut to custom style editor to settings dialog for Fix continuation style

--- src/ui/Logic/LanguageStructure.cs
+++ src/ui/Logic/LanguageStructure.cs
@@ -2737,6 +2737,9 @@
             public string UncheckInsertsLowercase { get; set; }
             public string HideContinuationCandidatesWithoutName { get; set; }
             public string IgnoreLyrics { get; set; }
+            public string ContinuationPause { get; set; }
+            public string Milliseconds { get; set; }
+            public string EditCustomContinuationStyle { get; set; }
             public string MinFrameGap { get; set; }
             public string XFramesAtYFrameRateGivesZMs { get; set; }
             public string UseXAsNewGap { get; set; }
