8ae94db8d4de2315d87c80690b4261f6b735af80
niksedk <nikse.dk@gmail.com>
2022-07-16 19:53:45 +0200

    Update German nouns

--- src/libse/GermanNouns.cs
+++ src/libse/GermanNouns.cs
@@ -1,13 +1,16 @@
 ﻿using Nikse.SubtitleEdit.Core.Common;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 
 namespace Nikse.SubtitleEdit.Core
 {
     public class GermanNouns
     {
         private readonly List<string> _germanNouns;
+        private readonly Dictionary<Regex, string> _regularExpressionList;
 
         public GermanNouns()
         {
@@ -15,8 +18,14 @@ namespace Nikse.SubtitleEdit.Core
             var inputFile = Path.Combine(Configuration.DictionariesDirectory, "deu_Nouns.txt");
             if (File.Exists(inputFile))
             {
-                _germanNouns = FileUtil.ReadAllLinesShared(inputFile, Encoding.UTF8);
+                _germanNouns = FileUtil.ReadAllLinesShared(inputFile, Encoding.UTF8).Select(p => p.Trim()).Where(p => p.Length > 1).ToList();
             }
+
+            _regularExpressionList = new Dictionary<Regex, string>
+            {
+                { new Regex(@"\bDas essen\b", RegexOptions.Compiled), "Das Essen" },
+                { new Regex(@"\bdas essen\b", RegexOptions.Compiled), "das Essen" }
+            };
         }
 
         public string UppercaseNouns(string text)
@@ -25,7 +34,14 @@ namespace Nikse.SubtitleEdit.Core
             if (textNoTags != textNoTags.ToUpperInvariant() && !string.IsNullOrEmpty(text))
             {
                 var st = new StrippableText(text);
+
                 st.FixCasing(_germanNouns, true, false, false, string.Empty);
+                
+                foreach (var regex in _regularExpressionList.Keys)
+                {
+                    st.StrippedText = regex.Replace(st.StrippedText, _regularExpressionList[regex]);
+                }
+
                 return st.MergedString;
             }
 
