290a706e356a2f27ccbbbd72f8745fc8e3d5ca49
niksedk <nikse.dk@gmail.com>
2022-07-19 06:03:55 +0200

    Use internal font style for scc

--- src/libse/Common/SccPositionAndStyle.cs
+++ src/libse/Common/SccPositionAndStyle.cs
@@ -5,12 +5,12 @@ namespace Nikse.SubtitleEdit.Core.Common
     public class SccPositionAndStyle
     {
         public Color ForeColor { get; }
-        public FontStyle Style { get; }
+        public SccFontStyle Style { get; }
         public int X { get; }
         public int Y { get; }
         public string Code { get; }
 
-        public SccPositionAndStyle(Color color, FontStyle style, int y, int x, string code)
+        public SccPositionAndStyle(Color color, SccFontStyle style, int y, int x, string code)
         {
             ForeColor = color;
             Style = style;
