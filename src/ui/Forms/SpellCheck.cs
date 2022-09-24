598f94bce2aaa30cec4d240223a74d9f1950238d
niksedk <nikse.dk@gmail.com>
2022-09-24 13:36:25 +0200

    Remember spell check font size

--- src/ui/Forms/SpellCheck.cs
+++ src/ui/Forms/SpellCheck.cs
@@ -1738,6 +1738,7 @@ namespace Nikse.SubtitleEdit.Forms
         {
             Font = useLargerFontForThisWindowToolStripMenuItem.Checked ? new Font(Font.FontFamily, Font.Size - 2, FontStyle.Regular) : new Font(Font.FontFamily, Font.Size + 2, FontStyle.Regular);
             useLargerFontForThisWindowToolStripMenuItem.Checked = !useLargerFontForThisWindowToolStripMenuItem.Checked;
+            Configuration.Settings.Tools.SpellCheckUseLargerFont = useLargerFontForThisWindowToolStripMenuItem.Checked;
         }
 
         private void SpellCheck_Shown(object sender, EventArgs e)
