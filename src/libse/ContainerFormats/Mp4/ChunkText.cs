2ebc3fa1203bf88c7dfc5da2e022d3f40ab04898
niksedk <nikse.dk@gmail.com>
2022-10-22 11:45:41 +0200

    Initial new GetParagraphs

---
+++ src/libse/ContainerFormats/Mp4/ChunkText.cs
@@ -0,0 +1,8 @@
+﻿namespace Nikse.SubtitleEdit.Core.ContainerFormats.Mp4
+{
+    public class ChunkText
+    {
+        public uint Size { get; set; }
+        public string Text { get; set; }
+    }
+}
