930358708d72dd1765414af40af0594a1059912a
niksedk <nikse.dk@gmail.com>
2022-10-24 18:16:53 +0200

    Update winget

--- build.bat
+++ build.bat
@@ -137,11 +137,10 @@ IF NOT EXIST "%WinGetFix%" (
 )
 "%WinGetFix%" "winget" 
 ECHO.
-
+ECHO Validate with: winget validate --manifest C:\git\subtitleedit\winget
+ECHO Test with    : winget install --manifest C:\git\subtitleedit\winget
+ECHO Submit with  : wingetcreate submit C:\git\subtitleedit\winget
 pause
-
-
-
 
 
 :EndSuccessful
