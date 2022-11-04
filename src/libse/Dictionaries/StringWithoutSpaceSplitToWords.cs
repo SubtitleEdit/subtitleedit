df4d9d5effc9ca87ac8947966712fd569afed200
niksedk <nikse.dk@gmail.com>
2022-11-04 05:48:48 +0100

    Work on dictionaries

--- src/libse/Dictionaries/StringWithoutSpaceSplitToWords.cs
+++ src/libse/Dictionaries/StringWithoutSpaceSplitToWords.cs
@@ -100,8 +100,8 @@ namespace Nikse.SubtitleEdit.Core.Dictionaries
                 wordList.AddRange(new List<string>
                 {
                     // Ignore list
-                    "Andor", "honour", "putain", "whoah", "eastside", "Starpath", "comlink", "Taamet",
-                    "Atwater", "Lakeview"
+                    "Andor", "honour", "honours", "putain", "whoah", "eastside", "Starpath", "comlink", "Taamet",
+                    "Atwater", "Lakeview", "Glassman", "Starfleet"
                 });
             }
 
