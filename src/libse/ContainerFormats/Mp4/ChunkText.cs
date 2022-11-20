785aa6336414d3135c7cab086abe02268a3ce9c6
niksedk <nikse.dk@gmail.com>
2022-11-20 09:15:12 +0100

    Minor mp4 refact

--- src/libse/ContainerFormats/Mp4/ChunkText.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Nikse.SubtitleEdit.Core.ContainerFormats.Mp4
-{
-    public class ChunkText
-    {
-        public string Text { get; set; }
-        public ulong Offset { get; set; }
-    }
-}
