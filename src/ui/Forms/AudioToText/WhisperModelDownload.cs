47c4010b62dda58fe468ab9f112bc9b34c022934
niksedk <nikse.dk@gmail.com>
2022-11-23 19:27:11 +0100

    Try to fix whisper download of large model - thx darnn :) Fix #6451

--- src/ui/Forms/AudioToText/WhisperModelDownload.cs
+++ src/ui/Forms/AudioToText/WhisperModelDownload.cs
@@ -66,7 +66,15 @@ namespace Nikse.SubtitleEdit.Forms.AudioToText
                 Refresh();
                 Cursor = Cursors.WaitCursor;
                 var httpClient = HttpClientHelper.MakeHttpClient();
-                using (var downloadStream = new MemoryStream())
+
+                var folder = WhisperHelper.GetWhisperModel().ModelFolder;
+                if (!Directory.Exists(folder))
+                {
+                    WhisperHelper.GetWhisperModel().CreateModelFolder();
+                }
+
+                var downloadFileName = Path.Combine(folder, LastDownloadedModel.Name);
+                using (var downloadStream = new FileStream(downloadFileName, FileMode.Create, FileAccess.Write))
                 {
                     var downloadTask = httpClient.DownloadAsync(url, downloadStream, new Progress<float>((progress) =>
                     {
@@ -106,25 +114,15 @@ namespace Nikse.SubtitleEdit.Forms.AudioToText
             DialogResult = DialogResult.Cancel;
         }
 
-        private void CompleteDownload(MemoryStream downloadStream)
+        private void CompleteDownload(FileStream downloadStream)
         {
             if (downloadStream.Length == 0)
             {
                 throw new Exception("No content downloaded - missing file or no internet connection!");
             }
 
-            var folder = WhisperHelper.GetWhisperModel().ModelFolder;
-            if (!Directory.Exists(folder))
-            {
-                WhisperHelper.GetWhisperModel().CreateModelFolder();
-            }
-
-            var fileName = Path.Combine(folder, LastDownloadedModel.Name + WhisperHelper.ModelExtension());
-            using (var fileStream = File.Create(fileName))
-            {
-                downloadStream.Seek(0, SeekOrigin.Begin);
-                downloadStream.CopyTo(fileStream);
-            }
+            downloadStream.Flush();
+            downloadStream.Close();
 
             Cursor = Cursors.Default;
             labelPleaseWait.Text = string.Empty;
