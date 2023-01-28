c8c9af3d2ece20c51d1a47acb9666b377f5d46c0
niksedk <nikse.dk@gmail.com>
2023-01-28 16:14:53 +0100

    Update change log

--- src/libse/SubtitleFormats/FormatLanguage.cs
+++ src/libse/SubtitleFormats/FormatLanguage.cs
@@ -5,6 +5,6 @@
         public static string LineNumberXErrorReadingFromSourceLineY { get; set; } = "Line {0} - error reading: {1}";
         public static string LineNumberXErrorReadingTimeCodeFromSourceLineY { get; set; } = "Line {0} - error reading time code: {1}";
         public static string LineNumberXExpectedNumberFromSourceLineY { get; set; } = "Line {0} - expected subtitle number: {1}";
-        public static string LineNumberXExpectedEmptyLine { get; set; } = "Line {0} - expected empty line but found number + time code (number skipped): {1}";
+        public static string LineNumberXExpectedEmptyLine { get; set; } = "Line {0}: Empty line expected, but found number ({1}) followed by time code.";
     }
 }
