23cfbf222ef012c9466c42b0ae66021badd1516f
niksedk <nikse.dk@gmail.com>
2023-02-22 07:07:51 +0100

    Work on stl ebu font...

--- src/Test/Logic/SubtitleFormats/EbuStlTest.cs
+++ src/Test/Logic/SubtitleFormats/EbuStlTest.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Nikse.SubtitleEdit.Core.Common;
 using Nikse.SubtitleEdit.Core.SubtitleFormats;
 using Nikse.SubtitleEdit.Logic;
@@ -42,7 +43,25 @@ namespace Test.Logic.SubtitleFormats
             });
             var reload = new Subtitle();
             target.LoadSubtitle(reload, ms.ToArray());
-            Assert.IsTrue(reload.Paragraphs[0].Text == "Gimme <font color=\"Red\">the</font> gun, <font color=\"Red\">Henry</font>.");
+            Assert.IsTrue(reload.Paragraphs[0].Text == subText);
+        }
+
+        [TestMethod]
+        public void EbuFont3()
+        {
+            var target = new Ebu();
+            var subtitle = new Subtitle();
+            var subText = "<font color=\"Magenta\">This is a magenta line.</font>" + Environment.NewLine + "This is white.";
+            subtitle.Paragraphs.Add(new Paragraph(subText, 0, 999));
+            var ms = new MemoryStream();
+            Ebu.EbuUiHelper = new UiEbuSaveHelper();
+            target.Save("test.stl", ms, subtitle, true, new Ebu.EbuGeneralSubtitleInformation()
+            {
+                DisplayStandardCode = "1",
+            });
+            var reload = new Subtitle();
+            target.LoadSubtitle(reload, ms.ToArray());
+            Assert.IsTrue(reload.Paragraphs[0].Text == subText);
         }
     }
 }
