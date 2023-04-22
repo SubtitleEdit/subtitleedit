72be1e8530e026a54281f814f9e862c4b5fe4829
niksedk <nikse.dk@gmail.com>
2023-04-22 15:15:06 +0200

    Improve audio-to-text post processing a little - thx  oep42 :) Working on #6856

---
+++ src/Test/Core/AudioToTextTest.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Nikse.SubtitleEdit.Core.AudioToText;
+using Nikse.SubtitleEdit.Core.Common;
+using Nikse.SubtitleEdit.Core.SubtitleFormats;
+
+namespace Test.Core
+{
+    [TestClass]
+    public class AudioToTextTest
+    {
+        [TestMethod]
+        public void AudioToTextPostProcessorAutoBalance()
+        {
+            var subtitle = new Subtitle();
+            new SubRip().LoadSubtitle(subtitle, @"1
+00:01:04,000 --> 00:01:07,693
+It's very important to understand that the
+world is not just a place witch accepts our
+
+2
+00:01:07,717 --> 00:01:08,940
+norms.".SplitToLines(), null);
+
+            var postProcessor = new AudioToTextPostProcessor("en");
+            var fixedSubtitle = postProcessor.Fix(AudioToTextPostProcessor.Engine.Whisper, subtitle, true, false, true, false, false, false);
+            Assert.AreEqual(2, fixedSubtitle.Paragraphs.Count);
+            Assert.IsTrue(fixedSubtitle.Paragraphs[1].Text.Length > 30);
+        }
+
+        [TestMethod]
+        public void AudioToTextPostProcessorAutoBalance2()
+        {
+            var subtitle = new Subtitle();
+            new SubRip().LoadSubtitle(subtitle, @"1
+00:01:04,000 --> 00:01:07,693
+It's very important to understand that the
+world just accepts our
+
+2
+00:01:07,717 --> 00:01:08,940
+norms.".SplitToLines(), null);
+
+            var postProcessor = new AudioToTextPostProcessor("en");
+            var fixedSubtitle = postProcessor.Fix(AudioToTextPostProcessor.Engine.Whisper, subtitle, true, false, true, false, false, false);
+            Assert.AreEqual(1, fixedSubtitle.Paragraphs.Count);
+            Assert.IsTrue(fixedSubtitle.Paragraphs[0].Text.CountCharacters(false) == 71);
+        }
+
+        [TestMethod]
+        public void AudioToTextPostProcessorAutoBalance3()
+        {
+            var subtitle = new Subtitle();
+            new SubRip().LoadSubtitle(subtitle, @"1
+00:01:04,000 --> 00:01:07,140
+Playing at a different venue almost every night, waking up in a different hotel
+
+2
+00:01:07,140 --> 00:01:08,000
+room every morning.".SplitToLines(), null);
+
+            var postProcessor = new AudioToTextPostProcessor("en");
+            var fixedSubtitle = postProcessor.Fix(AudioToTextPostProcessor.Engine.Whisper, subtitle, true, false, true, false, false, false);
+            Assert.AreEqual(2, fixedSubtitle.Paragraphs.Count);
+            Assert.IsTrue(fixedSubtitle.Paragraphs[0].Text.EndsWith(","));
+        }
+    }
+}
--- src/libse/Forms/SplitLongLinesHelper.cs
+++ src/libse/Forms/SplitLongLinesHelper.cs
@@ -1,5 +1,6 @@
 ﻿using Nikse.SubtitleEdit.Core.Common;
 using System;
+using System.Xml;
 
 namespace Nikse.SubtitleEdit.Core.Forms
 {
@@ -8,7 +9,7 @@ namespace Nikse.SubtitleEdit.Core.Forms
         public static bool QualifiesForSplit(string text, int singleLineMaxCharacters, int totalLineMaxCharacters)
         {
             var noTagText = HtmlUtil.RemoveHtmlTags(text.Trim(), true);
-            if (noTagText.Length > totalLineMaxCharacters)
+            if (noTagText.CountCharacters(false) > totalLineMaxCharacters)
             {
                 return true;
             }
@@ -27,42 +28,39 @@ namespace Nikse.SubtitleEdit.Core.Forms
             }
 
             var noTagSingleLine = Utilities.UnbreakLine(noTagText);
-            int lineSplitIdx = noTagSingleLine.IndexOfAny(new[] { ". -", "! -", "? -", "] -", ") -" }, StringComparison.Ordinal);
+            var lineSplitIdx = noTagSingleLine.IndexOfAny(new[] { ". -", "! -", "? -", "] -", ") -" }, StringComparison.Ordinal);
             if (lineSplitIdx < 0)
             {
                 return false;
             }
 
-            int maxLineLen = Math.Max(lineSplitIdx + 1, noTagSingleLine.Length - (lineSplitIdx + 2));
+            var maxLineLen = Math.Max(lineSplitIdx + 1, noTagSingleLine.Length - (lineSplitIdx + 2));
             return maxLineLen > singleLineMaxCharacters;
         }
 
         public static Subtitle SplitLongLinesInSubtitle(Subtitle subtitle, int totalLineMaxCharacters, int singleLineMaxCharacters)
         {
-            var splittedSubtitle = new Subtitle(subtitle);
+            var splitSubtitle = new Subtitle(subtitle);
             var language = LanguageAutoDetect.AutoDetectGoogleLanguage(subtitle);
 
             // calculate gaps
             var halfMinGaps = Configuration.Settings.General.MinimumMillisecondsBetweenLines / 2.0;
             var halfMinGapsMood = halfMinGaps + Configuration.Settings.General.MinimumMillisecondsBetweenLines % 2;
 
-            const int FirstLine = 0;
-            const int SecondLine = 1;
+            const int firstLine = 0;
+            const int secondLine = 1;
 
-            for (int i = splittedSubtitle.Paragraphs.Count - 1; i >= 0; i--)
+            for (var i = splitSubtitle.Paragraphs.Count - 1; i >= 0; i--)
             {
-                var oldParagraph = splittedSubtitle.Paragraphs[i];
+                var oldParagraph = splitSubtitle.Paragraphs[i];
 
                 // don't split into two paragraph if it can be balanced
                 var text = oldParagraph.Text;
                 var noHtmlLines = HtmlUtil.RemoveHtmlTags(text, true).SplitToLines();
 
-                bool autoBreak = true;
-                if (noHtmlLines.Count == 2 && noHtmlLines[0].Length <= totalLineMaxCharacters && noHtmlLines[1].Length < totalLineMaxCharacters &&
-                    (noHtmlLines[0].EndsWith('.') || noHtmlLines[0].EndsWith('!') || noHtmlLines[0].EndsWith('?') || noHtmlLines[0].EndsWith('؟')))
-                {
-                    autoBreak = false;
-                }
+                var autoBreak = !(noHtmlLines.Count == 2 && noHtmlLines[0].Length <= totalLineMaxCharacters && noHtmlLines[1].Length < totalLineMaxCharacters &&
+                                  (noHtmlLines[0].EndsWith('.') || noHtmlLines[0].EndsWith('!') || noHtmlLines[0].EndsWith('?') || noHtmlLines[0].EndsWith('؟')));
+
                 if (autoBreak)
                 {
                     text = Utilities.AutoBreakLine(oldParagraph.Text, language, true);
@@ -89,13 +87,13 @@ namespace Nikse.SubtitleEdit.Core.Forms
                 // calculate milliseconds per char
                 var millisecondsPerChar = oldParagraph.Duration.TotalMilliseconds / (HtmlUtil.RemoveHtmlTags(text, true).Length - Environment.NewLine.Length);
 
-                oldParagraph.Text = lines[FirstLine];
+                oldParagraph.Text = lines[firstLine];
 
                 // use optimal time to adjust duration
                 oldParagraph.EndTime.TotalMilliseconds = oldParagraph.StartTime.TotalMilliseconds + millisecondsPerChar * HtmlUtil.RemoveHtmlTags(oldParagraph.Text, true).Length - halfMinGaps;
 
                 // build second paragraph
-                var newParagraph = new Paragraph(oldParagraph) { Text = lines[SecondLine] };
+                var newParagraph = new Paragraph(oldParagraph) { Text = lines[secondLine] };
                 newParagraph.StartTime.TotalMilliseconds = oldParagraph.EndTime.TotalMilliseconds + halfMinGapsMood;
                 newParagraph.EndTime.TotalMilliseconds = newParagraph.StartTime.TotalMilliseconds + millisecondsPerChar * HtmlUtil.RemoveHtmlTags(newParagraph.Text, true).Length;
 
@@ -119,11 +117,11 @@ namespace Nikse.SubtitleEdit.Core.Forms
                 newParagraph.Text = Utilities.AutoBreakLine(newParagraph.Text, language);
 
                 // insert new paragraph after the current/old one
-                splittedSubtitle.Paragraphs.Insert(i + 1, newParagraph);
+                splitSubtitle.Paragraphs.Insert(i + 1, newParagraph);
             }
 
-            splittedSubtitle.Renumber();
-            return splittedSubtitle;
+            splitSubtitle.Renumber();
+            return splitSubtitle;
         }
 
         private const char Dash = '-'; // U+002D - HYPHEN-MINUS
@@ -135,9 +133,11 @@ namespace Nikse.SubtitleEdit.Core.Forms
             {
                 return;
             }
+
             // update first text
-            int dashIdx = p1.Text.IndexOf(Dash);
+            var dashIdx = p1.Text.IndexOf(Dash);
             p1.Text = p1.Text.Substring(0, dashIdx) + p1.Text.Substring(dashIdx + 1).TrimStart();
+
             // update second text
             dashIdx = p2.Text.IndexOf(Dash);
             p2.Text = p2.Text.Substring(0, dashIdx) + p2.Text.Substring(dashIdx + 1).TrimStart();
@@ -149,11 +149,13 @@ namespace Nikse.SubtitleEdit.Core.Forms
             {
                 return text.StartsWith(Dash);
             }
-            int closeIdx = text.IndexOf('>');
+
+            var closeIdx = text.IndexOf('>');
             if (closeIdx + 1 == text.Length) // found in last position
             {
                 return false;
             }
+
             return text[closeIdx + 1] == Dash;
         }
 
@@ -164,9 +166,9 @@ namespace Nikse.SubtitleEdit.Core.Forms
             {
                 return false;
             }
+
             var lastChar = noTagText[noTagText.Length - 1];
             return ".!?؟:)]♪".Contains(lastChar);
         }
-
     }
 }
