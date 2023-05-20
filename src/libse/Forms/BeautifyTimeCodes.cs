f47b51e0f8927423346df85d4081eb422614fb36
Martijn van Berkel (Flitskikker) <martijneinstein@hotmail.com>
2023-05-20 23:49:29 +0200

    Implement gaps in waveform snapping

--- src/libse/Forms/BeautifyTimeCodes.cs
+++ src/libse/Forms/BeautifyTimeCodes.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Nikse.SubtitleEdit.Core.Common;
 
 namespace Nikse.SubtitleEdit.Core.Forms
 {
-    internal class BeautifyTimeCodes
+    public static class BeautifyTimeCodes
     {
+        // Utils
+
+        public static double GetInCuesGapMs(double? frameRate = null)
+        {
+            return (TimeCode.BaseUnit / (frameRate ?? Configuration.Settings.General.CurrentFrameRate)) * Configuration.Settings.BeautifyTimeCodes.Profile.InCuesGap;
+        }
+
+        public static double GetOutCuesGapMs(double? frameRate = null)
+        {
+            return (TimeCode.BaseUnit / (frameRate ?? Configuration.Settings.General.CurrentFrameRate)) * Configuration.Settings.BeautifyTimeCodes.Profile.OutCuesGap;
+        }
     }
 }
