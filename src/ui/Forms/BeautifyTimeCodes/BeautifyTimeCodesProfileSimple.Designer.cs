441269642bf44cc0cb10626cbb3c10d48324a448
Martijn van Berkel (Flitskikker) <martijneinstein@hotmail.com>
2023-06-06 21:42:56 +0200

    Fix profile simple dialog closing on error

--- src/ui/Forms/BeautifyTimeCodes/BeautifyTimeCodesProfileSimple.Designer.cs
+++ src/ui/Forms/BeautifyTimeCodes/BeautifyTimeCodesProfileSimple.Designer.cs
@@ -482,7 +482,6 @@
             // 
             // BeautifyTimeCodesProfileSimple
             // 
-            this.AcceptButton = this.buttonOK;
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.buttonCancel;
