35f3396914ac6a1a5ab4e1a5012b7967020dc886
Ivandro Jao <ivandrofly@gmail.com>
2023-07-18 19:14:43 +0100

    Remove unnecessary usings across various files
    Multiple redundant using directives were removed from various files, contributing to the cleaner and more efficient codebase. These were no longer needed due to previous refactoring operations, where dependencies on these namespaces were eliminated.

--- src/ui/Forms/ConvertColorsToDialog.cs
+++ src/ui/Forms/ConvertColorsToDialog.cs
@@ -1,13 +1,6 @@
 ﻿using Nikse.SubtitleEdit.Core.Common;
 using Nikse.SubtitleEdit.Logic;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Nikse.SubtitleEdit.Forms
