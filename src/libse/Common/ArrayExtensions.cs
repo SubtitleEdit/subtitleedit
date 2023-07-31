f83a7d07a402ffa0840ba3aa8a4fe36b08ad49dd
Martijn van Berkel (Flitskikker) <martijneinstein@hotmail.com>
2023-07-31 14:31:58 +0200

    Clearer naming

--- src/libse/Common/ArrayExtensions.cs
+++ src/libse/Common/ArrayExtensions.cs
@@ -5,25 +5,25 @@ namespace Nikse.SubtitleEdit.Core.Common
 {
     public static class ArrayExtensions
     {
-        public static T FirstOnOrAfter<T>(this List<T> list, T target, T defaultValue)
+        public static T FirstOnOrAfter<T>(this List<T> orderedList, T target, T defaultValue)
         {
-            int index = list.BinarySearch(target);
+            int index = orderedList.BinarySearch(target);
             if (index < 0)
             {
                 index = ~index; // Get the bitwise complement to find the index of the first element greater than the target
             }
 
-            if (index < list.Count)
+            if (index < orderedList.Count)
             {
-                return list[index];
+                return orderedList[index];
             }
 
             return defaultValue;
         }
 
-        public static T FirstOnOrBefore<T>(this List<T> list, T target, T defaultValue)
+        public static T FirstOnOrBefore<T>(this List<T> orderedList, T target, T defaultValue)
         {
-            int index = list.BinarySearch(target);
+            int index = orderedList.BinarySearch(target);
             if (index < 0)
             {
                 index = ~index - 1; // Get the bitwise complement to find the index of the last element smaller than the target
@@ -31,15 +31,15 @@ namespace Nikse.SubtitleEdit.Core.Common
 
             if (index >= 0)
             {
-                return list[index];
+                return orderedList[index];
             }
 
             return defaultValue;
         }
 
-        public static int ClosestIndexTo(this List<int> list, int target)
+        public static int ClosestIndexTo(this List<int> orderedList, int target)
         {
-            int closestIndex = list.BinarySearch(target);
+            int closestIndex = orderedList.BinarySearch(target);
 
             // If the value is found, BinarySearch returns the index of the exact match.
             // Otherwise, it returns a negative value that indicates the bitwise complement of the first index
@@ -48,29 +48,29 @@ namespace Nikse.SubtitleEdit.Core.Common
             {
                 closestIndex = ~closestIndex;
 
-                if (closestIndex >= list.Count)
+                if (closestIndex >= orderedList.Count)
                 {
-                    return list.Count - 1;
+                    return orderedList.Count - 1;
                 }
                 else if ((closestIndex - 1) >= 0)
                 {
                     // If both indices are valid, return which one's value is closer.
-                    return Math.Abs(list[closestIndex - 1] - target) < Math.Abs(list[closestIndex] - target) ? closestIndex - 1 : closestIndex;
+                    return Math.Abs(orderedList[closestIndex - 1] - target) < Math.Abs(orderedList[closestIndex] - target) ? closestIndex - 1 : closestIndex;
                 }
             }
 
             return closestIndex;
         }
 
-        public static int ClosestTo(this List<int> list, int target)
+        public static int ClosestTo(this List<int> orderedList, int target)
         {
-            int closestIndex = list.ClosestIndexTo(target);
-            return list[closestIndex];
+            int closestIndex = orderedList.ClosestIndexTo(target);
+            return orderedList[closestIndex];
         }
 
-        public static int ClosestIndexTo(this List<double> list, double target)
+        public static int ClosestIndexTo(this List<double> orderedList, double target)
         {
-            int closestIndex = list.BinarySearch(target);
+            int closestIndex = orderedList.BinarySearch(target);
 
             // If the value is found, BinarySearch returns the index of the exact match.
             // Otherwise, it returns a negative value that indicates the bitwise complement of the first index
@@ -79,30 +79,30 @@ namespace Nikse.SubtitleEdit.Core.Common
             {
                 closestIndex = ~closestIndex;
 
-                if (closestIndex >= list.Count)
+                if (closestIndex >= orderedList.Count)
                 {
-                    return list.Count - 1;
+                    return orderedList.Count - 1;
                 }
                 else if ((closestIndex - 1) >= 0)
                 {
                     // If both indices are valid, return which one's value is closer.
-                    return Math.Abs(list[closestIndex - 1] - target) < Math.Abs(list[closestIndex] - target) ? closestIndex - 1 : closestIndex;
+                    return Math.Abs(orderedList[closestIndex - 1] - target) < Math.Abs(orderedList[closestIndex] - target) ? closestIndex - 1 : closestIndex;
                 }
             }
 
             return closestIndex;
         }
 
-        public static double ClosestTo(this List<double> list, double target)
+        public static double ClosestTo(this List<double> orderedList, double target)
         {
-            int closestIndex = list.ClosestIndexTo(target);
-            return list[closestIndex];
+            int closestIndex = orderedList.ClosestIndexTo(target);
+            return orderedList[closestIndex];
         }
 
-        public static int? FirstWithin(this List<int> list, int start, int end)
+        public static int? FirstWithin(this List<int> orderedList, int start, int end)
         {
-            int startIndex = list.BinarySearch(start);
-            int endIndex = list.BinarySearch(end);
+            int startIndex = orderedList.BinarySearch(start);
+            int endIndex = orderedList.BinarySearch(end);
 
             if (startIndex < 0)
             {
@@ -114,12 +114,12 @@ namespace Nikse.SubtitleEdit.Core.Common
                 endIndex = ~endIndex - 1;
             }
 
-            if (startIndex > endIndex || startIndex >= list.Count || list[startIndex] > end)
+            if (startIndex > endIndex || startIndex >= orderedList.Count || orderedList[startIndex] > end)
             {
                 return null;
             }
 
-            return list[startIndex];
+            return orderedList[startIndex];
         }
     }
 }
