ed75e62a9310e84efd3ae5092590ed15851b1cbe
niksedk <nikse.dk@gmail.com>
2023-08-13 20:37:41 +0200

    Add word + fix typo

--- src/ui/Forms/Main.cs
+++ src/ui/Forms/Main.cs
@@ -35279,7 +35279,7 @@ namespace Nikse.SubtitleEdit.Forms
                             extraMessage = Environment.NewLine + "Note you have a custom argument: " + customArgument;
                         }
                         extraMessage = extraMessage + Environment.NewLine + Environment.NewLine + "View the log file `whisper_log.txt`?";
-                        var r = MessageBox.Show($"No text found'" + extraMessage, MessageBoxButtons.YesNoCancel, MessageBoxIcon.Error);
+                        var r = MessageBox.Show($"No text found!" + extraMessage, MessageBoxButtons.YesNoCancel, MessageBoxIcon.Error);
                         if (r == DialogResult.Yes)
                         {
                             UiUtil.OpenFile(SeLogger.GetWhisperLogFilePath());
