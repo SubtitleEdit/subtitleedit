3872cc8bcf8586ce0d805228fee57db270cb4335
Ivandro Jao <ivandrofly@gmail.com>
2023-08-17 20:46:54 +0100

    Refactor plugin loading logic and improve validation checks.
    The refactoring was done for LocalPluginMetadataProvider and LocalPlugin classes. A new SymbolReader class is introduced to read property values from the plugin assemblies' types using reflection, removing repeated code for this task. String constants are introduced for the "*.dll" extension and for the properties names to avoid hardcoded strings.
    In LocalPlugin, a validation check method 'IsValid' is introduced to ensure the plugin's name is not null or empty and its version is greater than 0. This is to ensure only valid plugins get added to the installed plugins. Improved the equality check for plugin names to ignore casing. All these changes made the code more maintainable and robust, reducing possibility for errors.

--- src/ui/Plugins/LocalPluginMetadataProvider.cs
+++ src/ui/Plugins/LocalPluginMetadataProvider.cs
@@ -8,6 +8,8 @@ namespace Nikse.SubtitleEdit.Plugins
 {
     public class LocalPluginMetadataProvider
     {
+        private const string KnownPluginExtension = "*.dll";
+
         private readonly string _pluginFolder;
 
         public LocalPluginMetadataProvider(string pluginFolder)
@@ -17,7 +19,7 @@ namespace Nikse.SubtitleEdit.Plugins
 
         public IReadOnlyCollection<LocalPlugin> GetPlugins()
         {
-            var pluginFiles = Directory.GetFiles(_pluginFolder, "*.dll");
+            var pluginFiles = Directory.GetFiles(_pluginFolder, KnownPluginExtension);
 
             // no plugin installed
             if (pluginFiles.Length == 0)
@@ -27,25 +29,19 @@ namespace Nikse.SubtitleEdit.Plugins
 
             var installedPlugins = new List<LocalPlugin>();
             const string plugin = "IPlugin";
-            
+
             foreach (var pluginFile in pluginFiles)
             {
                 try
                 {
                     var assembly = Assembly.Load(File.ReadAllBytes(pluginFile));
-
                     // all plugins must implement the "IPlugin" interface
                     var pluginType = assembly.GetTypes().FirstOrDefault(type => type.GetInterface(plugin) != null);
-
-                    var pluginInfo = ParseFromType(pluginType);
-
-                    // invalid plugin info
-                    if (pluginInfo is null)
+                    var localPlugin = ParseFromType(pluginType);
+                    if (localPlugin?.IsValid() == true)
                     {
-                        continue;
+                        installedPlugins.Add(localPlugin);
                     }
-
-                    installedPlugins.Add(pluginInfo);
                 }
                 catch (Exception)
                 {
@@ -64,24 +60,52 @@ namespace Nikse.SubtitleEdit.Plugins
             }
 
             var instance = Activator.CreateInstance(pluginType);
+            var symbolReader = new SymbolReader(pluginType, instance);
 
-            // read name/text
-            var name = (string)pluginType
-                .GetProperty("Text", BindingFlags.Instance | BindingFlags.Public)
-                ?.GetValue(instance, null);
+            // read the following properties from plugin assembly
+            var text = symbolReader.ReadFromProperty<string>(SymbolReader.Text);
+            var description = symbolReader.ReadFromProperty<string>(SymbolReader.Description);
+            var version = symbolReader.ReadFromProperty<decimal>(SymbolReader.Version);
 
-            // read description
-            var description = (string)pluginType
-                .GetProperty(nameof(PluginInfo.Description), BindingFlags.Instance | BindingFlags.Public)
-                ?.GetValue(instance, null);
+            return new LocalPlugin(text, description, version);
+        }
 
-            // read version
-            // ReSharper disable once PossibleNullReferenceException
-            var version = (decimal)pluginType
-                .GetProperty(nameof(PluginInfo.Version), BindingFlags.Instance | BindingFlags.Public)
-                .GetValue(instance, null);
+        private static bool IsValid(string value) => !string.IsNullOrWhiteSpace(value);
+        private static bool IsValid(decimal value) => value > 0;
 
-            return new LocalPlugin(name, description, version);
+        private class SymbolReader
+        {
+            public const string Text = "Text";
+            public const string Description = "Description";
+            public const string Version = "Version";
+            
+            private readonly Type _pluginType;
+            private readonly object _instance;
+            private const BindingFlags BindingFlags = System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.Public;
+
+            public SymbolReader(Type pluginType, object instance)
+            {
+                _pluginType = pluginType;
+                _instance = instance;
+            }
+
+            public TValue ReadFromProperty<TValue>(string propertyName)
+            {
+                var propInfo = _pluginType.GetProperty(propertyName, BindingFlags);
+                if (propInfo is null)
+                {
+                    return default;
+                }
+
+                try
+                {
+                    return (TValue)propInfo.GetValue(_instance, null);
+                }
+                catch (Exception)
+                {
+                    return default;
+                }
+            }
         }
     }
 }
