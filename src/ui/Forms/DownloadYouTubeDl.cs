f2c6297cefd7a7676592f857f4e7aa852087e041
niksedk <nikse.dk@gmail.com>
2023-09-24 09:42:49 +0200

    Update yt-dlp link/hash

--- src/ui/Forms/DownloadYouTubeDl.cs
+++ src/ui/Forms/DownloadYouTubeDl.cs
@@ -11,8 +11,8 @@ namespace Nikse.SubtitleEdit.Forms
 {
     public sealed partial class DownloadYouTubeDl : Form
     {
-        public const string Url = "https://github.com/yt-dlp/yt-dlp/releases/download/2023.07.06/yt-dlp.exe";
-        public const string Sha512Hash = "fe7470971d0f02b9036aee509779e68a245746e90971da0a077553aa4259e25a6199e63b19d0b66c4f41f8a2eabfac584db7a9cdcc7f5b8ab5906d336e11ba80";
+        public const string Url = "https://github.com/yt-dlp/yt-dlp/releases/download/2023.09.24/yt-dlp.exe";
+        public const string Sha512Hash = "c49020fddc86b01c7c1479eab59b69072c9d64019e868a9eb49cc37db97fe0c6294fb4410cdd6400128c43c1dac0bec8208da70815f0450527f4261ea81dc665";
         public bool AutoClose { get; internal set; }
         private readonly CancellationTokenSource _cancellationTokenSource;
 
