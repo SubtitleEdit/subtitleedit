6ce098ef33af608c745f6c9eeb9b9206151fb5c1
niksedk <nikse.dk@gmail.com>
2023-10-07 15:00:49 +0200

    Minor UI fix

--- src/ui/Forms/LayoutPicker.Designer.cs
+++ src/ui/Forms/LayoutPicker.Designer.cs
@@ -224,7 +224,7 @@
             this.button9.Location = new System.Drawing.Point(427, 316);
             this.button9.Name = "button9";
             this.button9.Size = new System.Drawing.Size(195, 147);
-            this.button9.TabIndex = 92;
+            this.button9.TabIndex = 8;
             this.button9.Text = "9";
             this.button9.UseVisualStyleBackColor = true;
             this.button9.Click += new System.EventHandler(this.button9_Click);
