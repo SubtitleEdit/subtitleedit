91d104e18e1dd5e5c5c419af5346b06649c49cf2
niksedk <nikse.dk@gmail.com>
2023-11-11 20:13:18 +0100

    Update to Purview's Faster-Whisper r160.4 + some post-processing Related to #7593

--- src/Test/Core/AudioToTextTest.cs
+++ src/Test/Core/AudioToTextTest.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Nikse.SubtitleEdit.Core.AudioToText;
 using Nikse.SubtitleEdit.Core.Common;
 using Nikse.SubtitleEdit.Core.SubtitleFormats;
@@ -62,6 +63,57 @@ room every morning.".SplitToLines(), null);
             var fixedSubtitle = postProcessor.Fix(AudioToTextPostProcessor.Engine.Whisper, subtitle, true, false, true, false, false, false);
             Assert.AreEqual(2, fixedSubtitle.Paragraphs.Count);
             Assert.IsTrue(fixedSubtitle.Paragraphs[0].Text.EndsWith(","));
+        }
+
+        [TestMethod]
+        public void FixIt()
+        {
+            var raw = @"1
+00:00:00,100 --> 00:00:04,580
+Today on This Old House, I'll tour this
+modern home to show how beautiful features
+
+2
+00:00:04,581 --> 00:00:07,739
+and accessible design
+go hand in hand. We're
+
+3
+00:00:07,751 --> 00:00:10,920
+mixing mortar to patch
+the original brick on
+
+4
+00:00:10,921 --> 00:00:14,529
+this 1960 mid-century
+modern. And I'll help the
+
+5
+00:00:14,541 --> 00:00:18,160
+homeowner Billy build
+a DIY ramp for his son at
+
+6
+00:00:18,161 --> 00:00:35,230
+camp. Hey there, I'm Kevin
+O'Connor and welcome back
+
+7
+00:00:35,242 --> 00:00:51,010
+to our project here in
+Lexington, Massachusetts.";
+
+            var subtitle = new Subtitle();
+            new SubRip().LoadSubtitle(subtitle, raw.SplitToLines(), null);
+
+            var postProcessor = new AudioToTextPostProcessor("en");
+            var fixedSubtitle = postProcessor.Fix(AudioToTextPostProcessor.Engine.Whisper, subtitle, true, false, false, false, false, true);
+
+            Assert.AreEqual(7, fixedSubtitle.Paragraphs.Count);
+            Assert.IsTrue(fixedSubtitle.Paragraphs[1].Text.EndsWith("hand.", StringComparison.Ordinal));
+            Assert.IsTrue(fixedSubtitle.Paragraphs[2].Text.StartsWith("We're mixing", StringComparison.Ordinal));
+            Assert.IsTrue(fixedSubtitle.Paragraphs[4].Text.EndsWith("camp.", StringComparison.Ordinal));
+            Assert.IsTrue(fixedSubtitle.Paragraphs[5].Text.StartsWith("Hey there", StringComparison.Ordinal));
         }
     }
 }
