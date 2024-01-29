5068fc65df3c3106f2cca6312805bcc12ddf6a6b
Nikolaj Olsson <nikse.dk@gmail.com>
2024-01-29 07:56:06 +0100

    Fix json text decode bug - thx darnn :)

--- src/libse/AutoTranslate/MyMemoryApi.cs
+++ src/libse/AutoTranslate/MyMemoryApi.cs
@@ -23,7 +23,7 @@ namespace Nikse.SubtitleEdit.Core.AutoTranslate
         public void Initialize()
         {
             _httpClient = DownloaderFactory.MakeHttpClient();
-            _httpClient.DefaultRequestHeaders.Add("user-agent", "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36");
+            _httpClient.DefaultRequestHeaders.Add("user-agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:122.0) Gecko/20100101 Firefox/122.0");
             _httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Content-Type", "application/json; charset=UTF-8");
             _httpClient.BaseAddress = new Uri("https://api.mymemory.translated.net/get");
         }
