ccb19c6ca75e34b7e0e353e224e4d0983c6d9e06
Ivandro Jao <ivandrofly@gmail.com>
2024-03-25 10:14:34 +0000

    Remove unused namespaces in AdvancedTextBox.cs
    The System.Threading and System.Threading.Tasks namespaces were included in the AdvancedTextBox.cs file but were not being used. Their inclusion has been removed to clean up the code and improve readability.

--- src/ui/Controls/AdvancedTextBox.cs
+++ src/ui/Controls/AdvancedTextBox.cs
@@ -9,8 +9,6 @@ using System;
 using System.Collections.Generic;
 using System.Drawing;
 using System.Linq;
-using System.Threading;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Nikse.SubtitleEdit.Controls
