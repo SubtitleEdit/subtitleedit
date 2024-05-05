15126e0c7fd8d15b60ffbb351af0892c17bfe1a9
Nikolaj Olsson <nikse.dk@gmail.com>
2024-05-05 18:12:38 +0200

    Work on ttml Imsc11

--- src/libse/SubtitleFormats/TimedTextImsc11.cs
+++ src/libse/SubtitleFormats/TimedTextImsc11.cs
@@ -1,6 +1,7 @@
 ﻿using Nikse.SubtitleEdit.Core.Common;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Xml;
@@ -17,8 +18,11 @@ namespace Nikse.SubtitleEdit.Core.SubtitleFormats
         private static string GetXmlStructure()
         {
             return @"<?xml version='1.0' encoding='UTF-8'?>
-<tt xml:lang='en' xmlns='http://www.w3.org/ns/ttml' xmlns:ttm='http://www.w3.org/ns/ttml#metadata' xmlns:tts='http://www.w3.org/ns/ttml#styling' xmlns:ttp='http://www.w3.org/ns/ttml#parameter' xmlns:ittp='http://www.w3.org/ns/ttml/profile/imsc1#parameter' xmlns:itts='http://www.w3.org/ns/ttml/profile/imsc1#styling' ttp:profile='http://www.w3.org/ns/ttml/profile/imsc1/text' ttp:frameRate='[framerate]' ttp:timeBase='media'>
+<tt xml:lang='en' xmlns='http://www.w3.org/ns/ttml' xmlns:ttm='http://www.w3.org/ns/ttml#metadata' xmlns:tts='http://www.w3.org/ns/ttml#styling' xmlns:ttp='http://www.w3.org/ns/ttml#parameter' xmlns:ittp='http://www.w3.org/ns/ttml/profile/imsc1#parameter' xmlns:itts='http://www.w3.org/ns/ttml/profile/imsc1#styling' ttp:profile='http://www.w3.org/ns/ttml/profile/imsc1/text' ttp:frameRate='[frameRate]' ttp:frameRateMultiplier='[frameRateMultiplier]' ttp:timeBase='media'>
   <head>
+    <metadata>
+      <ttm:title>[title]</ttm:title>
+    </metadata>
     <styling>
       <style xml:id='style.center' tts:color='#ffffff' tts:opacity='1' tts:fontSize='100%' tts:fontFamily='default' tts:textAlign='center'/>
       <style xml:id='italic' tts:shear='16.6667%' tts:opacity='1' tts:fontSize='100%' tts:fontFamily='default'/>
@@ -67,7 +71,54 @@ namespace Nikse.SubtitleEdit.Core.SubtitleFormats
         {
             var xml = new XmlDocument { XmlResolver = null };
             var xmlStructure = GetXmlStructure();
-            xmlStructure = xmlStructure.Replace("[framerate]", ((int)Math.Round(Configuration.Settings.General.CurrentFrameRate, MidpointRounding.AwayFromZero)).ToString());
+            xmlStructure = xmlStructure.Replace("[frameRate]", ((int)Math.Round(Configuration.Settings.General.CurrentFrameRate, MidpointRounding.AwayFromZero)).ToString());
+
+            var frameDiff = Configuration.Settings.General.CurrentFrameRate % 1.0;
+            if (frameDiff < 0.001)
+            {
+                xmlStructure = xmlStructure.Replace("[frameRateMultiplier]", "1 1");
+            }
+            else
+            {
+                xmlStructure = xmlStructure.Replace("[frameRateMultiplier]", "1000 1001");
+            }
+
+            var original = new XmlDocument();
+            try
+            {
+                original.LoadXml(subtitle.Header);
+
+                var namespaceManagerOriginal = new XmlNamespaceManager(original.NameTable);
+                namespaceManagerOriginal.AddNamespace("ttml", TimedText10.TtmlNamespace);
+                namespaceManagerOriginal.AddNamespace("ttp", TimedText10.TtmlParameterNamespace);
+                namespaceManagerOriginal.AddNamespace("tts", TimedText10.TtmlStylingNamespace);
+                namespaceManagerOriginal.AddNamespace("ttm", TimedText10.TtmlMetadataNamespace);
+
+                var nodeTitle = original.DocumentElement.SelectSingleNode("ttml:head/ttml:metadata/ttml:title", namespaceManagerOriginal);
+                if (nodeTitle != null)
+                {
+                    xmlStructure = xmlStructure.Replace("[title]", nodeTitle.InnerXml);
+                }
+
+                var attr = original.DocumentElement.Attributes["ttp:timeBase"];
+                if (attr?.Value != null && (attr.Value == "clock" || attr.Value == "smpte"))
+                {
+                    xmlStructure = xmlStructure.Replace("ttp:timeBase=\"media\"", $"ttp:timeBase=\"{attr.Value}\"");
+                }
+            }
+            catch 
+            {
+                // ignore
+            }
+
+            var xmlTitle = string.IsNullOrEmpty(subtitle.FileName) ? title : Path.GetFileNameWithoutExtension(subtitle.FileName);
+            if (string.IsNullOrEmpty(xmlTitle))
+            {
+                xmlTitle = "Untitled";
+            }
+
+            xmlStructure = xmlStructure.Replace("[title]", HtmlUtil.EncodeNamed(xmlTitle));
+
 
             var language = LanguageAutoDetect.AutoDetectGoogleLanguage(subtitle);
             xmlStructure = xmlStructure.Replace("lang=\"en\"", $"lang=\"{language}\"");
@@ -256,11 +307,13 @@ namespace Nikse.SubtitleEdit.Core.SubtitleFormats
                     var frameRateMultiplier = xml.DocumentElement.Attributes["ttp:frameRateMultiplier"];
                     if (frameRateMultiplier != null)
                     {
-                        if (frameRateMultiplier.InnerText == "999 1000" && Math.Abs(fr - 30) < 0.01)
+                        if ((frameRateMultiplier.InnerText == "999 1000" ||
+                             frameRateMultiplier.InnerText == "1000 1001") && Math.Abs(fr - 30) < 0.01)
                         {
                             Configuration.Settings.General.CurrentFrameRate = 29.97;
                         }
-                        else if (frameRateMultiplier.InnerText == "999 1000" && Math.Abs(fr - 24) < 0.01)
+                        else if ((frameRateMultiplier.InnerText == "999 1000" ||
+                                  frameRateMultiplier.InnerText == "1000 1001") && Math.Abs(fr - 24) < 0.01)
                         {
                             Configuration.Settings.General.CurrentFrameRate = 23.976;
                         }
