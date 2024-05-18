7fa2d58c882fad611379238ba7f7606bae744893
Nikolaj Olsson <nikse.dk@gmail.com>
2024-05-18 08:12:20 +0200

    Check for null in QuartsPlayer - thx giogio33 :) Try to fix #8384

--- src/ui/Logic/VideoPlayers/QuartsPlayer.cs
+++ src/ui/Logic/VideoPlayers/QuartsPlayer.cs
@@ -116,19 +116,19 @@ namespace Nikse.SubtitleEdit.Logic.VideoPlayers
 
         public override void Play()
         {
-            _quartzFilgraphManager.Run();
+            _quartzFilgraphManager?.Run();
             _isPaused = false;
         }
 
         public override void Pause()
         {
-            _quartzFilgraphManager.Pause();
+            _quartzFilgraphManager?.Pause();
             _isPaused = true;
         }
 
         public override void Stop()
         {
-            _quartzFilgraphManager.Stop();
+            _quartzFilgraphManager?.Stop();
             _isPaused = true;
         }
 
@@ -140,8 +140,8 @@ namespace Nikse.SubtitleEdit.Logic.VideoPlayers
         {
             const int wsChild = 0x40000000;
 
-            string ext = System.IO.Path.GetExtension(videoFileName)?.ToLowerInvariant();
-            bool isAudio = Utilities.AudioFileExtensions.Contains(ext);
+            var ext = System.IO.Path.GetExtension(videoFileName)?.ToLowerInvariant();
+            var isAudio = Utilities.AudioFileExtensions.Contains(ext);
 
             OnVideoLoaded = onVideoLoaded;
             OnVideoEnded = onVideoEnded;
