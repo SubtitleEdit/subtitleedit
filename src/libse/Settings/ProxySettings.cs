181a8dd7164ede57d014fcad9674eaa61f352f19
Ivandro Jao <Ivandrofly@gmail.com>
2024-07-10 21:46:44 +0100

    Add data encoding and proxy management to ProxySettings
    Additional data encoding functionality and proxy management methods have been added to the ProxySettings class. Also updated references inside LegacyDownloader.cs to utilize the new CreateProxyFromSettings method in the ProxySettings class. These changes allow more robust and centralized control of application's proxy configurations.

--- src/libse/Settings/ProxySettings.cs
+++ src/libse/Settings/ProxySettings.cs
@@ -1,5 +1,7 @@
 using System;
+using System.Net;
 using System.Text;
+using Nikse.SubtitleEdit.Core.Common;
 
 namespace Nikse.SubtitleEdit.Core.Settings
 {
@@ -12,13 +14,53 @@ namespace Nikse.SubtitleEdit.Core.Settings
         public string Password { get; set; }
         public string Domain { get; set; }
 
+        /// <summary>
+        /// Decodes the given password by converting it from base64 string representation to plain text.
+        /// </summary>
+        /// <returns>The decoded plain text password.</returns>
+        /// <param name="password">The password to be decoded.</param>
         public string DecodePassword()
         {
             return Encoding.UTF8.GetString(Convert.FromBase64String(Password));
         }
+
+        /// <summary>
+        /// Encodes the given password by converting it to base64 string representation.
+        /// </summary>
+        /// <param name="unencryptedPassword">The unencrypted password to be encoded.</param>
         public void EncodePassword(string unencryptedPassword)
         {
             Password = Convert.ToBase64String(Encoding.UTF8.GetBytes(unencryptedPassword));
+        }
+
+        /// <summary>
+        /// Determines if the proxy should use default credentials.
+        /// </summary>
+        /// <returns>True if default credentials should be used, false otherwise.</returns>
+        public bool IsDefaultCredential() => string.IsNullOrEmpty(UserName);
+
+        /// <summary>
+        /// Creates a proxy from the current proxy settings.
+        /// </summary>
+        /// <returns>A new instance of the <see cref="IWebProxy"/> interface that represents the proxy defined by the current proxy settings.</returns>
+        public IWebProxy CreateProxyFromSettings()
+        {
+            return new WebProxy(ProxyAddress)
+            {
+                UseDefaultCredentials = IsDefaultCredential(),
+                Credentials = UseDefaultCredentials ? null : NewNetworkCredential(),
+            };
+        }
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="NetworkCredential"/> class based on the current proxy settings.
+        /// </summary>
+        /// <returns>A new instance of the <see cref="NetworkCredential"/> class that represents the network credentials defined by the current proxy settings.</returns>
+        private NetworkCredential NewNetworkCredential()
+        {
+            return string.IsNullOrEmpty(Domain)
+                ? new NetworkCredential(UserName, DecodePassword())
+                : new NetworkCredential(UserName, DecodePassword(), Domain);
         }
     }
 }
