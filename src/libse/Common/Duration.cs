78d5e48a69357014bd5cd6ab86a8e0acad8669f5
Ivandro Jao <Ivandrofly@gmail.com>
2024-07-17 08:36:08 +0100

    Rename variable in Duration class
    The commit changes the private variable '_value' in the Duration class to '_milliseconds'. This change provides a more accurate representation of the variable's function, improving code readability and maintainability.
    
    Signed-off-by: Ivandro Jao <Ivandrofly@gmail.com>

--- src/libse/Common/Duration.cs
+++ src/libse/Common/Duration.cs
@@ -5,24 +5,24 @@
     /// </summary>
     public readonly struct Duration
     {
-        private readonly double _value;
+        private readonly double _milliseconds;
 
-        public Duration(double value) => _value = value;
+        public Duration(double milliseconds) => _milliseconds = milliseconds;
 
         /// <summary>
         /// Represents a duration of time in milliseconds.
         /// </summary>
-        public double Milliseconds => _value;
+        public double Milliseconds => _milliseconds;
 
         /// <summary>
         /// Represents a duration of time in seconds.
         /// </summary>
-        public double Seconds => _value / TimeCode.BaseUnit;
+        public double Seconds => _milliseconds / TimeCode.BaseUnit;
 
         /// <summary>
         /// Converts the duration to a TimeCode object.
         /// </summary>
         /// <returns>A TimeCode object representing the duration.</returns>
-        public TimeCode ToTimeCode() => new TimeCode(_value);
+        public TimeCode ToTimeCode() => new TimeCode(_milliseconds);
     }
 }
