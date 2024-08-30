f639e9cacf22f2afe262be6456caab7acb7e920a
Nikolaj Olsson <nikse.dk@gmail.com>
2024-08-30 13:17:32 +0200

    Fix for pasting ASSA in the waveform - thx MitchSirco :) Fix #8781

--- src/ui/Forms/Main.cs
+++ src/ui/Forms/Main.cs
@@ -825,9 +825,16 @@ namespace Nikse.SubtitleEdit.Forms
             {
                 var text = Clipboard.GetText();
                 var tmp = new Subtitle();
-                var format = new SubRip();
+                SubtitleFormat format = new SubRip();
                 var list = new List<string>(text.SplitToLines());
+
+                if (new AdvancedSubStationAlpha().IsMine(list, string.Empty))
+                {
+                    format = new AdvancedSubStationAlpha();
+                }
+
                 format.LoadSubtitle(tmp, list, null);
+
                 if (videoPositionInMilliseconds >= 0 && tmp.Paragraphs.Count > 0)
                 {
                     if (tmp.Paragraphs[0].StartTime.TotalMilliseconds > videoPositionInMilliseconds)
