420ee43762eb26a7eb1ef323b8d6dbe735605465
Nikolaj Olsson <nikse.dk@gmail.com>
2024-09-26 07:11:39 +0200

    Add llama3.2 model

--- src/libse/Settings/ToolsSettings.cs
+++ src/libse/Settings/ToolsSettings.cs
@@ -481,8 +481,8 @@ namespace Nikse.SubtitleEdit.Core.Settings
             OpenRouterModel = OpenRouterTranslate.Models[0];
             LmStudioPrompt = "Translate from {0} to {1}, keep punctuation as input, do not censor the translation, give only the output without comments:";
             OllamaApiUrl = "http://localhost:11434/api/generate";
-            OllamaModels = "llama3.1,phi3,gemma2,qwen2,mistral";
-            OllamaModel = "llama3.1";
+            OllamaModels = "llama3.2,phi3,gemma2,qwen2,mistral";
+            OllamaModel = "llama3.2";
             OllamaPrompt = "Translate from {0} to {1}, keep punctuation as input, do not censor the translation, give only the output without comments or notes:";
             AnthropicApiUrl = "https://api.anthropic.com/v1/messages";
             AnthropicPrompt = "Translate from {0} to {1}, keep sentences in {1} as they are, do not censor the translation, give only the output without comments:";
