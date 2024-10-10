add6580a3e5c57afa66e27cc797e6691f989aad7
Ivandro Jao <Ivandrofly@gmail.com>
2024-10-10 09:49:27 +0100

    Refactor to use asynchronous methods for chapter retrieval
    Converted synchronous chapter retrieval methods to asynchronous in MatroskaFile class. Updated relevant calls in Main.cs to leverage the new asynchronous methods, improving responsiveness and efficiency.
    
    Signed-off-by: Ivandro Jao <Ivandrofly@gmail.com>

--- src/ui/Forms/Main.cs
+++ src/ui/Forms/Main.cs
@@ -33757,7 +33757,7 @@ namespace Nikse.SubtitleEdit.Forms
             var chaps = new List<MatroskaChapter>();
             using (var matroska = new MatroskaFile(_videoFileName))
             {
-                chaps = await Task.Run(() => matroska.GetChapters());
+                chaps = await matroska.GetChaptersAsync();
             }
 
             if (chaps?.Count > 0)
@@ -36887,7 +36887,7 @@ namespace Nikse.SubtitleEdit.Forms
             });
         }
 
-        private void videoInfoToolStripMenuItem_Click(object sender, EventArgs e)
+        private void async videoInfoToolStripMenuItem_Click(object sender, EventArgs e)
         {
             if (string.IsNullOrEmpty(_videoFileName) || _videoInfo == null)
             {
@@ -36945,7 +36945,7 @@ namespace Nikse.SubtitleEdit.Forms
                 if (mkvParser.IsValid)
                 {
                     sb.AppendLine($"Container: Matroska (mkv/webm)");
-                    var chapters = mkvParser.GetChapters();
+                    var chapters = await mkvParser.GetChaptersAsync();
                     if (chapters.Count > 0)
                     {
                         sb.AppendLine($" - Chapters: {chapters.Count}");
