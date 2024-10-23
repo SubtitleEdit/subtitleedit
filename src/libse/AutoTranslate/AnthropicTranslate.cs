2aa5d901fba73c097daefd2feb49af8976f99b23
nikolaj.olsson <nikolaj.olsson@visma.com>
2024-10-23 13:18:37 +0200

    Update Anthropic Claude Sonnet model name to "claude-3-5-sonnet-latest"

--- src/libse/AutoTranslate/AnthropicTranslate.cs
+++ src/libse/AutoTranslate/AnthropicTranslate.cs
@@ -29,7 +29,7 @@ namespace Nikse.SubtitleEdit.Core.AutoTranslate
         /// </summary>
         public static string[] Models => new[]
         {
-            "claude-3-5-sonnet-20240620", 
+            "claude-3-5-sonnet-latest", 
             "claude-3-opus-20240229", 
             "claude-3-sonnet-20240229", 
             "claude-3-haiku-20240307"
