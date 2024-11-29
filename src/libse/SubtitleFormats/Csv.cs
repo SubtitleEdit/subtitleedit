664e86b890cf0c0e01d0e1766b3754f3d5fbca2a
Nikolaj Olsson <nikse.dk@gmail.com>
2024-11-29 15:30:40 +0100

    Fix wrong timestamp for format "Csv" - thx MariaB99 :) Fix #9045

--- src/libse/SubtitleFormats/Csv.cs
+++ src/libse/SubtitleFormats/Csv.cs
@@ -17,9 +17,9 @@ namespace Nikse.SubtitleEdit.Core.SubtitleFormats
 
         public override bool IsMine(List<string> lines, string fileName)
         {
-            int fine = 0;
-            int failed = 0;
-            foreach (string line in lines)
+            var fine = 0;
+            var failed = 0;
+            foreach (var line in lines)
             {
                 if (CsvLine.IsMatch(line))
                 {
@@ -38,9 +38,9 @@ namespace Nikse.SubtitleEdit.Core.SubtitleFormats
             const string format = "{1}{0}{2}{0}{3}{0}\"{4}\"";
             var sb = new StringBuilder();
             sb.AppendLine(string.Format(format, Separator, "Number", "Start time in milliseconds", "End time in milliseconds", "Text"));
-            foreach (Paragraph p in subtitle.Paragraphs)
+            foreach (var p in subtitle.Paragraphs)
             {
-                sb.AppendLine(string.Format(format, Separator, p.Number, p.StartTime.TotalMilliseconds, p.EndTime.TotalMilliseconds, p.Text.Replace(Environment.NewLine, "\n")));
+                sb.AppendLine(string.Format(format, Separator, p.Number, (long)Math.Round(p.StartTime.TotalMilliseconds, MidpointRounding.AwayFromZero), (long)Math.Round(p.EndTime.TotalMilliseconds, MidpointRounding.AwayFromZero), p.Text.Replace(Environment.NewLine, "\n")));
             }
             return sb.ToString().Trim();
         }
@@ -48,20 +48,20 @@ namespace Nikse.SubtitleEdit.Core.SubtitleFormats
         public override void LoadSubtitle(Subtitle subtitle, List<string> lines, string fileName)
         {
             _errorCount = 0;
-            bool continuation = false;
+            var continuation = false;
             Paragraph p = null;
-            foreach (string line in lines)
+            foreach (var line in lines)
             {
                 if (CsvLine.IsMatch(line))
                 {
-                    string[] parts = line.Split(Separator.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+                    var parts = line.Split(Separator.ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
                     if (parts.Length == 4)
                     {
                         try
                         {
-                            int start = Convert.ToInt32(Utilities.FixQuotes(parts[1]));
-                            int end = Convert.ToInt32(Utilities.FixQuotes(parts[2]));
-                            string text = Utilities.FixQuotes(parts[3]);
+                            var start = Convert.ToInt32(Utilities.FixQuotes(parts[1]));
+                            var end = Convert.ToInt32(Utilities.FixQuotes(parts[2]));
+                            var text = Utilities.FixQuotes(parts[3]);
                             p = new Paragraph(text, start, end);
                             subtitle.Paragraphs.Add(p);
                             continuation = parts[3].StartsWith('"') && !parts[3].EndsWith('"');
@@ -91,6 +91,5 @@ namespace Nikse.SubtitleEdit.Core.SubtitleFormats
             }
             subtitle.Renumber();
         }
-
     }
 }
