f99511520834280103efeb8e49e271cebe85635e
Nikolaj Olsson <nikse.dk@gmail.com>
2024-11-29 07:48:12 +0100

    Work on unknown csv importer

--- src/Test/Core/CsvUtilTest.cs
+++ src/Test/Core/CsvUtilTest.cs
@@ -78,5 +78,57 @@ namespace Test.Core
             Assert.AreEqual("Thank \"you\".", result[2]);
             Assert.AreEqual(false, con);
         }
+
+        [TestMethod]
+        public void CsvSplitLines1()
+        {
+            // Arrange
+            var lines = new List<string>
+            {
+                "1,How are you?",
+                "2,\"How are you?\"",
+                "3,\"Thanks, I'm fine.\"",
+                "4,\"Well,",
+                "that is good to hear.\"",
+                "5,Bye!",
+            };
+
+            // Act
+            var result = CsvUtil.CsvSplitLines(lines, ',');
+
+            // Assert
+            Assert.AreEqual(5, result.Count);
+            Assert.AreEqual("How are you?", result[0][1]);
+            Assert.AreEqual("How are you?", result[1][1]);
+            Assert.AreEqual("Thanks, I'm fine.", result[2][1]);
+            Assert.AreEqual("Well," + Environment.NewLine + "that is good to hear.", result[3][1]);
+            Assert.AreEqual("Bye!", result[4][1]);
+        }
+
+        [TestMethod]
+        public void CsvSplitLines2()
+        {
+            // Arrange
+            var lines = new List<string>
+            {
+                "1;How are you?",
+                "2;\"How are you?\"",
+                "3;\"Thanks, I'm fine.\"",
+                "4;\"Well,",
+                "that is good to hear.\"",
+                "5;Bye!",
+            };
+
+            // Act
+            var result = CsvUtil.CsvSplitLines(lines, ';');
+
+            // Assert
+            Assert.AreEqual(5, result.Count);
+            Assert.AreEqual("How are you?", result[0][1]);
+            Assert.AreEqual("How are you?", result[1][1]);
+            Assert.AreEqual("Thanks, I'm fine.", result[2][1]);
+            Assert.AreEqual("Well," + Environment.NewLine + "that is good to hear.", result[3][1]);
+            Assert.AreEqual("Bye!", result[4][1]);
+        }
     }
 }
--- src/libse/Common/CsvUtil.cs
+++ src/libse/Common/CsvUtil.cs
@@ -1,11 +1,69 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Nikse.SubtitleEdit.Core.Common
 {
     public static class CsvUtil
     {
-        public static string[] CsvSplit(string line, bool quoteOn, out bool continuation)
+        public static List<List<string>> CsvSplitLines(List<string> lines, char separator)
+        {
+            var result = new List<List<string>>();
+            var continuation = false;
+            var lineResult = new List<string>();
+            foreach (var line in lines)
+            {
+                if (!continuation)
+                {
+                    var parts = CsvSplit(line, false, out var con, separator);
+                    continuation = con;
+
+                    lineResult.AddRange(parts);
+
+                    if (!continuation)
+                    {
+                        result.Add(lineResult);
+                        lineResult = new List<string>();
+                    }
+                }
+                else
+                {
+                    var parts = CsvSplit(line, true, out var con).ToList();
+                    continuation = con;
+
+                    if (parts.Count > 0)
+                    {
+                        if (lineResult.Count > 0)
+                        {
+                            lineResult[lineResult.Count - 1] += Environment.NewLine + parts[0];
+                        }
+                        else
+                        {
+                            lineResult.Add(parts[0]);
+                        }
+
+                        parts.RemoveAt(0);
+                        lineResult.AddRange(parts);
+                    }
+
+                    if (!continuation)
+                    {
+                        result.Add(lineResult);
+                        lineResult = new List<string>();
+                    }
+                }
+            }
+
+            if (lineResult.Count > 0)
+            {
+                result.Add(lineResult);
+            }
+
+            return result;
+        }
+
+        public static string[] CsvSplit(string line, bool quoteOn, out bool continuation, char separator = ',')
         {
             var lines = new List<string>();
             var stringOn = false;
@@ -24,7 +82,19 @@ namespace Nikse.SubtitleEdit.Core.Common
                 {
                     switch (ch)
                     {
-                        case ',' when !quoteOn:
+                        case ',' when !quoteOn && separator == ',':
+                            lines.Add(item.ToString());
+                            item = new StringBuilder();
+                            index++;
+                            stringOn = false;
+                            continue;
+                        case ';' when !quoteOn && separator == ';':
+                            lines.Add(item.ToString());
+                            item = new StringBuilder();
+                            index++;
+                            stringOn = false;
+                            continue;
+                        case '\t' when !quoteOn && separator == '\t':
                             lines.Add(item.ToString());
                             item = new StringBuilder();
                             index++;
@@ -61,7 +131,7 @@ namespace Nikse.SubtitleEdit.Core.Common
                     index++;
                     continue;
                 }
-                else if (ch == ',')
+                else if (ch == separator)
                 {
                     lines.Add(item.ToString());
                     item = new StringBuilder();
