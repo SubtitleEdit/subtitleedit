f974f02686e1c25ebd1c023b4469ec640b20ce85
Nikolaj Olsson <nikse.dk@gmail.com>
2025-02-02 16:05:35 +0100

    Fix DeepL retry - thx Hypn0iid :) Work on #9305

--- src/libse/AutoTranslate/DeepLXTranslate.cs
+++ src/libse/AutoTranslate/DeepLXTranslate.cs
@@ -50,25 +50,32 @@ namespace Nikse.SubtitleEdit.Core.AutoTranslate
         {
             const int httpStatusCodeTooManyRequests = 429;
 
-            var postContent = new FormUrlEncodedContent(new[]
-            {
-                new KeyValuePair<string, string>("text", text),
-                new KeyValuePair<string, string>("target_lang", targetLanguageCode),
-                new KeyValuePair<string, string>("source_lang", sourceLanguageCode),
-            });
+            var postContent = MakeContent(text, sourceLanguageCode, targetLanguageCode);
             var result = _client.PostAsync("/v2/translate", postContent, cancellationToken).Result;
             var resultContent = result.Content.ReadAsStringAsync().Result;
 
             if (result.StatusCode == HttpStatusCode.ServiceUnavailable || (int)result.StatusCode == httpStatusCodeTooManyRequests)
             {
                 Task.Delay(2555).Wait();
+                postContent = MakeContent(text, sourceLanguageCode, targetLanguageCode);
                 result = _client.PostAsync("/v2/translate", postContent, cancellationToken).Result;
                 resultContent = result.Content.ReadAsStringAsync().Result;
             }
 
             if (result.StatusCode == HttpStatusCode.ServiceUnavailable || (int)result.StatusCode == httpStatusCodeTooManyRequests)
             {
+                try
+                {
+                    _client.Dispose();
+                }
+                catch
+                {
+                    // ignore
+                }
                 Task.Delay(5307).Wait();
+                _client = new HttpClient();
+                _client.BaseAddress = new Uri(_apiUrl.Trim().TrimEnd('/'));
+                postContent = MakeContent(text, sourceLanguageCode, targetLanguageCode);
                 result = _client.PostAsync("/v2/translate", postContent, cancellationToken).Result;
                 resultContent = result.Content.ReadAsStringAsync().Result;
             }
@@ -117,6 +124,16 @@ namespace Nikse.SubtitleEdit.Core.AutoTranslate
                 SeLogger.Error(ex, "DeepLXTranslate.Translate: " + ex.Message + Environment.NewLine + resultContent);
                 throw;
             }
+        }
+
+        private static FormUrlEncodedContent MakeContent(string text, string sourceLanguageCode, string targetLanguageCode)
+        {
+            return new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("text", text),
+                new KeyValuePair<string, string>("target_lang", targetLanguageCode),
+                new KeyValuePair<string, string>("source_lang", sourceLanguageCode),
+            });
         }
     }
 }
