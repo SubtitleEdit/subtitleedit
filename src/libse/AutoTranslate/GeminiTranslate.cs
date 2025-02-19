9476455401a356c462596da9265364cf775273ee
Nikolaj Olsson <nikse.dk@gmail.com>
2025-02-19 17:56:38 +0100

    Fix namespace

--- src/libse/AutoTranslate/GeminiTranslate.cs
+++ src/libse/AutoTranslate/GeminiTranslate.cs
@@ -5,7 +5,6 @@ using System;
 using System.Collections.Generic;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Security.Policy;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
