2879650a2096079dfb0741794187d96411672c38
Nikolaj Olsson <nikse.dk@gmail.com>
2025-04-20 21:43:29 +0200

    Update unit tests

--- tests/Logic/VobSub/VobSubTest.cs
+++ tests/Logic/VobSub/VobSubTest.cs
@@ -26,5 +26,51 @@ namespace Tests.Logic.VobSub
 
             Assert.Equal(2, list.Count);
         }
+
+        [Fact]
+        public void VobSubNonStandardLengthPackets()
+        {
+            string fileName = Path.Combine(Directory.GetCurrentDirectory(), "Files", "sample_VodSub_nonStandardLength.sub");
+
+            var reader = new VobSubParser(true);
+            reader.Open(fileName);
+            // check if we got the right number of packs
+            Assert.Equal(6, reader.VobSubPacks.Count);
+            var list = reader.MergeVobSubPacks();
+
+            Assert.Equal(2, list.Count);
+        }
+
+        [Fact]
+        public void VobSubStuffingBytes()
+        {
+            string fileName = Path.Combine(Directory.GetCurrentDirectory(), "Files", "sample_VodSub_stuffingBytes.sub");
+
+            var reader = new VobSubParser(true);
+            reader.Open(fileName);
+            // check if we got the right number of packs
+            Assert.Equal(3, reader.VobSubPacks.Count);
+            // check if the stuffing data was correctly striped.
+            Assert.Equal(0, reader.VobSubPacks[2].Mpeg2Header.PackStuffingLength);
+
+            var list = reader.MergeVobSubPacks();
+            Assert.Single(list);
+        }
+
+        [Fact]
+        public void VodSubWindowPaddingWithoutPaddingStream()
+        {
+            // Older versions of SE and other encoders do not use padding streams to fit packs
+            // to a byte 2048 window. they just fill the rest of the window with 0xFF instead.
+            string fileName = Path.Combine(Directory.GetCurrentDirectory(), "Files", "sample_VodSub_windowStuffingWithoutPaddingStream.sub");
+
+            var reader = new VobSubParser(true);
+            reader.Open(fileName);
+            // check if we got the right number of packs
+            Assert.Equal(8, reader.VobSubPacks.Count);
+
+            var list = reader.MergeVobSubPacks();
+            Assert.Equal(2, list.Count);
+        }
     }
 }
