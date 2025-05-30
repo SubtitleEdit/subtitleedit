584c283ede4468ac90d5bafd5ef218f3bbd92845
Nikolaj Olsson <nikse.dk@gmail.com>
2025-05-30 14:55:58 +0200

    Add win7 ffmpeg/mpv support (perhaps) - thx Mark :)

--- src/ui/Forms/DownloadFfmpeg.cs
+++ src/ui/Forms/DownloadFfmpeg.cs
@@ -49,12 +49,21 @@ namespace Nikse.SubtitleEdit.Forms
             DialogResult = DialogResult.Cancel;
         }
 
+        private static bool IsWindows7()
+        {
+            return Environment.OSVersion.Version.Major == 6 && Environment.OSVersion.Version.Minor == 1;
+        }
+
         private void DownloadFfmpeg_Shown(object sender, EventArgs e)
         {
             var url = "https://github.com/SubtitleEdit/support-files/releases/download/ffmpeg-2025-03-31/ffmpeg-2025-03-31.zip";
             if (IntPtr.Size == 32)
             {
                 url = "https://github.com/SubtitleEdit/support-files/releases/download/ffmpegwin32v5.1/ffmpeg-win32-n5.1.zip";
+            }
+            else if (IsWindows7())
+            {
+                url = "https://github.com/SubtitleEdit/support-files/releases/download/ffmpegwin32v5.1/ffmpeg-2024-05-20-win7.zip";
             }
 
             if (_title.Contains("ffprobe", StringComparison.OrdinalIgnoreCase))
@@ -118,6 +127,7 @@ namespace Nikse.SubtitleEdit.Forms
                 "18d4d0d9a780292385a088965ddc7c773d9a5c524b1f9ecd6336287f03490b2507d59ce6fa552f1f316168747cd75f93f395de2dab7b2986351783814937d19c", // ffmpeg 32
                 "e715d308a666b8f16cc6585a14316029d905e42e2af8a5ad0a543360d80badfbdf3748080d825149f9e727ae56274f62121f5625929dba3b884b86ecd3a2a139", // ffmpeg 64
                 "72ee50ce0b529d550606c1b1193480a864046888826108249ca94503e892685ea354ea24d603a051fc4c3704895dda04a5e7a9b6f234ade5b29e6c85c1a73591", // ffprobe
+                "b7fba2d49212660d67652f4ffb5760fdc5c7c4732fa08af630bd2674fd90a9dc5de05cad543204f3846813bb566c708addb8d72b8b5c6c9ff973522b2fe25037", // ffmpeg for Windows 7 
             };
             var hash = Utilities.GetSha512Hash(downloadStream.ToArray());
             if (!sha512Hashes.Contains(hash))
