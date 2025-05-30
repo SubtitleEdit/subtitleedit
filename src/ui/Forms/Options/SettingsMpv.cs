1c9a2ddc3bb797b3deb19925048c816ecd37d95c
Nikolaj Olsson <nikse.dk@gmail.com>
2025-05-30 14:55:58 +0200

    Add win7 ffmpeg/mpv support (perhaps) - thx Mark :)

--- src/ui/Forms/Options/SettingsMpv.cs
+++ src/ui/Forms/Options/SettingsMpv.cs
@@ -29,9 +29,19 @@ namespace Nikse.SubtitleEdit.Forms.Options
             _cancellationTokenSource = new CancellationTokenSource();
         }
 
+        private static bool IsWindows7()
+        {
+            return Environment.OSVersion.Version.Major == 6 && Environment.OSVersion.Version.Minor == 1;
+        }
+
         private void ButtonDownloadClick(object sender, EventArgs e)
         {
             _downloadUrl = "https://github.com/SubtitleEdit/support-files/releases/download/libmpv-2025-01-25/libmpv2-" + IntPtr.Size * 8 + ".zip";
+            if (IsWindows7())
+            {
+                _downloadUrl = "https://github.com/SubtitleEdit/support-files/releases/download/libmpv-2024-06-09/libmpv2-" + IntPtr.Size * 8 + ".zip";
+            }
+
             try
             {
                 labelPleaseWait.Text = LanguageSettings.Current.General.PleaseWait;
@@ -84,6 +94,8 @@ namespace Nikse.SubtitleEdit.Forms.Options
             {
                 "e8bd85b3f536ed36e48d7ce7f0110ed21c020c22e9cf82567d128bb08a4c3d5957c408bed5300323420c0769276e5ae4fa131dbb0246b0853c3ce44e0ed6a203", // 32-bit
                 "318cb5688f95e1f2e307594d570b402005c6a3874c09ea757bdf6b9a845dbffa826c7652471e7435ddf38bb1a5c4d6d0eed40038824b37ea31563d814a15d4cc", // 64-bit
+                "a6d6ee7b2a1173a77ee07e046d6f9875842d88ee41a8a935ff5f906300553a704c0b39ae8a1a42eb7c7b1dc06b4e6e9d28d62a2d93384b70ce45de9d9ac8f8d7", // Win7, 32-bit
+                "ae62aa72a7678199c8470cbec38f94e930469332f90a2da77dab1d89ef86e3ba71a0d5258cbf1fe8680e84736ea123ebdc5737a93384de1106fba6c0b78ea560", // Win7, 64-bit
             };
             if (!validHashes.Contains(hash))
             {
