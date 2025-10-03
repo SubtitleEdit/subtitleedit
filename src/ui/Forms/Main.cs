bd875f00426162d9d57f951e2c7cadfb6610083a
Nikolaj Olsson <nikse.dk@gmail.com>
2025-10-03 12:27:20 +0200

    Allow  vosk v0.3.45 - thx vivadavid :) Fix #9976

--- src/ui/Forms/Main.cs
+++ src/ui/Forms/Main.cs
@@ -36574,9 +36574,11 @@ namespace Nikse.SubtitleEdit.Forms
                 IntPtr.Size * 8 == 32
                 ? "1cc13d8e2ffd3ad7ca76941c99e8ad00567d0b8135878c3a80fb938054cf98bde1f692647e6d19df7526c98aa5ad975d72dba20bf1759baedba5c753a14480bb"
                 : "77479a934650b40968d54dcf71fce17237c59b62b6c64ad3d6b5433486b76b6202eb956e93597ba466c67aa0d553db7b2863e0aeb8856a6dd29a3aba3a14bf66";
+
             var hash = Utilities.GetSha512Hash(FileUtil.ReadAllBytesShared(voskDll));
 
-            _hasCurrentVosk = currentVoskDllSha512Hash == hash;
+            _hasCurrentVosk = hash == currentVoskDllSha512Hash ||
+                hash == "88271d24efa7d75e0902291c755941c6d4cdd80598d4e2e00b669194d09e527345903c7b57bde9f829796b82ce8f01d142b9f65e133239504d42b042aace593c";
             return _hasCurrentVosk;
         }
 
