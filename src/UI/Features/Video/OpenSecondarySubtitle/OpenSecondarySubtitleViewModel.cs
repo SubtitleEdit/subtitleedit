c6311b3fc1def159d840149de75f4126d7307e55
Nikolaj Olsson <nikse.dk@gmail.com>
2026-03-12 20:19:10 +0100

    work o secondary subtitle

--- src/UI/Features/Video/OpenSecondarySubtitle/OpenSecondarySubtitleViewModel.cs
+++ src/UI/Features/Video/OpenSecondarySubtitle/OpenSecondarySubtitleViewModel.cs
@@ -14,10 +14,12 @@ using Nikse.SubtitleEdit.Features.Sync.VisualSync;
 using Nikse.SubtitleEdit.Features.Video.BurnIn;
 using Nikse.SubtitleEdit.Logic;
 using Nikse.SubtitleEdit.Logic.Config;
+using Nikse.SubtitleEdit.Logic.Media;
 using Nikse.SubtitleEdit.Logic.VideoPlayers.LibMpvDynamic;
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
@@ -26,30 +28,25 @@ namespace Nikse.SubtitleEdit.Features.Video.OpenFromUrl;
 
 public partial class OpenSecondarySubtitleViewModel : ObservableObject
 {
-    [ObservableProperty] private bool _alignmentAn1;
-    [ObservableProperty] private bool _alignmentAn2;
-    [ObservableProperty] private bool _alignmentAn3;
-    [ObservableProperty] private bool _alignmentAn4;
-    [ObservableProperty] private bool _alignmentAn5;
-    [ObservableProperty] private bool _alignmentAn6;
-    [ObservableProperty] private bool _alignmentAn7;
-    [ObservableProperty] private bool _alignmentAn8;
-    [ObservableProperty] private bool _alignmentAn9;
     [ObservableProperty] private Color _subtitleColor;
     [ObservableProperty] private FontBoxItem _selectedFontBoxType;
     [ObservableProperty] private decimal _fontSize;
     [ObservableProperty] private ObservableCollection<SubtitleDisplayItem> _paragraphs;
     [ObservableProperty] private int _selectedParagraphIndex = -1;
+    [ObservableProperty] private AlignmentItem _selectedFontAlignment;
 
     public ObservableCollection<FontBoxItem> FontBoxTypes { get; }
+    public ObservableCollection<AlignmentItem> FontAlignments { get; }
     public VideoPlayerControl VideoPlayerControl { get; set; }
     public ComboBox ComboBoxParagraphs { get; set; }
 
     private Subtitle _secondarySubtitle = new Subtitle();
     private Subtitle _subtitle = new Subtitle();
     private SubtitleFormat _subtitleFormat = new SubRip();
+    private SubtitleFormat _assaFormat = new AdvancedSubStationAlpha();
     private readonly IWindowService _windowService;
     private string? _videoFileName;
+    private FfmpegMediaInfo2? _mediaInfo;
     private readonly string _tempSubtitleFileName;
     private LibMpvDynamicPlayer? _mpvPlayer;
     private bool _isSubtitleLoaded;
@@ -63,19 +60,22 @@ public partial class OpenSecondarySubtitleViewModel : ObservableObject
     public OpenSecondarySubtitleViewModel(IWindowService windowService)
     {
         _windowService = windowService;
-        _alignmentAn2 = true;
-        _subtitleColor = Colors.White;
-        _fontSize = 20;
+
+        SubtitleColor = Colors.White;
         FontBoxTypes = new ObservableCollection<FontBoxItem>
         {
             new(FontBoxType.None, Se.Language.General.None),
             new(FontBoxType.OneBox, Se.Language.Video.BurnIn.OneBox),
             new(FontBoxType.BoxPerLine, Se.Language.Video.BurnIn.BoxPerLine),
         };
-        _selectedFontBoxType = FontBoxTypes[0];
-        _paragraphs = new ObservableCollection<SubtitleDisplayItem>();
+        SelectedFontBoxType = FontBoxTypes[0];
+        FontAlignments = new ObservableCollection<AlignmentItem>(AlignmentItem.Alignments);
+        SelectedFontAlignment = AlignmentItem.Alignments[1]; // an8 = Top-center
+        Paragraphs = new ObservableCollection<SubtitleDisplayItem>();
+
         _tempSubtitleFileName = Path.Combine(Path.GetTempPath(), Guid.NewGuid().ToString() + ".ass");
         _oldSubtitleText = string.Empty;
+
         VideoPlayerControl = new VideoPlayerControl(new VideoPlayerInstanceNone());
         ComboBoxParagraphs = new ComboBox();
         VideoPlayerControl.SurfacePointerPressed += (_, _) => VideoPlayerControl.TogglePlayPause();
@@ -101,7 +101,7 @@ public partial class OpenSecondarySubtitleViewModel : ObservableObject
     [RelayCommand]
     private void Ok()
     {
-        ResultSubtitle = BuildAssaSubtitle();
+        ResultSubtitle = BuildAssaSubtitle(false);
         OkPressed = true;
         Window?.Close();
     }
@@ -118,10 +118,10 @@ public partial class OpenSecondarySubtitleViewModel : ObservableObject
         _subtitle = subtitle;
         _subtitleFormat = subtitleFormat;
         _videoFileName = videoFileName;
+        _mediaInfo = mediaInfo;
 
-        var assaFormat = new AdvancedSubStationAlpha();
         Paragraphs = new ObservableCollection<SubtitleDisplayItem>(
-            secondarySubtitle.Paragraphs.Select(p => new SubtitleDisplayItem(new SubtitleLineViewModel(p, assaFormat))));
+            secondarySubtitle.Paragraphs.Select(p => new SubtitleDisplayItem(new SubtitleLineViewModel(p, _assaFormat))));
 
         Dispatcher.UIThread.Post(() =>
         {
@@ -129,11 +129,10 @@ public partial class OpenSecondarySubtitleViewModel : ObservableObject
             {
                 _ = VideoPlayerControl.Open(videoFileName);
             }
-        });
-    }
 
-    public void BoxTypeChanged(object? sender, SelectionChangedEventArgs e)
-    {
+            var width = mediaInfo?.Dimension.Width ?? 1920;
+            FontSize = AssaResampler.Resample(AdvancedSubStationAlpha.DefaultWidth, width, Se.Settings.Video.MpvPreviewFontSize);
+        });
     }
 
     public void ComboBoxParagraphsChanged(object? sender, SelectionChangedEventArgs e)
@@ -233,9 +232,8 @@ public partial class OpenSecondarySubtitleViewModel : ObservableObject
                 return;
             }
 
-            var subtitle = BuildAssaSubtitle();
-            var format = new AdvancedSubStationAlpha();
-            var text = format.ToText(subtitle, string.Empty);
+            var subtitle = BuildAssaSubtitle(true);
+            var text = _assaFormat.ToText(subtitle, string.Empty);
             if (_oldSubtitleText == text)
             {
                 return;
@@ -259,29 +257,29 @@ public partial class OpenSecondarySubtitleViewModel : ObservableObject
 
     private string GetBorderStyle()
     {
-        if (SelectedFontBoxType.BoxType == FontBoxType.OneBox) return "4";
-        if (SelectedFontBoxType.BoxType == FontBoxType.BoxPerLine) return "3";
+        if (SelectedFontBoxType.BoxType == FontBoxType.OneBox)
+        {
+            return "4";
+        }
+
+        if (SelectedFontBoxType.BoxType == FontBoxType.BoxPerLine)
+        {
+            return "3";
+        }
+
         return "1";
     }
 
     private string GetAlignment()
     {
-        if (AlignmentAn1) return "1";
-        if (AlignmentAn3) return "3";
-        if (AlignmentAn4) return "4";
-        if (AlignmentAn5) return "5";
-        if (AlignmentAn6) return "6";
-        if (AlignmentAn7) return "7";
-        if (AlignmentAn8) return "8";
-        if (AlignmentAn9) return "9";
-        return "2";
+        return SelectedFontAlignment.Code;
     }
 
-    private Subtitle BuildAssaSubtitle()
+    private Subtitle BuildAssaSubtitle(bool mergeWithSubtitle)
     {
         var style = new SsaStyle
         {
-            Name = "Default",
+            Name = "Style" + Guid.NewGuid().ToString().Replace("-", string.Empty),
             FontName = "Arial",
             FontSize = FontSize,
             Primary = SubtitleColor.ToSKColor(),
@@ -299,18 +297,50 @@ public partial class OpenSecondarySubtitleViewModel : ObservableObject
             ScaleY = 100,
         };
 
-        var format = new AdvancedSubStationAlpha();
-        var tempSub = new Subtitle();
-        var tempText = format.ToText(tempSub, string.Empty);
-        var tempLines = tempText.SplitToLines();
-        format.LoadSubtitle(tempSub, tempLines, string.Empty);
-
-        var header = AdvancedSubStationAlpha.GetHeaderAndStylesFromAdvancedSubStationAlpha(
-            tempSub.Header,
+        var result = new Subtitle(_secondarySubtitle);
+        result.Header = AdvancedSubStationAlpha.GetHeaderAndStylesFromAdvancedSubStationAlpha(
+            AdvancedSubStationAlpha.DefaultHeader,
             new List<SsaStyle> { style });
 
-        var result = new Subtitle(_secondarySubtitle);
-        result.Header = header;
+        var width = _mediaInfo?.Dimension.Width ?? 1920;
+        var height = _mediaInfo?.Dimension.Height ?? 1080;
+        result.Header = AdvancedSubStationAlpha.AddTagToHeader("PlayResX", "PlayResX: " + width.ToString(CultureInfo.InvariantCulture), "[Script Info]", result.Header);
+        result.Header = AdvancedSubStationAlpha.AddTagToHeader("PlayResY", "PlayResY: " + height.ToString(CultureInfo.InvariantCulture), "[Script Info]", result.Header);
+
+        if (mergeWithSubtitle)
+        {
+            if (_subtitleFormat.GetType() == typeof(AdvancedSubStationAlpha))
+            {
+                result = new Subtitle(_subtitle);
+                foreach (var p in _secondarySubtitle.Paragraphs)
+                {
+                    p.Extra = style.Name;
+                    p.Layer = -1;
+                    result.Paragraphs.Add(p);
+                }
+            }
+            else
+            {
+                var defaultStyle = AdvancedSubStationAlpha.GetSsaStyle("Default", result.Header);
+                defaultStyle.FontSize = AssaResampler.Resample(AdvancedSubStationAlpha.DefaultWidth, width, Se.Settings.Video.MpvPreviewFontSize);
+                defaultStyle.Bold = Se.Settings.Video.MpvPreviewFontBold;
+                result.Header = AdvancedSubStationAlpha.UpdateOrAddStyle(result.Header, defaultStyle);
+
+                foreach (var p in _subtitle.Paragraphs)
+                {
+                    p.Layer = 1;
+                    p.Extra = "Default";
+                    result.Paragraphs.Add(p);
+                }
+            }
+        }
+        else
+        {
+            foreach (var p in _secondarySubtitle.Paragraphs)
+            {
+                p.Extra = style.Name;
+            }
+        }
 
         return result;
     }
