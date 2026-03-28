d0604dd4424425b24cfd22b4f3784196b6df42e2
Nikolaj Olsson <nikse.dk@gmail.com>
2026-03-28 07:05:01 +0100

    Work on translation

--- src/UI/Logic/Initializers/LanguageInitializer.cs
+++ src/UI/Logic/Initializers/LanguageInitializer.cs
@@ -38,6 +38,7 @@ public class LanguageInitializer() : ILanguageInitializer
         "Norwegian",
         "Polish",
         "Portuguese",
+        "Portuguese (Brazil)",
         "Romanian",
         "Russian",
         "Slovak",
