c981a835bd312ece7b6ede6c3910f94120cab497
Nikolaj Olsson <nikse.dk@gmail.com>
2026-06-05 15:23:14 +0200

    v5.0.0-rc3

--- src/ui/Logic/Config/Se.cs
+++ src/ui/Logic/Config/Se.cs
@@ -15,7 +15,7 @@ namespace Nikse.SubtitleEdit.Logic.Config;
 
 public class Se
 {
-    public static string Version { get; set; } = "v5.0.0-rc2";
+    public static string Version { get; set; } = "v5.0.0-rc3";
 
     public SeGeneral General { get; set; } = new();
     public List<SeShortCut> Shortcuts { get; set; } = new();
