639fe6cb4d368074b60225535ed4da763d52b480
niksedk <nikse.dk@gmail.com>
2026-07-08 20:07:41 +0200

    updare change log

--- src/ui/Logic/Config/Se.cs
+++ src/ui/Logic/Config/Se.cs
@@ -17,7 +17,7 @@ public class Se
 {
     internal const int CurrentMacOsFontMigrationVersion = 1;
 
-    public static string Version { get; set; } = "v5.1.0-beta11";
+    public static string Version { get; set; } = "v5.1.0-beta12";
 
     public SeGeneral General { get; set; } = new();
     public List<SeShortCut> Shortcuts { get; set; } = new();
